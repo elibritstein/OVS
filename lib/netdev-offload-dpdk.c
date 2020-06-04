d7b432e4b910c9c4e959a5bf8fda9290df2d3061
Eli Britstein <elibr@mellanox.com>
2020-06-04 08:33:46 +0000

    netdev-offload-dpdk: Refactor context release to a helper function
    As a pre-step towards delayed release, refactor to a helper function.
    
    Issue: 2193935
    Change-Id: I37d04b3a3c6ba7b5969f2a6d5e33f72d4a3101e8
    Signed-off-by: Eli Britstein <elibr@mellanox.com>

--- lib/netdev-offload-dpdk.c
+++ lib/netdev-offload-dpdk.c
@@ -429,6 +429,17 @@ get_context_data_by_id(struct context_metadata *md, uint32_t id, void *data)
 }
 
 static void
+context_release(struct context_metadata *md, uint32_t id,
+                struct context_data *data_cur)
+{
+    hmap_remove(&md->i2d_hmap, &data_cur->i2d_node);
+    hmap_remove(&md->d2i_hmap, &data_cur->d2i_node);
+    free(data_cur->data);
+    free(data_cur);
+    md->id_free(id);
+}
+
+static void
 put_context_data_by_id(struct context_metadata *md, uint32_t id)
 {
     struct context_data *data_cur;
@@ -449,11 +460,7 @@ put_context_data_by_id(struct context_metadata *md, uint32_t id)
                         data_cur->refcnt, data_cur->id);
             ds_destroy(&s);
             if (data_cur->refcnt == 0) {
-                hmap_remove(&md->i2d_hmap, &data_cur->i2d_node);
-                hmap_remove(&md->d2i_hmap, &data_cur->d2i_node);
-                free(data_cur->data);
-                free(data_cur);
-                md->id_free(id);
+                context_release(md, id, data_cur);
             }
             return;
         }
