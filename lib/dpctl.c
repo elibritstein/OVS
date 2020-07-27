3fcf458f1f8184ea5b5dca509c7d6f5c3e4a2b22
Salem Sol <salems@mellanox.com>
2020-07-27 23:37:52 +0300

    dpctl: e2e: introduce a new command to dump the cache statistics
    Introduce a new command "ovs-appctl dpctl/dump-e2e-stats" to dump
    the e2e cache statistics.
    
    Issue: 2247700
    Change-Id: Icc0fe86fce3ff5ab013e619a114d081246221706
    Signed-off-by: Salem Sol <salems@mellanox.com>

--- lib/dpctl.c
+++ lib/dpctl.c
@@ -1441,6 +1441,32 @@ dpctl_del_flow(int argc, const char *argv[], struct dpctl_params *dpctl_p)
 }
 
 static int
+dpctl_dump_e2e_stats(int argc, const char *argv[], struct dpctl_params *dpctl_p)
+{
+    struct dpif *dpif;
+    int error;
+
+    error = opt_dpif_open(argc, argv, dpctl_p, 0, &dpif);
+    if (!error) {
+        struct ds s = DS_EMPTY_INITIALIZER;
+
+        error = dpif_dump_e2e_stats(dpif, &s);
+
+        if (!error) {
+            dpctl_print(dpctl_p, "%s\n",ds_cstr(&s));
+        } else {
+            dpctl_error(dpctl_p, error, "Could not dump e2e cache stats");
+        }
+        if (s.string) {
+            ds_destroy(&s);
+        }
+        dpif_close(dpif);
+    }
+
+    return error;
+}
+
+static int
 dpctl_parse_flow_line(int command, struct ds *s, char **flow, char **action)
 {
     const char *line = ds_cstr(s);
@@ -1650,7 +1676,6 @@ dpctl_list_commands(int argc OVS_UNUSED, const char *argv[] OVS_UNUSED,
 
     return 0;
 }
-
 
 static int
 dpctl_dump_conntrack(int argc, const char *argv[],
@@ -2862,6 +2887,7 @@ static const struct dpctl_command all_commands[] = {
       0, 1, dpctl_offload_stats_show, DP_RO },
     { "dump-conntrack", "[-m] [-s] [dp] [zone=N]",
       0, 4, dpctl_dump_conntrack, DP_RO },
+    { "dump-e2e-stats", "", 0, 0, dpctl_dump_e2e_stats, DP_RO },
     { "flush-conntrack", "[dp] [zone=N] [ct-tuple]", 0, 3,
       dpctl_flush_conntrack, DP_RW },
     { "cache-get-size", "[dp]", 0, 1, dpctl_cache_get_size, DP_RO },
