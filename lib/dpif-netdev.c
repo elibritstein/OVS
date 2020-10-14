e593dc4af98f58a3d7b34ad2ba788bd45e55e258
Michael Savisko <michaelsav@nvidia.com>
2020-10-14 17:28:31 +0300

    dpif-netdev: e2e: Add stats counters for number of CT offloads
    add_ct_flow_hw for number of successful CT offload operations and
    add_ct_flow_err for number of failed CT offload operations.
    
    Issue: 2316890
    Change-Id: I6f0c8be23cd71e8741924aec8f4d859a49165ea3
    Signed-off-by: Michael Savisko <michaelsav@nvidia.com>
    Reviewed-by: Eli Britstein <elibr@nvidia.com>

--- lib/dpif-netdev.c
+++ lib/dpif-netdev.c
@@ -8617,6 +8617,8 @@ struct e2e_cache_thread_msg_queues {
  * del_merged_flow_hw = Amount of delete merged flow messages dispatched to
  *                      HW offload.
  * merged_flows_in_cache = Amount of merged flows in E2E cache.
+ * add_ct_flow_hw = Amount of successful CT offload operations to MT.
+ * add_ct_flow_err = Amount of failed CT offload operations MT.
  */
 struct e2e_cache_stats {
     atomic_uint64_t generated_msgs;
@@ -8633,6 +8635,8 @@ struct e2e_cache_stats {
     atomic_uint64_t add_merged_flow_hw;
     atomic_uint64_t del_merged_flow_hw;
     atomic_uint64_t merged_flows_in_cache;
+    uint32_t add_ct_mt_flow_hw;
+    uint32_t add_ct_mt_flow_err;
 };
 
 static struct e2e_cache_thread_msg_queues e2e_cache_thread_msg_queues = {
@@ -8708,6 +8712,10 @@ dpif_netdev_dump_e2e_stats(struct ds *s)
                   atomic_count_get64(&stats->merged_flows_in_cache));
     ds_put_format(s, "\n%-45s : %"PRIu64"", "flows in e2e DB",
                   (uint64_t)hmap_count(&flows_map));
+    ds_put_format(s, "\n%-45s : %"PRIu32"", "successful CT MT flows to HW",
+                  stats->add_ct_mt_flow_hw);
+    ds_put_format(s, "\n%-45s : %"PRIu32"", "failed CT MT flows to HW",
+                  stats->add_ct_mt_flow_err);
 }
 
 static void
@@ -9569,8 +9577,13 @@ e2e_cache_offload_ct_mt_flows(struct dp_netdev *dp,
         memcpy(actions, mt_flows[i]->actions, mt_flows[i]->actions_size);
         ret = dp_netdev_ct_offload_add_cb(&offload, &match, actions,
                                           mt_flows[i]->actions_size);
-        mt_flows[i]->offload_state = ret ? E2E_OL_STATE_CT_ERR
-                                         : E2E_OL_STATE_CT_HW;
+        if (OVS_LIKELY(ret == 0)) {
+            mt_flows[i]->offload_state = E2E_OL_STATE_CT_HW;
+            e2e_stats.add_ct_mt_flow_hw++;
+        } else {
+            mt_flows[i]->offload_state = E2E_OL_STATE_CT_ERR;
+            e2e_stats.add_ct_mt_flow_err++;
+        }
     }
     if (actions) {
         free(actions);
