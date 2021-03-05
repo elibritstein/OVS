7c53ec9131c1125b39573ef76dfdcc850d9d75e4
Gaetan Rivet <grive@u256.net>
2021-03-05 15:22:43 +0100

    netdev-offload-dpdk: Use proper try-ref function
    When context metadata were modified to use CMAPs [1], an issue was seen when
    trying to allocate a new metadata node. Searching the CMAP and finding
    an existing node had a race with the delayed release, triggering
    crashes.
    
    To fix it, a heavy-handed solution was used, to take the map lock,
    re-iterate and only then take the reference on the metadata, safely. In
    that case, the metadata reference was sure to be correct.
    
    This function was actually trying to work like
    ovs_refcount_try_ref_rcu(), while being even slower. The conditional
    reference taking is slow as well, costing a CAS loop, but it is
    *assured* to be much faster than taking and releasing a lock, and
    iterating the CMAP within.
    
    Use the proper try-ref function.
    
    [1]: 0483d0660ae8 ("netdev-offload-dpdk: Use cmap for context maps")
    
    Issue: 2440745
    Change-Id: I8410a7f3a8d05a48b64f3d1c0ce1ae00b43588f2
    Signed-off-by: Gaetan Rivet <grive@u256.net>

--- lib/netdev-offload-dpdk.c
+++ lib/netdev-offload-dpdk.c
@@ -371,27 +371,6 @@ struct context_data {
     uint32_t priv_refcount;
 };
 
-static bool
-context_data_ref(struct context_metadata *md,
-                 struct context_data *ctx_data,
-                 uint32_t dhash)
-{
-    struct context_data *data_cur;
-    bool found = false;
-
-    ovs_mutex_lock(&md->maps_lock);
-    CMAP_FOR_EACH_WITH_HASH_PROTECTED (data_cur, d2i_node, dhash,
-                                       &md->d2i_map) {
-        if (data_cur == ctx_data) {
-            ovs_refcount_ref(&data_cur->refcount);
-            found = true;
-            break;
-        }
-    }
-    ovs_mutex_unlock(&md->maps_lock);
-    return found;
-}
-
 static int
 get_context_data_id_by_data(struct context_metadata *md,
                             struct context_data *data_req,
@@ -408,7 +387,7 @@ get_context_data_id_by_data(struct context_metadata *md,
     dhash = hash_bytes(data_req->data, md->data_size, 0);
     CMAP_FOR_EACH_WITH_HASH (data_cur, d2i_node, dhash, &md->d2i_map) {
         if (!memcmp(data_req->data, data_cur->data, md->data_size)) {
-            if (!context_data_ref(md, data_cur, dhash)) {
+            if (!ovs_refcount_try_ref_rcu(&data_cur->refcount)) {
                 /* If a reference could not be taken, it means that
                  * while the data has been found within the map, it has
                  * since been removed and related ID freed. At this point,
@@ -1502,7 +1481,7 @@ get_shared_age_ctx(struct netdev *netdev,
     dhash = hash_bytes(&app_counter_key, sizeof app_counter_key, 0);
     CMAP_FOR_EACH_WITH_HASH (data_cur, d2i_node, dhash, &md->d2i_map) {
         if (app_counter_key == *((uintptr_t *) data_cur->data)) {
-            if (!context_data_ref(md, data_cur, dhash)) {
+            if (!ovs_refcount_try_ref_rcu(&data_cur->refcount)) {
                 /* If a reference could not be taken, it means that
                  * while the data has been found within the map, it has
                  * since been removed and related ID freed. At this point,
