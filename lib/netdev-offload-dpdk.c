c2697024d4dd89b4ecad31c6ae26143ee1b7df08
Gaetan Rivet <grive@u256.net>
2021-03-12 21:36:44 +0100

    netdev-offload-dpdk: Use gc for context-item
    Delete offload metadata using the garbage collector.
    
    Issue: 2618331
    Change-Id: I7aff2c1d80b441f4370109827d6289160c7dfba7
    Signed-off-by: Gaetan Rivet <grive@u256.net>

--- lib/netdev-offload-dpdk.c
+++ lib/netdev-offload-dpdk.c
@@ -504,6 +504,7 @@ static struct ovs_list *context_release_lists;
 
 struct context_release_item {
     struct ovs_list node;
+    struct ovsrcu_gc_node gc_node;
     long long int timestamp;
     struct context_metadata *md;
     uint32_t id;
@@ -512,7 +513,7 @@ struct context_release_item {
 };
 
 static void
-context_item_unref(struct context_release_item *item)
+context_item_gc(struct context_release_item *item)
 {
     free(item->data->data);
     free(item->data);
@@ -567,7 +568,7 @@ context_release(struct context_release_item *item)
                         &data->associated_i2d_node,
                         data->associated_i2d_hash);
         }
-        ovsrcu_postpone(context_item_unref, item);
+        ovsrcu_gc(context_item_gc, item, gc_node);
         ovs_mutex_unlock(&md->maps_lock);
         return;
     }
