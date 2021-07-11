elibritstein/OVS

commit b6207b1d2711933cb4c4f03d6ac91f4a7da71912

netdev-offload-dpdk: Support offload of set IPv6 actions.
Add support for set IPv6 actions.

Signed-off-by: Eli Britstein <elibr@mellanox.com>
Reviewed-by: Roni Bar Yanai <roniba@mellanox.com>
Acked-by: Sriharsha Basavapatna <sriharsha.basavapatna@broadcom.com>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

lib/netdev-offload-dpdk.c
@@ -437,6 +437,20 @@ dump_flow_action(struct ds *s, const struct rte_flow_action *actions)
         ds_put_cstr(s, "/ ");
     } else if (actions->type == RTE_FLOW_ACTION_TYPE_OF_POP_VLAN) {
         ds_put_cstr(s, "of_pop_vlan / ");
+    } else if (actions->type == RTE_FLOW_ACTION_TYPE_SET_IPV6_SRC ||
+               actions->type == RTE_FLOW_ACTION_TYPE_SET_IPV6_DST) {
+        const struct rte_flow_action_set_ipv6 *set_ipv6 = actions->conf;
+
+        char *dirstr = actions->type == RTE_FLOW_ACTION_TYPE_SET_IPV6_DST
+                       ? "dst" : "src";
+
+        ds_put_format(s, "set_ipv6_%s ", dirstr);
+        if (set_ipv6) {
+            ds_put_cstr(s, "ipv6_addr ");
+            ipv6_format_addr((struct in6_addr *) &set_ipv6->ipv6_addr, s);
+            ds_put_cstr(s, " ");
+        }
+        ds_put_cstr(s, "/ ");
     } else {
         ds_put_format(s, "unknown rte flow action (%d)\n", actions->type);
     }
@@ -984,6 +998,12 @@ BUILD_ASSERT_DECL(sizeof(struct rte_flow_action_set_ipv4) ==
                   MEMBER_SIZEOF(struct ovs_key_ipv4, ipv4_dst));
 BUILD_ASSERT_DECL(sizeof(struct rte_flow_action_set_ttl) ==
                   MEMBER_SIZEOF(struct ovs_key_ipv4, ipv4_ttl));
+BUILD_ASSERT_DECL(sizeof(struct rte_flow_action_set_ipv6) ==
+                  MEMBER_SIZEOF(struct ovs_key_ipv6, ipv6_src));
+BUILD_ASSERT_DECL(sizeof(struct rte_flow_action_set_ipv6) ==
+                  MEMBER_SIZEOF(struct ovs_key_ipv6, ipv6_dst));
+BUILD_ASSERT_DECL(sizeof(struct rte_flow_action_set_ttl) ==
+                  MEMBER_SIZEOF(struct ovs_key_ipv6, ipv6_hlimit));
 BUILD_ASSERT_DECL(sizeof(struct rte_flow_action_set_tp) ==
                   MEMBER_SIZEOF(struct ovs_key_tcp, tcp_src));
 BUILD_ASSERT_DECL(sizeof(struct rte_flow_action_set_tp) ==
@@ -1031,6 +1051,18 @@ parse_set_actions(struct flow_actions *actions,
 
             if (mask && !is_all_zeros(mask, sizeof *mask)) {
                 VLOG_DBG_RL(&rl, "Unsupported IPv4 set action");
+                return -1;
+            }
+        } else if (nl_attr_type(sa) == OVS_KEY_ATTR_IPV6) {
+            const struct ovs_key_ipv6 *key = nl_attr_get(sa);
+            const struct ovs_key_ipv6 *mask = masked ? key + 1 : NULL;
+
+            add_set_flow_action(ipv6_src, RTE_FLOW_ACTION_TYPE_SET_IPV6_SRC);
+            add_set_flow_action(ipv6_dst, RTE_FLOW_ACTION_TYPE_SET_IPV6_DST);
+            add_set_flow_action(ipv6_hlimit, RTE_FLOW_ACTION_TYPE_SET_TTL);
+
+            if (mask && !is_all_zeros(mask, sizeof *mask)) {
+                VLOG_DBG_RL(&rl, "Unsupported IPv6 set action");
                 return -1;
             }
         } else if (nl_attr_type(sa) == OVS_KEY_ATTR_TCP) {

commit 56c32e1692550ac57c3bb25d7308f410081c4585

netdev-offload-dpdk: Fix IPv6 rewrite cast-align warning
Compile with -Werror and -Wcast-align:

lib/netdev-offload-dpdk.c:578:30: error: cast increases required alignment
    of target type [-Werror=cast-align]
  578 |    ipv6_format_addr((struct in6_addr *) &set_ipv6->ipv6_addr, s);
      |                     ^

Fix it.

Change-Id: I39bdd0fd84038334367ea702398e16d1f7cb2fdf
Fixes: b6207b1d2711 ("netdev-offload-dpdk: Support offload of set IPv6 actions.")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -568,8 +568,12 @@ dump_flow_action(struct ds *s, struct ds *s_extra,
 
         ds_put_format(s, "set_ipv6_%s ", dirstr);
         if (set_ipv6) {
+            BUILD_ASSERT_DECL(
+                offsetof(struct rte_flow_action_set_ipv6, ipv6_addr) %
+                sizeof(struct in6_addr) == 0);
             ds_put_cstr(s, "ipv6_addr ");
-            ipv6_format_addr((struct in6_addr *) &set_ipv6->ipv6_addr, s);
+            ipv6_format_addr(ALIGNED_CAST(struct in6_addr *,
+                                          &set_ipv6->ipv6_addr), s);
             ds_put_cstr(s, " ");
         }
         ds_put_cstr(s, "/ ");