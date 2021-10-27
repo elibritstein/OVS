a2db27d8732bfc2f6b0c1efa59f80033d8ca076f
Eli Britstein <elibr@nvidia.com>
2021-10-27 17:21:13 +0000

    netdev-offload-dpdk: Fix CT offload with VLANs
    CT flow for example:
    flow create 1 ingress priority 0 group 3 transfer pattern eth type is
    0x0800 has_vlan is 0 / ipv4 src is 3.3.3.10 dst is 3.3.3.11 proto is 0x1
    / icmp / tag index is 0 data spec 0x100 data mask 0xff00 / end actions
    count shared 1 identifier 1 / set_tag index 0 data 0x00000022 mask
    0x000000ff / set_meta data 0x00000001 mask 0x0000ffff / jump group 1 /
    end
    
    Those matches are wrong:
    -  eth type is 0x0800
    -  has_vlan is 0
    
    CT should not match about VLANs/no VLANs, neither explicitly on
    eth-type. For CT, skip non-relevant matches to fix that issue and
    improve performance.
    
    Issue: 2838977
    Change-Id: I5fa8176710a394ba68019fe59e060f6f79979fec
    Signed-off-by: Eli Britstein <elibr@nvidia.com>

--- lib/netdev-offload-dpdk.c
+++ lib/netdev-offload-dpdk.c
@@ -3497,7 +3497,9 @@ parse_flow_match(struct netdev *netdev,
     consumed_masks->packet_type = 0;
 
     /* Eth */
-    if (match->wc.masks.dl_type ||
+    if (act_vars->is_ct_conn) {
+        add_flow_pattern(patterns, RTE_FLOW_ITEM_TYPE_ETH, NULL, NULL, NULL);
+    } else if (match->wc.masks.dl_type ||
         !eth_addr_is_zero(match->wc.masks.dl_src) ||
         !eth_addr_is_zero(match->wc.masks.dl_dst)) {
         struct rte_flow_item_eth *spec, *mask;
@@ -3699,7 +3701,8 @@ parse_flow_match(struct netdev *netdev,
         consumed_masks->nw_proto = 0;
     }
 
-    if (proto != IPPROTO_ICMP && proto != IPPROTO_UDP  &&
+    if (!act_vars->is_ct_conn &&
+        proto != IPPROTO_ICMP && proto != IPPROTO_UDP  &&
         proto != IPPROTO_SCTP && proto != IPPROTO_TCP  &&
         (match->wc.masks.tp_src ||
          match->wc.masks.tp_dst ||
@@ -3811,7 +3814,8 @@ parse_flow_match(struct netdev *netdev,
         }
     }
     /* ct-label */
-    if (!is_all_zeros(&match->wc.masks.ct_label,
+    if (!act_vars->is_ct_conn &&
+        !is_all_zeros(&match->wc.masks.ct_label,
                       sizeof match->wc.masks.ct_label)) {
         ovs_u128 tmp_u128;
         uint32_t mask;
