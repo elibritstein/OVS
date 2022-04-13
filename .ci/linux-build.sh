eb5d7371279dbfb91540941a6a1607ae4bcb97e3
Eli Britstein <elibr@nvidia.com>
2022-04-13 11:14:55 +0000

    Revert "dpdk: Update to use DPDK v21.11."
    This reverts commit 17346b3899d98730fc90f039a966b107aeae30b5.
    
    Issue: 2971531
    Change-Id: I26bb3e8adafb0e6185594a73a0c981c460b99564
    Signed-off-by: Eli Britstein <elibr@nvidia.com>

--- .ci/linux-build.sh
+++ .ci/linux-build.sh
@@ -159,10 +159,6 @@ function install_dpdk()
     # Disable building DPDK unit tests. Not needed for OVS build or tests.
     DPDK_OPTS="$DPDK_OPTS -Dtests=false"
 
-    # Disable DPDK developer mode, this results in less build checks and less
-    # meson verbose outputs.
-    DPDK_OPTS="$DPDK_OPTS -Ddeveloper_mode=disabled"
-
     # Install DPDK using prefix.
     DPDK_OPTS="$DPDK_OPTS --prefix=$(pwd)/build"
 
@@ -220,7 +216,7 @@ fi
 
 if [ "$DPDK" ] || [ "$DPDK_SHARED" ]; then
     if [ -z "$DPDK_VER" ]; then
-        DPDK_VER="21.11"
+        DPDK_VER="20.11.1"
     fi
     install_dpdk $DPDK_VER
 fi
