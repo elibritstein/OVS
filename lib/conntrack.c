elibritstein/OVS

commit ec1b933a7ccf0e7d1d1744044c26bf12df77629b

conntrack: Prepare offloading items
Prepare the CT offloading items as a pre-step towards adding and
removing ct offload items.

Issue: 1957447
Change-Id: I8e1540328beb5d3a857d05492bc2a1f7979ef794
Signed-off-by: Noa Ezra <noae@mellanox.com>

lib/conntrack.c
@@ -303,9 +303,14 @@ conntrack_offload_fill_item_common(struct ct_flow_offload_item *item,
 
 static void
 conntrack_offload_del_conn(struct conntrack *ct OVS_UNUSED,
-                           struct conn *conn OVS_UNUSED)
+                           struct conn *conn)
 {
-    VLOG_DBG("conntrack_offload_del_conn");
+    struct ct_flow_offload_item item;
+    int dir;
+
+    for (dir = 0; dir < CT_DIR_NUM; dir ++) {
+        conntrack_offload_fill_item_common(&item, conn, dir);
+    }
 }
 
 /* Initializes the connection tracker 'ct'.  The caller is responsible for
@@ -1461,7 +1466,6 @@ conntrack_swap_conn_key(const struct conn_key *key,
     swapped->dst = key->src;
 }
 
-OVS_UNUSED
 static void
 conntrack_offload_fill_item_add(struct ct_flow_offload_item *item,
                                 struct conn *conn,
@@ -1505,13 +1509,46 @@ conntrack_offload_fill_item_add(struct ct_flow_offload_item *item,
 }
 
 static void
-conntrack_offload_add_conn(struct conntrack *ct OVS_UNUSED,
-                           struct dp_packet *packet OVS_UNUSED,
-                           uint32_t mark OVS_UNUSED,
-                           ovs_u128 label OVS_UNUSED)
+conntrack_offload_prepare_add(struct ct_flow_offload_item *item,
+                              struct conn *conn,
+                              struct dp_packet *packet,
+                              uint32_t mark,
+                              ovs_u128 label,
+                              void *dp)
 {
-    VLOG_DBG("conntrack_add_ct_offload_item");
-    packet->md.conn->offloads.flags |= CT_OFFLOAD_SKIP;
+    int dir = ct_get_packet_dir(packet->md.reply);
+
+    conn->offloads.dir_info[dir].port = packet->md.orig_in_port;
+    conn->offloads.dir_info[dir].dp = dp;
+
+    conntrack_offload_fill_item_add(item, conn, packet, mark, label);
+}
+
+static void
+conntrack_offload_add_conn(struct conntrack *ct,
+                           struct dp_packet *packet,
+                           uint32_t mark,
+                           ovs_u128 label)
+{
+    struct conn *conn = packet->md.conn;
+    struct ct_flow_offload_item item;
+
+    /* CT doesn't handle alg */
+    if (conn->alg || conn->alg_related) {
+        conn->offloads.flags |= CT_OFFLOAD_SKIP;
+        return;
+    }
+
+    if ((packet->md.reply && !(conn->offloads.flags & CT_OFFLOAD_REP)) ||
+        (!packet->md.reply && !(conn->offloads.flags & CT_OFFLOAD_INIT))) {
+        conntrack_offload_prepare_add(&item, conn, packet, mark, label, ct->dp);
+        conn->offloads.flags |= packet->md.reply ? CT_OFFLOAD_REP
+                                                 : CT_OFFLOAD_INIT;
+    }
+
+    if ((conn->offloads.flags & CT_OFFLOAD_BOTH) == CT_OFFLOAD_BOTH) {
+        conn->offloads.flags |= CT_OFFLOAD_SKIP;
+    }
 }
 
 /* Sends the packets in '*pkt_batch' through the connection tracker 'ct'.  All

commit 68094b8f9be751893405eec0d6e62279b1ca3338

conntrack: Fix offload timestamp init
The CT offload item uses an uninitialized value for its timestamp,
breaking the latency measurements.

Fix it by using the PMD context clock value at the point
conntrack_execute() was called generating the offload.

Issue: 3160425
Change-Id: I74e332e1de9cd7d84f2d8e62ecf2b70a2138e150
Fixes: ec1b933a7ccf ("conntrack: Prepare offloading items")
Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

lib/conntrack.c
@@ -363,6 +363,7 @@ conntrack_offload_del_conn(struct conntrack *ct,
         conn_dir->offloads.dir_info[CT_DIR_INIT].status = false;
         conn_dir->offloads.dir_info[CT_DIR_REP].status = false;
     }
+    item[CT_DIR_INIT].timestamp = time_usec();
     item[CT_DIR_INIT].refcnt = conn->offloads.refcnt;
     item[CT_DIR_REP].refcnt = NULL;
     offload_class->conn_del(item);
@@ -1757,7 +1758,7 @@ static void
 conntrack_offload_add_conn(struct conntrack *ct,
                            struct dp_packet *packet,
                            struct conn *conn,
-                           bool reply)
+                           bool reply, long long now_us)
 {
     struct conntrack_offload_class *offload_class;
     struct ct_flow_offload_item item[CT_DIR_NUM];
@@ -1819,6 +1820,7 @@ conntrack_offload_add_conn(struct conntrack *ct,
         refcnt = xmalloc(sizeof *refcnt);
         ovs_refcount_init(refcnt);
         ovs_refcount_ref(refcnt);
+        item[CT_DIR_INIT].timestamp = now_us;
         item[CT_DIR_INIT].refcnt = refcnt;
         item[CT_DIR_REP].refcnt = NULL;
         offload_class->conn_add(item);
@@ -1851,9 +1853,11 @@ conntrack_execute(struct conntrack *ct, struct dp_packet_batch *pkt_batch,
                   const struct ovs_key_ct_labels *setlabel,
                   ovs_be16 tp_src, ovs_be16 tp_dst, const char *helper,
                   const struct nat_action_info_t *nat_action_info,
-                  long long now, uint32_t tp_id)
+                  long long now_us, uint32_t tp_id)
 {
-    ipf_preprocess_conntrack(ct->ipf, pkt_batch, now, dl_type, zone,
+    long long now_ms = now_us / 1000;
+
+    ipf_preprocess_conntrack(ct->ipf, pkt_batch, now_ms, dl_type, zone,
                              ct->hash_basis);
 
     struct dp_packet *packet;
@@ -1876,7 +1880,7 @@ conntrack_execute(struct conntrack *ct, struct dp_packet_batch *pkt_batch,
             packet->md.ct_state = CS_INVALID;
             write_ct_md_alg_exp(packet, zone, NULL, NULL);
         } else {
-            process_one(ct, packet, &ctx, zone, force, commit, now, setmark,
+            process_one(ct, packet, &ctx, zone, force, commit, now_ms, setmark,
                         setlabel, nat_action_info, tp_src, tp_dst, helper,
                         tp_id);
         }
@@ -1908,7 +1912,8 @@ conntrack_execute(struct conntrack *ct, struct dp_packet_batch *pkt_batch,
                 actual_conn->offloads.dir_info[dir].pkt_ct_label[1] =
                     updated_label_bits;
 
-                conntrack_offload_add_conn(ct, packet, conn, ctx.reply);
+                conntrack_offload_add_conn(ct, packet, conn, ctx.reply,
+                                           now_us);
             }
             if (ct_e2e_cache_enabled) {
                 e2e_cache_trace_add_ct(ct, packet, conn, ctx.reply);
@@ -1916,7 +1921,7 @@ conntrack_execute(struct conntrack *ct, struct dp_packet_batch *pkt_batch,
         }
     }
 
-    ipf_postprocess_conntrack(ct->ipf, pkt_batch, now, dl_type);
+    ipf_postprocess_conntrack(ct->ipf, pkt_batch, now_ms, dl_type);
 
     return 0;
 }

lib/conntrack.h
@@ -129,6 +129,7 @@ struct ct_flow_offload_item {
     ovs_u128 ufid;
     void *dp;
     uintptr_t ctid_key;
+    long long int timestamp;
 
     /* matches */
     struct ct_match ct_match;

lib/dpif-netdev.c
@@ -3025,7 +3025,7 @@ mark_to_flow_find(const struct dp_netdev_pmd_thread *pmd,
 static struct dp_offload_thread_item *
 dp_netdev_alloc_flow_offload(struct dp_netdev *dp,
                              struct dp_netdev_flow *flow,
-                             int op)
+                             int op, long long now)
 {
     struct dp_offload_thread_item *item;
     struct dp_offload_flow_item *flow_offload;
@@ -3035,6 +3035,7 @@ dp_netdev_alloc_flow_offload(struct dp_netdev *dp,
 
     item->type = DP_OFFLOAD_FLOW;
     item->dp = dp;
+    item->timestamp = now;
 
     flow_offload->flow = flow;
     flow_offload->op = op;
@@ -3800,8 +3801,8 @@ queue_netdev_flow_del(struct dp_netdev_pmd_thread *pmd,
         e2e_cache_flow_del(&flow->mega_ufid, pmd->dp);
     }
     offload = dp_netdev_alloc_flow_offload(pmd->dp, flow,
-                                           DP_NETDEV_FLOW_OFFLOAD_OP_DEL);
-    offload->timestamp = pmd->ctx.now;
+                                           DP_NETDEV_FLOW_OFFLOAD_OP_DEL,
+                                           pmd->ctx.now);
     dp_netdev_offload_flow_enqueue(offload);
 }
 
@@ -3866,6 +3867,7 @@ dp_netdev_ct_offload_add_item(struct ct_flow_offload_item *ct_offload)
     item = xzalloc(sizeof *item + CT_DIR_NUM * sizeof *ct_offload);
     item->type = DP_OFFLOAD_CT;
     item->dp = NULL;
+    item->timestamp = ct_offload[0].timestamp;
     for (dir = 0; dir < CT_DIR_NUM; dir++) {
         item->data->ct_offload_item[dir] = ct_offload[dir];
         item->data->ct_offload_item[dir].op = DP_NETDEV_FLOW_OFFLOAD_OP_ADD;
@@ -3888,6 +3890,7 @@ dp_netdev_ct_offload_del_item(struct ct_flow_offload_item *ct_offload)
     item = xzalloc(sizeof *item + CT_DIR_NUM * sizeof *ct_offload);
     item->type = DP_OFFLOAD_CT;
     item->dp = NULL;
+    item->timestamp = ct_offload[0].timestamp;
     for (dir = 0; dir < CT_DIR_NUM; dir++) {
         item->data->ct_offload_item[dir] = ct_offload[dir];
         item->data->ct_offload_item[dir].op = DP_NETDEV_FLOW_OFFLOAD_OP_DEL;
@@ -4044,7 +4047,7 @@ queue_netdev_flow_put(struct dp_netdev_pmd_thread *pmd,
                            actions_len);
     }
 
-    item = dp_netdev_alloc_flow_offload(pmd->dp, flow, op);
+    item = dp_netdev_alloc_flow_offload(pmd->dp, flow, op, pmd->ctx.now);
     flow_offload = &item->data->flow;
     flow_offload->match = *match;
     flow_offload->actions = xmalloc(actions_len);
@@ -4052,7 +4055,6 @@ queue_netdev_flow_put(struct dp_netdev_pmd_thread *pmd,
     flow_offload->actions_len = actions_len;
     flow_offload->orig_in_port = flow->orig_in_port;
 
-    item->timestamp = pmd->ctx.now;
     dp_netdev_offload_flow_enqueue(item);
 }
 
@@ -9682,7 +9684,8 @@ static inline void
 e2e_cache_populate_offload_item(struct dp_offload_thread_item *offload_item,
                                 int op,
                                 struct dp_netdev *dp,
-                                struct dp_netdev_flow *flow);
+                                struct dp_netdev_flow *flow,
+                                long long now);
 
 static int
 e2e_cache_ct_flow_offload_del_mt(struct dp_netdev *dp,
@@ -9692,6 +9695,7 @@ e2e_cache_ct_flow_offload_del_mt(struct dp_netdev *dp,
     struct dp_offload_thread_item *offload_item;
     struct e2e_cache_stats *e2e_stats;
     struct dp_netdev_flow flow;
+    long long now = time_usec();
     int ret;
 
     e2e_stats = &dp_offload_threads[tid].e2e_stats;
@@ -9701,9 +9705,11 @@ e2e_cache_ct_flow_offload_del_mt(struct dp_netdev *dp,
         ct_flow->ct_match[0].odp_port;
 
     offload_item = dp_netdev_alloc_flow_offload(dp, &flow,
-                                                DP_NETDEV_FLOW_OFFLOAD_OP_DEL);
+                                                DP_NETDEV_FLOW_OFFLOAD_OP_DEL,
+                                                now);
     e2e_cache_populate_offload_item(offload_item,
-                                    DP_NETDEV_FLOW_OFFLOAD_OP_DEL, dp, &flow);
+                                    DP_NETDEV_FLOW_OFFLOAD_OP_DEL, dp, &flow,
+                                    now);
 
     ret = dp_netdev_flow_offload_del(offload_item);
     free(offload_item);
@@ -10242,7 +10248,8 @@ static inline void
 e2e_cache_populate_offload_item(struct dp_offload_thread_item *offload_item,
                                 int op,
                                 struct dp_netdev *dp,
-                                struct dp_netdev_flow *flow)
+                                struct dp_netdev_flow *flow,
+                                long long now)
 {
     struct dp_offload_flow_item *flow_offload;
     flow_offload = &offload_item->data->flow;
@@ -10250,6 +10257,7 @@ e2e_cache_populate_offload_item(struct dp_offload_thread_item *offload_item,
     memset(offload_item, 0, sizeof *offload_item);
     offload_item->type = DP_OFFLOAD_FLOW;
     offload_item->dp = dp;
+    offload_item->timestamp = now;
     flow_offload->flow = flow;
     flow_offload->op = op;
     flow_offload->is_e2e_cache_flow = true;
@@ -10432,7 +10440,8 @@ e2e_cache_merged_flow_offload_del(struct e2e_cache_merged_flow *merged_flow)
     offload_item = xmalloc(sizeof *offload_item +
                            sizeof offload_item->data->flow);
     e2e_cache_populate_offload_item(offload_item,
-                                    DP_NETDEV_FLOW_OFFLOAD_OP_DEL, dp, &flow);
+                                    DP_NETDEV_FLOW_OFFLOAD_OP_DEL, dp, &flow,
+                                    time_usec());
 
     merged_flow->dp = NULL;
     e2e_stats->del_merged_flow_hw++;
@@ -10511,7 +10520,8 @@ e2e_cache_merged_flow_offload_put(struct dp_netdev *dp,
     offload_item = xmalloc(sizeof *offload_item +
                            sizeof offload_item->data->flow);
     e2e_cache_populate_offload_item(offload_item,
-                                    DP_NETDEV_FLOW_OFFLOAD_OP_ADD, dp, &flow);
+                                    DP_NETDEV_FLOW_OFFLOAD_OP_ADD, dp, &flow,
+                                    time_usec());
 
     num_elements = trc_info->num_elements;
     /* For CT2CT, don't associate the last megaflow. */
@@ -12036,7 +12046,7 @@ dp_execute_cb(void *aux_, struct dp_packet_batch *packets_,
         conntrack_execute(dp->conntrack, packets_, aux->flow->dl_type, force,
                           commit, zone, setmark, setlabel, aux->flow->tp_src,
                           aux->flow->tp_dst, helper, nat_action_info_ref,
-                          pmd->ctx.now / 1000, tp_id);
+                          pmd->ctx.now, tp_id);
         break;
     }
 

tests/test-conntrack.c
@@ -85,7 +85,7 @@ ct_thread_main(void *aux_)
     struct dp_packet *pkt;
     ovs_be16 dl_type;
     size_t i;
-    long long now = time_msec();
+    long long now = time_usec();
 
     pkt_batch = prepare_packets(batch_size, change_conn, aux->tid, &dl_type);
     ovs_barrier_block(&barrier);
@@ -159,7 +159,7 @@ pcap_batch_execute_conntrack(struct conntrack *ct_,
 {
     struct dp_packet_batch new_batch;
     ovs_be16 dl_type = htons(0);
-    long long now = time_msec();
+    long long now = time_usec();
 
     dp_packet_batch_init(&new_batch);
 