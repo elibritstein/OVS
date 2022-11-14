84a0b8a1eda0707a7ac59b8424ba16c01c6f05cb
Eli Britstein <elibr@nvidia.com>
2022-11-14 13:46:55 +0200

    dp-packet: Fix dp-packet may be used initialized
    With --enable-Werror and --with-dpdk=no:
    
    In function 'dp_packet_reset_offload',
        inlined from 'dp_packet_init__' at ../lib/dp-packet.c:35:5,
        inlined from 'dp_packet_use__' at ../lib/dp-packet.c:50:5,
        inlined from 'dp_packet_use' at ../lib/dp-packet.c:60:5,
        inlined from 'dp_packet_init' at ../lib/dp-packet.c:126:5,
        inlined from 'dp_packet_new' at ../lib/dp-packet.c:154:5:
    ../lib/dp-packet.h:944:32: error: 'MEM[(uint32_t *)p_14 + 16B]' may be used uninitialized [-Werror=maybe-uninitialized]
      944 |     *dp_packet_ol_flags_ptr(p) &= ~DP_PACKET_OL_SUPPORTED_MASK;
          |                                ^
    
    OVS-issue: 3282529
    Fixes: a47e2db209e4 ("dp-packet: Refactor offloading API.")
    Signed-off-by: Eli Britstein <elibr@nvidia.com>

--- lib/dp-packet.h
+++ lib/dp-packet.h
@@ -641,7 +641,8 @@ dp_packet_init_specific(struct dp_packet *p)
 {
     /* This initialization is needed for packets that do not come from DPDK
      * interfaces, when vswitchd is built with --with-dpdk. */
-    p->mbuf.ol_flags = p->mbuf.tx_offload = p->mbuf.packet_type = 0;
+    *dp_packet_ol_flags_ptr(p) = 0;
+    p->mbuf.tx_offload = p->mbuf.packet_type = 0;
     p->mbuf.nb_segs = 1;
     p->mbuf.next = NULL;
 }
@@ -720,9 +721,9 @@ dp_packet_get_meta(const struct dp_packet *p, uint32_t *meta)
 #else /* DPDK_NETDEV */
 
 static inline void
-dp_packet_init_specific(struct dp_packet *p OVS_UNUSED)
+dp_packet_init_specific(struct dp_packet *p)
 {
-    /* There are no implementation-specific fields for initialization. */
+    *dp_packet_ol_flags_ptr(p) = 0;
 }
 
 static inline void *
