elibritstein/OVS

commit 69185eb25acbaa4b4744f6d9387920ba1c3625eb

ovs-ofctl: Only allow usable protocols for group commands
parse_ofp_group_mod_str() may limit the usable protocols according
to the group and in particular its actions. However, without this
change ovs-ofctl ignores this calculation.

Signed-off-by: Simon Horman <simon.horman@netronome.com>
Acked-by: Jarno Rajahalme <jrajahalme@nicira.com>

utilities/ovs-ofctl.c
@@ -2083,19 +2083,22 @@ ofctl_benchmark(int argc OVS_UNUSED, char *argv[])
 
 static void
 ofctl_group_mod__(const char *remote, struct ofputil_group_mod *gms,
-                 size_t n_gms)
+                  size_t n_gms, enum ofputil_protocol usable_protocols)
 {
+    enum ofputil_protocol protocol;
     struct ofputil_group_mod *gm;
+    enum ofp_version version;
     struct ofpbuf *request;
 
     struct vconn *vconn;
     size_t i;
 
-    open_vconn(remote, &vconn);
+    protocol = open_vconn_for_flow_mod(remote, &vconn, usable_protocols);
+    version = ofputil_protocol_to_ofp_version(protocol);
 
     for (i = 0; i < n_gms; i++) {
         gm = &gms[i];
-        request = ofputil_encode_group_mod(vconn_get_version(vconn), gm);
+        request = ofputil_encode_group_mod(version, gm);
         if (request) {
             transact_noreply(vconn, request);
         }
@@ -2120,7 +2123,7 @@ ofctl_group_mod_file(int argc OVS_UNUSED, char *argv[], uint16_t command)
     if (error) {
         ovs_fatal(0, "%s", error);
     }
-    ofctl_group_mod__(argv[1], gms, n_gms);
+    ofctl_group_mod__(argv[1], gms, n_gms, usable_protocols);
     for (i = 0; i < n_gms; i++) {
         ofputil_bucket_list_destroy(&gms[i].buckets);
     }
@@ -2142,7 +2145,7 @@ ofctl_group_mod(int argc, char *argv[], uint16_t command)
         if (error) {
             ovs_fatal(0, "%s", error);
         }
-        ofctl_group_mod__(argv[1], &gm, 1);
+        ofctl_group_mod__(argv[1], &gm, 1, usable_protocols);
         ofputil_bucket_list_destroy(&gm.buckets);
     }
 }

commit cd29b375abd44affacbc6a6b1f663cab75898bf7

ovs-ofctl: Fix 'usable_protocols' may be used uninitialized
With --enable-Werror and --with-dpdk=no:

../utilities/ovs-ofctl.c: In function 'ofctl_group_mod_file':
../utilities/ovs-ofctl.c:3107:16: error: 'usable_protocols' may be used uninitialized in this function [-Werror=maybe-uninitialized]
 3107 |     protocol = open_vconn_for_flow_mod(remote, &vconn, usable_protocols);
      |                ^
../utilities/ovs-ofctl.c:3124:27: note: 'usable_protocols' was declared here
 3124 |     enum ofputil_protocol usable_protocols;
      |                           ^

Change-Id: If63dca1dee84ea675af2663d3080ea58513be144
Fixes: 69185eb25acb ("ovs-ofctl: Only allow usable protocols for group commands")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

utilities/ovs-ofctl.c
@@ -3121,7 +3121,7 @@ static void
 ofctl_group_mod_file(int argc OVS_UNUSED, char *argv[], int command)
 {
     struct ofputil_group_mod *gms = NULL;
-    enum ofputil_protocol usable_protocols;
+    enum ofputil_protocol usable_protocols = 0;
     size_t n_gms = 0;
     char *error;
 