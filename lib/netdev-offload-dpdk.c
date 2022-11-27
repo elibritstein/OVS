elibritstein/OVS

commit 5ba0bfd2272ccee780b0a6bf9aac1ab884773459

netdev-offload-dpdk: Flush ESW members when flushing the ESW manager
Offload flows are applied on the ESW manager of each netdev. If it is
flushed, the offloads of its members should be flushed, as they depend
on it.

Issue: 3129943
Change-Id: Id02c0a6516f75be9ba0abd14c3c2c12ffcd46edd
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -5852,11 +5852,11 @@ static void
 ct_tables_uninit(struct netdev *netdev, unsigned int tid);
 
 static int
-netdev_offload_dpdk_flow_flush(struct netdev *netdev)
+netdev_offload_dpdk_flow_flush__(struct netdev *netdev)
 {
+    unsigned int tid = netdev_offload_thread_id();
     struct cmap *map = offload_data_map(netdev);
     struct ufid_to_rte_flow_data *data;
-    unsigned int tid = netdev_offload_thread_id();
 
     if (!map) {
         return -1;
@@ -5874,6 +5874,67 @@ netdev_offload_dpdk_flow_flush(struct netdev *netdev)
     }
 
     return 0;
+}
+
+struct flush_esw_members_aux {
+    struct netdev *esw_netdev;
+    int ret;
+};
+
+/* Upon flushing the ESW manager, its members netdevs should be flushed too,
+ * as their offloads are done on it.
+ */
+static bool
+flush_esw_members_cb(struct netdev *netdev,
+                     odp_port_t odp_port OVS_UNUSED,
+                     void *aux_)
+{
+    struct flush_esw_members_aux *aux = aux_;
+    struct netdev *esw_netdev;
+    int netdev_esw_mgr_pid;
+    int esw_mgr_pid;
+
+    esw_netdev = aux->esw_netdev;
+
+    /* Skip the ESW netdev itself. */
+    if (netdev == esw_netdev) {
+        return false;
+    }
+
+    netdev_esw_mgr_pid = netdev_dpdk_get_esw_mgr_port_id(netdev);
+    esw_mgr_pid = netdev_dpdk_get_esw_mgr_port_id(esw_netdev);
+
+    /* Skip a non-member. */
+    if (netdev_esw_mgr_pid == -1 || esw_mgr_pid == -1 ||
+        netdev_esw_mgr_pid != esw_mgr_pid) {
+        return false;
+    }
+
+    if (netdev_offload_dpdk_flow_flush__(netdev)) {
+        aux->ret = -1;
+        return true;
+    }
+
+    return false;
+}
+
+static int
+netdev_offload_dpdk_flow_flush(struct netdev *netdev)
+{
+    struct flush_esw_members_aux aux = {
+        .esw_netdev = netdev,
+        .ret = 0,
+    };
+
+    if (netdev_offload_dpdk_flow_flush__(netdev)) {
+        return -1;
+    }
+
+    if (!netdev_vport_is_vport_class(netdev->netdev_class)) {
+        netdev_ports_traverse(netdev->dpif_type, flush_esw_members_cb, &aux);
+    }
+
+    return aux.ret;
 }
 
 struct get_vport_netdev_aux {

commit 2e0e6fdd22ddadedf9bbd37a4eef5d27329d7064

netdev-offload-dpdk: Fix flushing of a non ESW manager port
Offloads are applied on the ESW manager port. Upon its removal, it is
flushed and the offloads on all its members should be flushed too as
done in [1]. However, when flushing a non ESW manager port, only its
offloads should be flushed. In [1] there was a missing check for the
flushed port to be an ESW manager or not. Fix it.

[1]: 5ba0bfd2272c ("netdev-offload-dpdk: Flush ESW members when flushing the ESW manager")

OVS-issue: 3273447
Fixes: 5ba0bfd2272c ("netdev-offload-dpdk: Flush ESW members when flushing the ESW manager")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -5918,6 +5918,7 @@ flush_netdev_flows_in_related(struct netdev *netdev, struct netdev *related)
 
 struct flush_esw_members_aux {
     struct netdev *esw_netdev;
+    int esw_mgr_pid;
     int ret;
 };
 
@@ -5935,6 +5936,7 @@ flush_esw_members_cb(struct netdev *netdev,
     int esw_mgr_pid;
 
     esw_netdev = aux->esw_netdev;
+    esw_mgr_pid = aux->esw_mgr_pid;
 
     /* Skip the ESW netdev itself. */
     if (netdev == esw_netdev) {
@@ -5942,11 +5944,9 @@ flush_esw_members_cb(struct netdev *netdev,
     }
 
     netdev_esw_mgr_pid = netdev_dpdk_get_esw_mgr_port_id(netdev);
-    esw_mgr_pid = netdev_dpdk_get_esw_mgr_port_id(esw_netdev);
 
     /* Skip a non-member. */
-    if (netdev_esw_mgr_pid == -1 || esw_mgr_pid == -1 ||
-        netdev_esw_mgr_pid != esw_mgr_pid) {
+    if (netdev_esw_mgr_pid == -1 || netdev_esw_mgr_pid != esw_mgr_pid) {
         return false;
     }
 
@@ -5986,7 +5986,13 @@ netdev_offload_dpdk_flow_flush(struct netdev *netdev)
     }
 
     if (!netdev_vport_is_vport_class(netdev->netdev_class)) {
-        netdev_ports_traverse(netdev->dpif_type, flush_esw_members_cb, &aux);
+        /* If the flushed netdev is an ESW manager, flush its members too. */
+        aux.esw_mgr_pid = netdev_dpdk_get_esw_mgr_port_id(netdev);
+        if (aux.esw_mgr_pid != -1 &&
+            aux.esw_mgr_pid == netdev_dpdk_get_port_id(netdev)) {
+            netdev_ports_traverse(netdev->dpif_type, flush_esw_members_cb,
+                                  &aux);
+        }
         netdev_ports_traverse(netdev->dpif_type, flush_in_vport_cb, netdev);
     }
 