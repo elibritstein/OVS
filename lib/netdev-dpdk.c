elibritstein/OVS

commit 297f0b3a2d543d2ff0b07d29c7056cce33dfc1ff

netdev-dpdk: Support netdev without representor
Create a single netdev for all kind of vDPA interfaces.
1. With representor:
   ovs-vsctl add-port br0-ovs vdpa0 -- set Interface vdpa0 \
   type=dpdkvdpa options:vdpa-socket-path=<socket_path> \
   options:vdpa-accelerator-devargs=<VF_pci> \
   options:dpdk-devargs=<representor>
2. Without representor:
   ovs-vsctl add-port br0-ovs vdpa0 -- set Interface vdpa0 \
   type=dpdkvdpa options:vdpa-socket-path=<socket_path> \
   options:vdpa-accelerator-devargs=<VF_pci>

Issue: 2216478
Change-Id: I8a61e1aa7a24b0dada0870343a5519970053b3b5
Signed-off-by: Noa Ezra <noae@mellanox.com>

lib/netdev-dpdk.c
@@ -532,6 +532,7 @@ struct netdev_dpdk {
     );
 
     struct netdev_dpdk_vdpa_relay *relay;
+    bool netdev_rep;
 };
 
 struct netdev_rxq_dpdk {
@@ -944,6 +945,11 @@ dpdk_watchdog(void *dummy OVS_UNUSED)
         ovs_mutex_lock(&dpdk_mutex);
         LIST_FOR_EACH (dev, list_node, &dpdk_list) {
             ovs_mutex_lock(&dev->mutex);
+            if (!dev->netdev_rep) {
+                ovs_mutex_unlock(&dev->mutex);
+                continue;
+            }
+
             if (dev->type == DPDK_DEV_ETH) {
                 check_link_status(dev);
             }
@@ -2108,6 +2114,7 @@ netdev_dpdk_vdpa_set_config(struct netdev *netdev, const struct smap *args,
                 smap_get(args, "vdpa-accelerator-devargs");
     const char *vdpa_socket_path =
                 smap_get(args, "vdpa-socket-path");
+    const char *rep = smap_get(args, "dpdk-devargs");
     int vdpa_max_queues = smap_get_int(args, "vdpa-max-queues", -1);
     bool vdpa_sw = smap_get_bool(args, "vdpa-sw", false);
     int err = 0;
@@ -2118,11 +2125,14 @@ netdev_dpdk_vdpa_set_config(struct netdev *netdev, const struct smap *args,
                  netdev->name);
         goto free_relay;
     }
+    dev->netdev_rep = (rep == NULL) ? false : true;
 
-    err = netdev_dpdk_set_config(netdev, args, errp);
-    if (err) {
-        VLOG_ERR("netdev_dpdk_set_config failed. Port: %s", netdev->name);
-        goto free_relay;
+    if (dev->netdev_rep) {
+        err = netdev_dpdk_set_config(netdev, args, errp);
+        if (err) {
+            VLOG_ERR("netdev_dpdk_set_config failed. Port: %s", netdev->name);
+            goto free_relay;
+        }
     }
 
     err = netdev_dpdk_vdpa_config_impl(dev->relay, dev->port_id,
@@ -2606,13 +2616,16 @@ netdev_dpdk_vdpa_rxq_recv(struct netdev_rxq *rxq,
 {
     struct netdev_dpdk *dev = netdev_dpdk_cast(rxq->netdev);
     int fwd_rx;
-    int ret;
+    int ret = 0;
 
     fwd_rx = netdev_dpdk_vdpa_rxq_recv_impl(dev->relay, rxq->queue_id);
-    ret = netdev_dpdk_rxq_recv(rxq, batch, qfill);
-    if ((ret == EAGAIN) && fwd_rx) {
-        return 0;
+    if (dev->netdev_rep) {
+        ret = netdev_dpdk_rxq_recv(rxq, batch, qfill);
+        if ((ret == EAGAIN) && fwd_rx) {
+            return 0;
+        }
     }
+
     return ret;
 }
 
@@ -3037,6 +3050,21 @@ netdev_dpdk_eth_send(struct netdev *netdev, int qid,
 }
 
 static int
+netdev_dpdk_vdpa_send(struct netdev *netdev,
+                      int qid,
+                      struct dp_packet_batch *batch,
+                      bool concurrent_txq)
+{
+    struct netdev_dpdk *dev = netdev_dpdk_cast(netdev);
+
+    if (!dev->netdev_rep) {
+        return 0;
+    } else {
+        return netdev_dpdk_eth_send(netdev, qid, batch, concurrent_txq);
+    }
+}
+
+static int
 netdev_dpdk_set_etheraddr__(struct netdev_dpdk *dev, const struct eth_addr mac)
     OVS_REQUIRES(dev->mutex)
 {
@@ -3291,6 +3319,20 @@ out:
     ovs_mutex_unlock(&dev->mutex);
 
     return 0;
+}
+
+static int
+netdev_dpdk_vdpa_get_stats(const struct netdev *netdev,
+                           struct netdev_stats *stats)
+{
+    struct netdev_dpdk *dev = netdev_dpdk_cast(netdev);
+    int ret = 0;
+
+    if (dev->netdev_rep) {
+        ret = netdev_dpdk_get_stats(netdev, stats);
+    }
+
+    return ret;
 }
 
 static int
@@ -5228,13 +5270,24 @@ netdev_dpdk_vdpa_reconfigure(struct netdev *netdev)
     struct netdev_dpdk *dev = netdev_dpdk_cast(netdev);
     int err;
 
-    err = netdev_dpdk_reconfigure(netdev);
-    if (err) {
-        VLOG_ERR("netdev_dpdk_reconfigure failed. Port %s", netdev->name);
-        goto out;
+    if (dev->netdev_rep) {
+        err = netdev_dpdk_reconfigure(netdev);
+        if (err) {
+            VLOG_ERR("netdev_dpdk_reconfigure failed. Port %s", netdev->name);
+            goto out;
+        }
+        ovs_mutex_lock(&dev->mutex);
+    } else {
+        netdev->n_txq = dev->requested_n_txq;
+        netdev->n_rxq = dev->requested_n_rxq;
+        ovs_mutex_lock(&dev->mutex);
+
+        err = netdev_dpdk_mempool_configure(dev);
+        if (err && err != EEXIST) {
+            return err;
+        }
     }
 
-    ovs_mutex_lock(&dev->mutex);
     err = netdev_dpdk_vdpa_update_relay(dev->relay, dev->dpdk_mp->mp,
                                         dev->up.n_rxq);
     if (err) {
@@ -5598,9 +5651,9 @@ static const struct netdev_class dpdk_vdpa_class = {
     .rxq_recv = netdev_dpdk_vdpa_rxq_recv,
     .set_config = netdev_dpdk_vdpa_set_config,
     .reconfigure = netdev_dpdk_vdpa_reconfigure,
-    .get_stats = netdev_dpdk_get_stats,
+    .get_stats = netdev_dpdk_vdpa_get_stats,
     .get_custom_stats = netdev_dpdk_vdpa_get_custom_stats,
-    .send = netdev_dpdk_eth_send
+    .send = netdev_dpdk_vdpa_send
 };
 
 void

commit d0b7796adb2b8f6e36a1d8d11f2f8c63053a453d

netdev-dpdk: Don't run dpdk commands on invalid ports (vdpa)
Upon configuring a vdpa port without a representor, it is not a valid dpdk
port. Therefore, it is not "started". Thus, there is no need to stop it,
otherwise, an error message is emitted. Also, report -1 as an error for
querying such port IDs.

OVS-issue: 3278106
Fixes: 297f0b3a2d54 ("netdev-dpdk: Support netdev without representor")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-dpdk.c
@@ -1570,8 +1570,10 @@ netdev_dpdk_destruct(struct netdev *netdev)
 
     ovs_mutex_lock(&dpdk_mutex);
 
-    rte_eth_dev_stop(dev->port_id);
-    dev->started = false;
+    if (dev->started) {
+        rte_eth_dev_stop(dev->port_id);
+        dev->started = false;
+    }
 
     if (dev->attached) {
         /* Retrieve eth device data before closing it.
@@ -5420,14 +5422,15 @@ unlock:
 int
 netdev_dpdk_get_esw_mgr_port_id(struct netdev *netdev)
 {
-    struct netdev_dpdk *dev;
+    struct netdev_dpdk *dev = netdev_dpdk_cast(netdev);
     int ret = -1;
 
-    if (!is_dpdk_class(netdev->netdev_class)) {
+    if (!is_dpdk_class(netdev->netdev_class) ||
+        !rte_eth_dev_is_valid_port(dev->port_id) ||
+        !rte_eth_dev_is_valid_port(dev->esw_mgr_port_id)) {
         goto out;
     }
 
-    dev = netdev_dpdk_cast(netdev);
     ret = dev->esw_mgr_port_id;
 out:
     return ret;
@@ -5436,14 +5439,14 @@ out:
 int
 netdev_dpdk_get_port_id(struct netdev *netdev)
 {
-    struct netdev_dpdk *dev;
+    struct netdev_dpdk *dev = netdev_dpdk_cast(netdev);
     int ret = -1;
 
-    if (!is_dpdk_class(netdev->netdev_class)) {
+    if (!is_dpdk_class(netdev->netdev_class) ||
+        !rte_eth_dev_is_valid_port(dev->port_id)) {
         goto out;
     }
 
-    dev = netdev_dpdk_cast(netdev);
     ret = dev->port_id;
 out:
     return ret;