7ea39d60bbe57c3ecd5db51413640d4b370d4ef0
Eli Britstein <elibr@nvidia.com>
2022-12-04 15:13:14 +0200

    iofp-actions: Fix pcp may be used uninitialized
    ../../lib/ofp-actions.c: In function 'ofpacts_parse__':
    ../../lib/ofp-actions.c:1742:24: error: 'pcp' may be used uninitialized in this function []8;;https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html#index-Wmaybe-uninitialized-Werror=maybe-uninitialized]8;;]
     1742 |     vlan_pcp->vlan_pcp = pcp;
          |                        ^
    ../../lib/ofp-actions.c:1730:13: note: 'pcp' was declared here
     1730 |     uint8_t pcp;
          |             ^
    
    OVS-issue: 3282529
    Signed-off-by: Eli Britstein <elibr@nvidia.com>

--- lib/ofp-actions.c
+++ lib/ofp-actions.c
@@ -1727,7 +1727,7 @@ parse_set_vlan_pcp(char *arg, bool push_vlan_if_needed,
                    const struct ofpact_parse_params *pp)
 {
     struct ofpact_vlan_pcp *vlan_pcp;
-    uint8_t pcp;
+    uint8_t pcp = 0;
     char *error;
 
     error = str_to_u8(arg, "VLAN PCP", &pcp);
