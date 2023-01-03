elibritstein/OVS

commit 502ec5938632efee8e5d9aa1951262e620a8d0ec

conntrack: Offload written CT label+mark value
Mark and label are read before processing the connection update.
If a connection has setmark and setlabel set and the connection
becomes '+est' as a result of this execution, those new values
won't be properly taken.

If anything is updated, it is written in 'packet->md'.
Read it and apply as-is to the offload.

Issue: 3103161
Change-Id: I43eafc8bcb8a9fee4c83bbd514feb52d1756a830
Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

lib/conntrack-private.h
@@ -133,6 +133,8 @@ struct ct_dir_info {
     void *dp;
     int status;
     uint8_t pkt_ct_state;
+    uint32_t pkt_ct_mark[2];
+    ovs_u128 pkt_ct_label[2];
     bool e2e_flow;
     uint8_t e2e_seen_pkts;
 };

lib/conntrack.c
@@ -1622,9 +1622,7 @@ conntrack_swap_conn_key(const struct conn_key *key,
 static void
 conntrack_offload_fill_item_add(struct ct_flow_offload_item *item,
                                 struct conn *conn,
-                                int dir,
-                                uint32_t mark,
-                                ovs_u128 label)
+                                int dir)
 {
     /* nat_conn has opposite directions. */
     bool reply = !!conn->master_conn ^ dir;
@@ -1655,11 +1653,10 @@ conntrack_offload_fill_item_add(struct ct_flow_offload_item *item,
 
     conntrack_offload_fill_item_common(item, conn, dir);
     item->ct_state = conn->offloads.dir_info[dir].pkt_ct_state;
-    item->mark_key = conn->mark;
-    item->mark_mask = mark ^ conn->mark;
-    item->label_key = conn->label;
-    item->label_mask.u64.hi = label.u64.hi ^ conn->label.u64.hi;
-    item->label_mask.u64.lo = label.u64.lo ^ conn->label.u64.lo;
+    item->mark_key = conn->offloads.dir_info[dir].pkt_ct_mark[0];
+    item->mark_mask = conn->offloads.dir_info[dir].pkt_ct_mark[1];
+    item->label_key = conn->offloads.dir_info[dir].pkt_ct_label[0];
+    item->label_mask = conn->offloads.dir_info[dir].pkt_ct_label[1];
     item->status = &conn->offloads.dir_info[dir].status;
 }
 
@@ -1681,9 +1678,7 @@ static inline void
 e2e_cache_trace_add_ct(struct conntrack *ct,
                        struct dp_packet *p,
                        struct conn *conn,
-                       bool reply,
-                       uint32_t mark,
-                       ovs_u128 label)
+                       bool reply)
 {
     struct conntrack_offload_class *offload_class;
     uint32_t e2e_trace_size = p->e2e_trace_size;
@@ -1712,7 +1707,7 @@ e2e_cache_trace_add_ct(struct conntrack *ct,
                conn->master_conn->offloads.dir_info[dir].dp) {
         conn = conn->master_conn;
     }
-    conntrack_offload_fill_item_add(&item, conn, dir, mark, label);
+    conntrack_offload_fill_item_add(&item, conn, dir);
     item.ct_match.odp_port = p->md.in_port.odp_port;
 
     dir_info = &conn->offloads.dir_info[dir];
@@ -1754,15 +1749,13 @@ e2e_cache_trace_add_ct(struct conntrack *ct,
     p->e2e_trace_size = e2e_trace_size + 1;
 }
 #else
-#define e2e_cache_trace_add_ct(ct, p, conn, r, m, l) do { } while (0)
+#define e2e_cache_trace_add_ct(ct, p, conn, r) do { } while (0)
 #endif
 
 static void
 conntrack_offload_add_conn(struct conntrack *ct,
                            struct dp_packet *packet,
                            struct conn *conn,
-                           uint32_t mark,
-                           ovs_u128 label,
                            bool reply)
 {
     struct conntrack_offload_class *offload_class;
@@ -1813,8 +1806,7 @@ conntrack_offload_add_conn(struct conntrack *ct,
                        conn->master_conn->offloads.dir_info[dir].dp) {
                 conn = conn->master_conn;
             }
-            conntrack_offload_fill_item_add(&item[dir], conn, dir, mark,
-                                            label);
+            conntrack_offload_fill_item_add(&item[dir], conn, dir);
             ufid[dir] = &conn->offloads.dir_info[dir].ufid;
         }
         offload_class->conn_get_ufid(&item[CT_DIR_INIT],
@@ -1865,13 +1857,12 @@ conntrack_execute(struct conntrack *ct, struct dp_packet_batch *pkt_batch,
 
     struct dp_packet *packet;
     struct conn_lookup_ctx ctx;
-    uint32_t mark;
-    ovs_u128 label;
 
     DP_PACKET_BATCH_FOR_EACH (i, packet, pkt_batch) {
         struct conn *conn = packet->md.conn;
-        mark = conn ? conn->mark : 0;
-        label = conn ? conn->label : OVS_U128_ZERO;
+        uint32_t orig_mark = packet->md.ct_mark;
+        ovs_u128 orig_label = packet->md.ct_label;
+
         ctx.conn = NULL;
         if (OVS_UNLIKELY(packet->md.ct_state == CS_INVALID)) {
             write_ct_md_alg_exp(packet, zone, NULL, NULL);
@@ -1892,12 +1883,34 @@ conntrack_execute(struct conntrack *ct, struct dp_packet_batch *pkt_batch,
         if ((packet->md.ct_state & CS_ESTABLISHED) && conn) {
             if (netdev_is_flow_api_enabled() &&
                 !(conn->offloads.flags & CT_OFFLOAD_SKIP)) {
-                conntrack_offload_add_conn(ct, packet, conn, mark, label,
-                                           ctx.reply);
+                ovs_u128 updated_label_bits = ovs_u128_xor(packet->md.ct_label,
+                                                           orig_label);
+                uint32_t updated_mark_bits = packet->md.ct_mark ^ orig_mark;
+                int dir = ctx.reply ? CT_DIR_REP : CT_DIR_INIT;
+                struct conn *actual_conn = conn;
+
+                if (conn->nat_conn &&
+                    conn->nat_conn->offloads.dir_info[dir].dp) {
+                    actual_conn = conn->nat_conn;
+                } else if (conn->master_conn &&
+                           conn->master_conn->offloads.dir_info[dir].dp) {
+                    actual_conn = conn->master_conn;
+                }
+
+                actual_conn->offloads.dir_info[dir].pkt_ct_mark[0] =
+                    packet->md.ct_mark;
+                actual_conn->offloads.dir_info[dir].pkt_ct_mark[1] =
+                    updated_mark_bits;
+
+                actual_conn->offloads.dir_info[dir].pkt_ct_label[0] =
+                    packet->md.ct_label;
+                actual_conn->offloads.dir_info[dir].pkt_ct_label[1] =
+                    updated_label_bits;
+
+                conntrack_offload_add_conn(ct, packet, conn, ctx.reply);
             }
             if (ct_e2e_cache_enabled) {
-                e2e_cache_trace_add_ct(ct, packet, conn, ctx.reply, mark,
-                                       label);
+                e2e_cache_trace_add_ct(ct, packet, conn, ctx.reply);
             }
         }
     }

commit eebb609a275b7423145349ab1c83c1197f4b6e3e

conntrack-offload: Always set ct-mark/label values
table=0,arp,actions=NORMAL
table=0,ip,ct_state=-trk actions=ct(zone=2, table=1)
table=1,ip,in_port=eth0,ct_state=+trk+new
    actions=ct(zone=2, commit, exec(set_field:0x64->ct_mark)),eth1
table=1,ip,in_port=eth0,ct_state=+trk+est
    actions=ct(zone=2, commit, exec(set_field:0x64->ct_mark)),eth1
table=1,ip,in_port=eth1,ct_state=+trk+est,ct_mark=0x64
    actions=ct(zone=2, commit),eth0

In case CT mark/label are already set via previous flows, before the
connection is established, they are kept unchanged while processing a
packet when the connection is first established.

In such case the offload didn't include them in its action. Fix it by
always setting them.

Issue: OVS-3300113
Fixes: 502ec5938632 ("conntrack: Offload written CT label+mark value")
Fixes: e7f231670155 ("conntrack: Introduce fill item functions")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/conntrack-dist.c
@@ -174,9 +174,7 @@ ctd_conntrack_execute(struct dp_packet *pkt)
     struct conn_lookup_ctx *ctx;
     const uint32_t *setmark;
     struct conntrack *ct;
-    ovs_u128 orig_label;
     const char *helper;
-    uint32_t orig_mark;
     struct conn *conn;
     struct ct_exec *e;
     long long now_us;
@@ -206,8 +204,6 @@ ctd_conntrack_execute(struct dp_packet *pkt)
                              ct->hash_basis);
 
     conn = pkt->md.conn;
-    orig_mark = pkt->md.ct_mark;
-    orig_label = pkt->md.ct_label;
 
     ctx = &pkt->ct_exec.ct_lookup_ctx;
     ctx->conn = NULL;
@@ -224,8 +220,7 @@ ctd_conntrack_execute(struct dp_packet *pkt)
         ctd_process_one(pkt);
     }
     conn = pkt->md.conn ? pkt->md.conn : ctx->conn;
-    process_one_ct_offload(ct, pkt, conn, ctx->reply, now_us, orig_mark,
-                           orig_label);
+    process_one_ct_offload(ct, pkt, conn, ctx->reply, now_us);
 
     ipf_postprocess_conntrack(ct->ipf, &pkt_batch, now_ms, dl_type);
 

lib/conntrack-offload.c
@@ -150,10 +150,8 @@ conntrack_offload_fill_item_add(struct ct_flow_offload_item *item,
 
     conntrack_offload_fill_item_common(item, conn, dir);
     item->ct_state = conn->offloads.dir_info[dir].pkt_ct_state;
-    item->mark_key = conn->offloads.dir_info[dir].pkt_ct_mark[0];
-    item->mark_mask = conn->offloads.dir_info[dir].pkt_ct_mark[1];
-    item->label_key = conn->offloads.dir_info[dir].pkt_ct_label[0];
-    item->label_mask = conn->offloads.dir_info[dir].pkt_ct_label[1];
+    item->mark_key = conn->offloads.dir_info[dir].pkt_ct_mark;
+    item->label_key = conn->offloads.dir_info[dir].pkt_ct_label;
     item->timestamp = now;
 }
 
@@ -328,9 +326,7 @@ process_one_ct_offload(struct conntrack *ct,
                        struct dp_packet *packet,
                        struct conn *conn,
                        bool reply,
-                       long long now_us,
-                       uint32_t orig_mark,
-                       ovs_u128 orig_label)
+                       long long now_us)
 {
     if (!conn || (conn->key.nw_proto != IPPROTO_UDP &&
                   conn->key.nw_proto != IPPROTO_TCP) ||
@@ -340,9 +336,6 @@ process_one_ct_offload(struct conntrack *ct,
 
     if (netdev_is_flow_api_enabled() &&
         !(conn->offloads.flags & CT_OFFLOAD_SKIP)) {
-        ovs_u128 updated_label_bits = ovs_u128_xor(packet->md.ct_label,
-                                                   orig_label);
-        uint32_t updated_mark_bits = packet->md.ct_mark ^ orig_mark;
         int dir = reply ? CT_DIR_REP : CT_DIR_INIT;
         struct conn *actual_conn = conn;
 
@@ -354,15 +347,8 @@ process_one_ct_offload(struct conntrack *ct,
             actual_conn = conn->master_conn;
         }
 
-        actual_conn->offloads.dir_info[dir].pkt_ct_mark[0] =
-            packet->md.ct_mark;
-        actual_conn->offloads.dir_info[dir].pkt_ct_mark[1] =
-            updated_mark_bits;
-
-        actual_conn->offloads.dir_info[dir].pkt_ct_label[0] =
-            packet->md.ct_label;
-        actual_conn->offloads.dir_info[dir].pkt_ct_label[1] =
-            updated_label_bits;
+        actual_conn->offloads.dir_info[dir].pkt_ct_mark = packet->md.ct_mark;
+        actual_conn->offloads.dir_info[dir].pkt_ct_label = packet->md.ct_label;
 
         conntrack_offload_add_conn(ct, packet, conn, reply, now_us);
     }

lib/conntrack-offload.h
@@ -39,9 +39,7 @@ struct ct_flow_offload_item {
     /* actions */
     uint8_t ct_state;
     ovs_u128 label_key;
-    ovs_u128 label_mask;
     uint32_t mark_key;
-    uint32_t mark_mask;
 
     /* Pre-created CT actions */
     bool ct_actions_set;
@@ -76,9 +74,7 @@ process_one_ct_offload(struct conntrack *ct,
                        struct dp_packet *packet,
                        struct conn *conn,
                        bool reply,
-                       long long now_us,
-                       uint32_t orig_mark,
-                       ovs_u128 orig_label);
+                       long long now_us);
 int
 conn_hw_update(struct conntrack *ct,
                struct conntrack_offload_class *offload_class,

lib/conntrack-private.h
@@ -130,8 +130,8 @@ struct ct_dir_info {
     ovs_u128 ufid;
     void *dp;
     uint8_t pkt_ct_state;
-    uint32_t pkt_ct_mark[2];
-    ovs_u128 pkt_ct_label[2];
+    uint32_t pkt_ct_mark;
+    ovs_u128 pkt_ct_label;
     bool e2e_flow;
     uint8_t e2e_seen_pkts;
 };

lib/conntrack.c
@@ -1547,8 +1547,6 @@ conntrack_execute(struct conntrack *ct, struct dp_packet_batch *pkt_batch,
 
     DP_PACKET_BATCH_FOR_EACH (i, packet, pkt_batch) {
         struct conn *conn = packet->md.conn;
-        uint32_t orig_mark = packet->md.ct_mark;
-        ovs_u128 orig_label = packet->md.ct_label;
 
         ctx.conn = NULL;
         if (OVS_UNLIKELY(packet->md.ct_state == CS_INVALID)) {
@@ -1567,8 +1565,7 @@ conntrack_execute(struct conntrack *ct, struct dp_packet_batch *pkt_batch,
                         tp_id);
         }
         conn = packet->md.conn ? packet->md.conn : ctx.conn;
-        process_one_ct_offload(ct, packet, conn, ctx.reply, now_us, orig_mark,
-                               orig_label);
+        process_one_ct_offload(ct, packet, conn, ctx.reply, now_us);
     }
 
     ipf_postprocess_conntrack(ct->ipf, pkt_batch, now_ms, dl_type);

lib/dpif-netdev.c
@@ -3737,6 +3737,20 @@ dp_netdev_fill_ct_match(struct match *match, const struct ct_match *ct_match)
 }
 
 static void
+dp_netdev_set_ct_mark_labels_attr(struct ofpbuf *buf,
+                                  uint16_t attr,
+                                  void *offload_key,
+                                  size_t size)
+{
+    uint8_t *key, *mask;
+
+    key = nl_msg_put_unspec_zero(buf, attr, 2 * size);
+    mask = key + size;
+    memcpy(key, offload_key, size);
+    memset(mask, 0xFF, size);
+}
+
+static void
 dp_netdev_create_ct_actions(struct ofpbuf *buf,
                             struct ct_flow_offload_item *offload)
 {
@@ -3818,24 +3832,10 @@ dp_netdev_create_ct_actions(struct ofpbuf *buf,
         nl_msg_end_nested(buf, offset);
     }
     offset = nl_msg_start_nested(buf, OVS_ACTION_ATTR_CT);
-    if (offload->mark_mask) {
-        uint32_t *mark_key, *mark_mask;
-
-        mark_key = nl_msg_put_unspec_zero(buf, OVS_CT_ATTR_MARK,
-                                          2 * sizeof *mark_key);
-        mark_mask = mark_key + 1;
-        *mark_key = offload->mark_key;
-        *mark_mask = offload->mark_mask;
-    }
-    if (!is_all_zeros(&offload->label_mask, sizeof offload->label_mask)) {
-        ovs_u128 *labels_key, *labels_mask;
-
-        labels_key = nl_msg_put_unspec_zero(buf, OVS_CT_ATTR_LABELS,
-                                            2 * sizeof *labels_key);
-        labels_mask = labels_key + 1;
-        *labels_key = offload->label_key;
-        *labels_mask = offload->label_mask;
-    }
+    dp_netdev_set_ct_mark_labels_attr(buf, OVS_CT_ATTR_MARK,
+                                      &offload->mark_key, sizeof(uint32_t));
+    dp_netdev_set_ct_mark_labels_attr(buf, OVS_CT_ATTR_LABELS,
+                                      &offload->label_key, sizeof(ovs_u128));
     nl_msg_put_u16(buf, OVS_CT_ATTR_ZONE, offload->ct_match.key.zone);
 
     end = helper;

lib/netdev-offload-dpdk.c
@@ -4864,10 +4864,12 @@ parse_ct_actions(struct netdev *netdev,
                 set_value = act_resources->ct_action_label_id;
                 set_mask = reg_fields[REG_FIELD_CT_LABEL_ID].mask;
             } else {
-                if (key->u32[1] & mask->u32[1] ||
-                    key->u32[2] & mask->u32[2] ||
-                    key->u32[3] & mask->u32[3]) {
-                    return -1;
+                if (!act_vars->is_ct_conn) {
+                    if (key->u32[1] & mask->u32[1] ||
+                        key->u32[2] & mask->u32[2] ||
+                        key->u32[3] & mask->u32[3]) {
+                        return -1;
+                    }
                 }
                 set_value = key->u32[0] & mask->u32[0];
                 set_mask = mask->u32[0];