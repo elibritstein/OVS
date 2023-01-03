elibritstein/OVS

commit 837c4dafac43d0d194f8006ff0ed86b6194bee5c

netdev-offload-dpdk: Introduce get/put shared_ctx
Issue: 2971531
Change-Id: Ib0b7872e91d52215e0db77fa9f51b6a6880ae145
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -57,6 +57,11 @@ static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(600, 600);
  * A mapping from ufid to dpdk rte_flow.
  */
 
+struct shared_age_ctx {
+    struct rte_flow_action_handle *act_hdl;
+    struct netdev *netdev;
+};
+
 struct act_resources {
     uint32_t next_table_id;
     uint32_t self_table_id;
@@ -1449,6 +1454,130 @@ static int
 disassociate_flow_id(uint32_t flow_id)
 {
     return disassociate_id_data(&flow_miss_ctx_md, flow_id);
+}
+
+static struct context_metadata shared_age_md = {
+    .name = "shared-age",
+    .maps_lock = OVS_MUTEX_INITIALIZER,
+    .d2i_map = CMAP_INITIALIZER,
+    .data_size = sizeof(uintptr_t),
+    .priv_size = sizeof(struct shared_age_ctx),
+};
+
+OVS_UNUSED
+static struct shared_age_ctx **
+get_shared_age_ctx(struct netdev *netdev,
+                   uintptr_t app_counter_id,
+                   bool create)
+{
+    struct context_metadata *md = &shared_age_md;
+    struct rte_flow_action_age age_conf = {
+        .timeout = 0xFFFFFF,
+    };
+    struct rte_flow_action action = {
+        .type = RTE_FLOW_ACTION_TYPE_AGE,
+        .conf = &age_conf,
+    };
+    struct context_data *data_cur;
+    struct rte_flow_error error;
+    struct shared_age_ctx *ctx;
+    size_t data_size;
+    size_t dhash;
+
+    dhash = hash_bytes(&app_counter_id, sizeof app_counter_id, 0);
+    CMAP_FOR_EACH_WITH_HASH (data_cur, d2i_node, dhash, &md->d2i_map) {
+        if (app_counter_id == *((uintptr_t *) data_cur->data)) {
+            if (!context_data_ref(md, data_cur, dhash)) {
+                /* If a reference could not be taken, it means that
+                 * while the data has been found within the map, it has
+                 * since been removed and related ID freed. At this point,
+                 * allocate a new data node altogether. */
+                break;
+            }
+            return (struct shared_age_ctx **) &data_cur->priv;
+        }
+    }
+
+    if (!create) {
+        return NULL;
+    }
+
+    data_cur = xzalloc(sizeof *data_cur);
+    if (!data_cur) {
+        return NULL;
+    }
+    data_size = ROUND_UP(md->data_size, 8);
+    data_cur->data = xmalloc(data_size + md->priv_size);
+    if (!data_cur->data) {
+        goto err_data_alloc;
+    }
+    data_cur->priv = (uint8_t *) data_cur->data + data_size;
+    ctx = data_cur->priv;
+    ctx->act_hdl = netdev_dpdk_indirect_action_create(netdev, &action, &error);
+    if (ctx->act_hdl == NULL) {
+        goto err_indir;
+    }
+    ctx->netdev = netdev;
+
+    *((uintptr_t *) data_cur->data) = app_counter_id;
+    ovs_refcount_init(&data_cur->refcount);
+    ovs_mutex_lock(&md->maps_lock);
+    data_cur->d2i_hash = dhash;
+    cmap_insert(&md->d2i_map, &data_cur->d2i_node, dhash);
+    ovs_mutex_unlock(&md->maps_lock);
+
+    return (struct shared_age_ctx **) &data_cur->priv;
+
+err_indir:
+    free(data_cur->data);
+err_data_alloc:
+    free(data_cur);
+    return NULL;
+}
+
+static void
+context_data_unref(struct context_data *data)
+{
+    free(data->data);
+    free(data);
+}
+
+OVS_UNUSED
+static void
+put_shared_age_ctx(struct shared_age_ctx **pctx)
+{
+    struct context_metadata *md = &shared_age_md;
+    struct rte_flow_error error;
+    struct shared_age_ctx *ctx;
+    struct context_data *data;
+
+    if (pctx == NULL) {
+        return;
+    }
+
+    data = CONTAINER_OF(pctx, struct context_data, priv);
+    ctx = *pctx;
+
+    ovs_mutex_lock(&md->maps_lock);
+
+    if (ovs_refcount_unref(&data->refcount) > 1) {
+        /* Data has been referenced again since delayed release request. */
+        goto out;
+    }
+
+    cmap_remove(&md->d2i_map, &data->d2i_node, data->d2i_hash);
+    if (ctx->netdev == NULL || ctx->act_hdl == NULL) {
+        goto err;
+    }
+
+    netdev_dpdk_indirect_action_destroy(ctx->netdev, ctx->act_hdl, &error);
+    ctx->netdev = NULL;
+    ctx->act_hdl = NULL;
+
+err:
+    ovsrcu_postpone(context_data_unref, data);
+out:
+    ovs_mutex_unlock(&md->maps_lock);
 }
 
 static void

commit 411a685c62f43c3aafae03b3325f37e93293dd68

netdev-offload-dpdk: Use dpdk port id for indirect destroy/query
Indirect actions are shared between different ports.
It is possible that the port on which the action was first created on is
not valid anymore when trying to destroy it, which can lead to a crash.

As those objects are shared, they are managed on the ESW manager port
anyway - both for creation/query/destroy. Keep the ESW manager dpdk port
id, which guaranteed to be valid for that lifetime, and use if for
destroy/query of those objects.

Issue: OVS-3314021
Fixes: 837c4dafac43 ("netdev-offload-dpdk: Introduce get/put shared_ctx")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-dpdk.c
@@ -5671,42 +5671,25 @@ netdev_dpdk_indirect_action_create(struct netdev *netdev,
 }
 
 int
-netdev_dpdk_indirect_action_destroy(struct netdev *netdev,
+netdev_dpdk_indirect_action_destroy(int port_id,
                                     struct rte_flow_action_handle *act_hdl,
                                     struct rte_flow_error *error)
 {
-    struct netdev_dpdk *dev;
     int ret;
 
-    if (!is_dpdk_class(netdev->netdev_class)) {
-        return -1;
-    }
-
-    dev = netdev_dpdk_cast(netdev);
-    ovs_mutex_lock(&dev->mutex);
-    ret = rte_flow_action_handle_destroy(dev->esw_mgr_port_id, act_hdl, error);
-    ovs_mutex_unlock(&dev->mutex);
+    ret = rte_flow_action_handle_destroy(port_id, act_hdl, error);
     return ret;
 }
 
 int
-netdev_dpdk_indirect_action_query(struct netdev *netdev,
+netdev_dpdk_indirect_action_query(int port_id,
                                   struct rte_flow_action_handle *act_hdl,
                                   void *data,
                                   struct rte_flow_error *error)
 {
-    struct netdev_dpdk *dev;
     int ret;
 
-    if (!is_dpdk_class(netdev->netdev_class)) {
-        return -1;
-    }
-
-    dev = netdev_dpdk_cast(netdev);
-    ovs_mutex_lock(&dev->mutex);
-    ret = rte_flow_action_handle_query(dev->esw_mgr_port_id, act_hdl, data,
-                                       error);
-    ovs_mutex_unlock(&dev->mutex);
+    ret = rte_flow_action_handle_query(port_id, act_hdl, data, error);
     return ret;
 }
 

lib/netdev-dpdk.h
@@ -155,11 +155,11 @@ netdev_dpdk_indirect_action_create(struct netdev *,
                                    const struct rte_flow_action *,
                                    struct rte_flow_error *);
 int
-netdev_dpdk_indirect_action_destroy(struct netdev *,
+netdev_dpdk_indirect_action_destroy(int port_id,
                                     struct rte_flow_action_handle *,
                                     struct rte_flow_error *);
 int
-netdev_dpdk_indirect_action_query(struct netdev *,
+netdev_dpdk_indirect_action_query(int port_id,
                                   struct rte_flow_action_handle *,
                                   void *,
                                   struct rte_flow_error *);

lib/netdev-offload-dpdk.c
@@ -64,7 +64,7 @@ static bool netdev_offload_dpdk_disable_zone_tables = false;
 
 struct indirect_ctx {
     struct rte_flow_action_handle *act_hdl;
-    struct netdev *netdev;
+    int port_id;
 };
 
 struct per_thread {
@@ -1739,9 +1739,10 @@ get_indirect_ctx(struct netdev *netdev,
     ctx = data_cur->priv;
     ctx->act_hdl = netdev_dpdk_indirect_action_create(netdev, action, &error);
     if (ctx->act_hdl == NULL) {
+        ctx->port_id = -1;
         goto err_indir;
     }
-    ctx->netdev = netdev;
+    ctx->port_id = netdev_dpdk_get_esw_mgr_port_id(netdev);
 
     memcpy(data_cur->data, key, md->data_size);
     ovs_refcount_init(&data_cur->refcount);
@@ -1802,14 +1803,14 @@ put_indirect_ctx(struct context_metadata *md, struct indirect_ctx **pctx)
     }
 
     cmap_remove(&md->d2i_map, &data->d2i_node, data->d2i_hash);
-    if (ctx->netdev == NULL || ctx->act_hdl == NULL) {
-        VLOG_ERR_RL(&rl, "%s: %s: invalid ctx: netdev=%p, ctx_hdl=%p",
-                    __func__, md->name, ctx->netdev, ctx->act_hdl);
+    if (ctx->port_id == -1 || ctx->act_hdl == NULL) {
+        VLOG_ERR_RL(&rl, "%s: %s: invalid ctx: port_id=%d, ctx_hdl=%p",
+                    __func__, md->name, ctx->port_id, ctx->act_hdl);
         goto err;
     }
 
-    netdev_dpdk_indirect_action_destroy(ctx->netdev, ctx->act_hdl, &error);
-    ctx->netdev = NULL;
+    netdev_dpdk_indirect_action_destroy(ctx->port_id, ctx->act_hdl, &error);
+    ctx->port_id = -1;
     ctx->act_hdl = NULL;
 
 err:
@@ -1834,8 +1835,8 @@ dump_shared_age(struct ds *s, void *data)
                       *((uintptr_t *) did->key), did->create);
     }
     if (did->ctx) {
-        ds_put_format(s, "ctx->netdev=%s, ctx->act_hdl=%p",
-                      netdev_get_name(did->ctx->netdev), did->ctx->act_hdl);
+        ds_put_format(s, "ctx->port_id=%d, ctx->act_hdl=%p",
+                      did->ctx->port_id, did->ctx->act_hdl);
     } else {
         ds_put_cstr(s, "ctx=NULL");
     }
@@ -1891,8 +1892,8 @@ dump_shared_count(struct ds *s, void *data)
         }
     }
     if (did->ctx) {
-        ds_put_format(s, "ctx->netdev=%s, ctx->act_hdl=%p",
-                      netdev_get_name(did->ctx->netdev), did->ctx->act_hdl);
+        ds_put_format(s, "ctx->port_id=%d, ctx->act_hdl=%p",
+                      did->ctx->port_id, did->ctx->act_hdl);
     } else {
         ds_put_cstr(s, "ctx=NULL");
     }
@@ -5922,7 +5923,7 @@ netdev_offload_dpdk_flow_get(struct netdev *netdev,
                                            rte_flow, &query, &error);
     } else {
         ctx = *rte_flow_data->act_resources.pshared_count_ctx;
-        ret = netdev_dpdk_indirect_action_query(ctx->netdev, ctx->act_hdl,
+        ret = netdev_dpdk_indirect_action_query(ctx->port_id, ctx->act_hdl,
                                                 &query, &error);
     }
     if (ret) {
@@ -6264,7 +6265,7 @@ netdev_offload_dpdk_ct_counter_query(struct netdev *netdev,
     }
     ctx = *pctx;
 
-    ret = netdev_dpdk_indirect_action_query(ctx->netdev, ctx->act_hdl,
+    ret = netdev_dpdk_indirect_action_query(ctx->port_id, ctx->act_hdl,
                                             &query_age, &error);
     if (!ret && query_age.sec_since_last_hit_valid &&
         (query_age.sec_since_last_hit * 1000) <= (now - prev_now)) {