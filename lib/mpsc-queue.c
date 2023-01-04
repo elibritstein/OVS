elibritstein/OVS

commit 5396ba5b21c4eae9726f4726786e53d317ccfdce

mpsc-queue: Module for lock-free message passing.
Add a lockless multi-producer/single-consumer (MPSC), linked-list based,
intrusive, unbounded queue that does not require deferred memory
management.

The queue is designed to improve the specific MPSC setup.  A benchmark
accompanies the unit tests to measure the difference in this configuration.
A single reader thread polls the queue while N writers enqueue elements
as fast as possible.  The mpsc-queue is compared against the regular ovs-list
as well as the guarded list.  The latter usually offers a slight improvement
by batching the element removal, however the mpsc-queue is faster.

The average is of each producer threads time:

   $ ./tests/ovstest test-mpsc-queue benchmark 3000000 1
   Benchmarking n=3000000 on 1 + 1 threads.
    type\thread:  Reader      1    Avg
     mpsc-queue:     167    167    167 ms
     list(spin):      89     80     80 ms
    list(mutex):     745    745    745 ms
   guarded list:     788    788    788 ms

   $ ./tests/ovstest test-mpsc-queue benchmark 3000000 2
   Benchmarking n=3000000 on 1 + 2 threads.
    type\thread:  Reader      1      2    Avg
     mpsc-queue:      98     97     94     95 ms
     list(spin):     185    171    173    172 ms
    list(mutex):     203    199    203    201 ms
   guarded list:     269    269    188    228 ms

   $ ./tests/ovstest test-mpsc-queue benchmark 3000000 3
   Benchmarking n=3000000 on 1 + 3 threads.
    type\thread:  Reader      1      2      3    Avg
     mpsc-queue:      76     76     65     76     72 ms
     list(spin):     246    110    240    238    196 ms
    list(mutex):     542    541    541    539    540 ms
   guarded list:     535    535    507    511    517 ms

   $ ./tests/ovstest test-mpsc-queue benchmark 3000000 4
   Benchmarking n=3000000 on 1 + 4 threads.
    type\thread:  Reader      1      2      3      4    Avg
     mpsc-queue:      73     68     68     68     68     68 ms

lib/mpsc-queue.c
@@ -1,0 +1,251 @@
+/*
+ * Copyright (c) 2021 NVIDIA Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include "ovs-atomic.h"
+
+#include "mpsc-queue.h"
+
+/* Multi-producer, single-consumer queue
+ * =====================================
+ *
+ * This an implementation of the MPSC queue described by Dmitri Vyukov [1].
+ *
+ * One atomic exchange operation is done per insertion.  Removal in most cases
+ * will not require atomic operation and will use one atomic exchange to close
+ * the queue chain.
+ *
+ * Insertion
+ * =========
+ *
+ * The queue is implemented using a linked-list.  Insertion is done at the
+ * back of the queue, by swapping the current end with the new node atomically,
+ * then pointing the previous end toward the new node.  To follow Vyukov
+ * nomenclature, the end-node of the chain is called head.  A producer will
+ * only manipulate the head.
+ *
+ * The head swap is atomic, however the link from the previous head to the new
+ * one is done in a separate operation.  This means that the chain is
+ * momentarily broken, when the previous head still points to NULL and the
+ * current head has been inserted.
+ *
+ * Considering a series of insertions, the queue state will remain consistent
+ * and the insertions order is compatible with their precedence, thus the
+ * queue is serializable.  However, because an insertion consists in two
+ * separate memory transactions, it is not linearizable.
+ *
+ * Removal
+ * =======
+ *
+ * The consumer must deal with the queue inconsistency.  It will manipulate
+ * the tail of the queue and move it along the latest consumed elements.
+ * When an end of the chain of elements is found (the next pointer is NULL),
+ * the tail is compared with the head.
+ *
+ * If both points to different addresses, then the queue is in an inconsistent
+ * state: the tail cannot move forward as the next is NULL, but the head is not
+ * the last element in the chain: this can only happen if the chain is broken.
+ *
+ * In this case, the consumer must wait for the producer to finish writing the
+ * next pointer of its current tail: 'MPSC_QUEUE_RETRY' is returned.
+ *
+ * Removal is thus in most cases (when there are elements in the queue)
+ * accomplished without using atomics, until the last element of the queue.
+ * There, the head is atomically loaded. If the queue is in a consistent state,
+ * the head is moved back to the queue stub by inserting the stub in the queue:
+ * ending the queue is the same as an insertion, which is one atomic XCHG.
+ *
+ * Forward guarantees
+ * ==================
+ *
+ * Insertion and peeking are wait-free: they will execute in a known bounded
+ * number of instructions, regardless of the state of the queue.
+ *
+ * However, while removal consists in peeking and a constant write to
+ * update the tail, it can repeatedly fail until the queue become consistent.
+ * It is thus dependent on other threads progressing.  This means that the
+ * queue forward progress is obstruction-free only.  It has a potential for
+ * livelocking.
+ *
+ * The chain will remain broken as long as a producer is not finished writing
+ * its next pointer.  If a producer is cancelled for example, the queue could
+ * remain broken for any future readings.  This queue should either be used
+ * with cooperative threads or insertion must only be done outside cancellable
+ * sections.
+ *
+ * Performances
+ * ============
+ *
+ * In benchmarks this structure was better than alternatives such as:
+ *
+ *   * A reversed Treiber stack [2], using 1 CAS per operations
+ *     and requiring reversal of the node list on removal.
+ *
+ *   * Michael-Scott lock-free queue [3], using 2 CAS per operations.
+ *
+ * While it is not linearizable, this queue is well-suited for message passing.
+ * If a proper hardware XCHG operation is used, it scales better than
+ * CAS-based implementations.
+ *
+ * References
+ * ==========
+ *
+ * [1]: http://www.1024cores.net/home/lock-free-algorithms/queues/intrusive-mpsc-node-based-queue
+ *
+ * [2]: R. K. Treiber. Systems programming: Coping with parallelism.
+ *      Technical Report RJ 5118, IBM Almaden Research Center, April 1986.
+ *
+ * [3]: M. M. Michael, Simple, Fast, and Practical Non-Blocking and
+ *      Blocking Concurrent Queue Algorithms
+ * [3]: https://www.cs.rochester.edu/research/synchronization/pseudocode/queues.html
+ *
+ */
+
+void
+mpsc_queue_init(struct mpsc_queue *queue)
+{
+    atomic_store_relaxed(&queue->head, &queue->stub);
+    atomic_store_relaxed(&queue->tail, &queue->stub);
+    atomic_store_relaxed(&queue->stub.next, NULL);
+
+    ovs_mutex_init(&queue->read_lock);
+}
+
+void
+mpsc_queue_destroy(struct mpsc_queue *queue)
+    OVS_EXCLUDED(queue->read_lock)
+{
+    ovs_mutex_destroy(&queue->read_lock);
+}
+
+enum mpsc_queue_poll_result
+mpsc_queue_poll(struct mpsc_queue *queue, struct mpsc_queue_node **node)
+    OVS_REQUIRES(queue->read_lock)
+{
+    struct mpsc_queue_node *tail;
+    struct mpsc_queue_node *next;
+    struct mpsc_queue_node *head;
+
+    atomic_read_relaxed(&queue->tail, &tail);
+    atomic_read_explicit(&tail->next, &next, memory_order_acquire);
+
+    if (tail == &queue->stub) {
+        if (next == NULL) {
+            return MPSC_QUEUE_EMPTY;
+        }
+
+        atomic_store_relaxed(&queue->tail, next);
+        tail = next;
+        atomic_read_explicit(&tail->next, &next, memory_order_acquire);
+    }
+
+    if (next != NULL) {
+        atomic_store_relaxed(&queue->tail, next);
+        *node = tail;
+        return MPSC_QUEUE_ITEM;
+    }
+
+    atomic_read_explicit(&queue->head, &head, memory_order_acquire);
+    if (tail != head) {
+        return MPSC_QUEUE_RETRY;
+    }
+
+    mpsc_queue_insert(queue, &queue->stub);
+
+    atomic_read_explicit(&tail->next, &next, memory_order_acquire);
+    if (next != NULL) {
+        atomic_store_relaxed(&queue->tail, next);
+        *node = tail;
+        return MPSC_QUEUE_ITEM;
+    }
+
+    return MPSC_QUEUE_EMPTY;
+}
+
+struct mpsc_queue_node *
+mpsc_queue_pop(struct mpsc_queue *queue)
+    OVS_REQUIRES(queue->read_lock)
+{
+    enum mpsc_queue_poll_result result;
+    struct mpsc_queue_node *node;
+
+    do {
+        result = mpsc_queue_poll(queue, &node);
+        if (result == MPSC_QUEUE_EMPTY) {
+            return NULL;
+        }
+    } while (result == MPSC_QUEUE_RETRY);
+
+    return node;
+}
+
+void
+mpsc_queue_push_front(struct mpsc_queue *queue, struct mpsc_queue_node *node)
+    OVS_REQUIRES(queue->read_lock)
+{
+    struct mpsc_queue_node *tail;
+
+    atomic_read_relaxed(&queue->tail, &tail);
+    atomic_store_relaxed(&node->next, tail);
+    atomic_store_relaxed(&queue->tail, node);
+}
+
+struct mpsc_queue_node *
+mpsc_queue_tail(struct mpsc_queue *queue)
+    OVS_REQUIRES(queue->read_lock)
+{
+    struct mpsc_queue_node *tail;
+    struct mpsc_queue_node *next;
+
+    atomic_read_relaxed(&queue->tail, &tail);
+    atomic_read_explicit(&tail->next, &next, memory_order_acquire);
+
+    if (tail == &queue->stub) {
+        if (next == NULL) {
+            return NULL;
+        }
+
+        atomic_store_relaxed(&queue->tail, next);
+        tail = next;
+    }
+
+    return tail;
+}
+
+/* Get the next element of a node. */
+struct mpsc_queue_node *mpsc_queue_next(struct mpsc_queue *queue,
+                                        struct mpsc_queue_node *prev)
+    OVS_REQUIRES(queue->read_lock)
+{
+    struct mpsc_queue_node *next;
+
+    atomic_read_explicit(&prev->next, &next, memory_order_acquire);
+    if (next == &queue->stub) {
+        atomic_read_explicit(&next->next, &next, memory_order_acquire);
+    }
+    return next;
+}
+
+void
+mpsc_queue_insert(struct mpsc_queue *queue, struct mpsc_queue_node *node)
+{
+    struct mpsc_queue_node *prev;
+
+    atomic_store_relaxed(&node->next, NULL);
+    prev = atomic_exchange_explicit(&queue->head, node, memory_order_acq_rel);
+    atomic_store_explicit(&prev->next, node, memory_order_release);
+}

lib/automake.mk
@@ -182,6 +182,8 @@ lib_libopenvswitch_la_SOURCES = \
 	lib/memory.h \
 	lib/meta-flow.c \
 	lib/mov-avg.h \
+	lib/mpsc-queue.c \
+	lib/mpsc-queue.h \
 	lib/multipath.c \
 	lib/multipath.h \
 	lib/namemap.c \

lib/mpsc-queue.h
@@ -1,0 +1,190 @@
+/*
+ * Copyright (c) 2021 NVIDIA Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MPSC_QUEUE_H
+#define MPSC_QUEUE_H 1
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#include <openvswitch/thread.h>
+#include <openvswitch/util.h>
+
+#include "ovs-atomic.h"
+
+/* Multi-producer, single-consumer queue
+ * =====================================
+ *
+ * This data structure is a lockless queue implementation with
+ * the following properties:
+ *
+ *  * Multi-producer: multiple threads can write concurrently.
+ *    Insertion in the queue is thread-safe, no inter-thread
+ *    synchronization is necessary.
+ *
+ *  * Single-consumer: only a single thread can safely remove
+ *    nodes from the queue.  The queue must be 'acquired' using
+ *    'mpsc_queue_acquire()' before removing nodes.
+ *
+ *  * Unbounded: the queue is backed by a linked-list and is not
+ *    limited in number of elements.
+ *
+ *  * Intrusive: queue elements are allocated as part of larger
+ *    objects.  Objects are retrieved by offset manipulation.
+ *
+ *  * per-producer FIFO: Elements in the queue are kept in the
+ *    order their producer inserted them.  The consumer retrieves
+ *    them in in the same insertion order.  When multiple
+ *    producers insert at the same time, either will proceed.
+ *
+ * This queue is well-suited for message passing between threads,
+ * where any number of thread can insert a message and a single
+ * thread is meant to receive and process it.
+ *
+ * Thread-safety
+ * =============
+ *
+ *  The consumer thread must acquire the queue using 'mpsc_queue_acquire()'.
+ *  Once the queue is protected against concurrent reads, the thread can call
+ *  the consumer API:
+ *
+ *      * mpsc_queue_poll() to peek and return the tail of the queue
+ *      * mpsc_queue_pop() to remove the tail of the queue
+ *      * mpsc_queue_tail() to read the current tail
+ *      * mpsc_queue_push_front() to enqueue an element safely at the tail
+ *      * MPSC_QUEUE_FOR_EACH() to iterate over the current elements,
+ *        without removing them.
+ *      * MPSC_QUEUE_FOR_EACH_POP() to iterate over the elements while
+ *        removing them.
+ *
+ *  When a thread is finished with reading the queue, it can release the
+ *  reader lock using 'mpsc_queue_release()'.
+ *
+ *  Producers can always insert elements in the queue, even if no consumer
+ *  acquired the reader lock.  No inter-producer synchronization is needed.
+ *
+ *  The consumer thread is also allowed to insert elements while it holds the
+ *  reader lock.
+ *
+ *  Producer threads must never be cancelled while writing to the queue.
+ *  This will block the consumer, that will then lose any subsequent elements
+ *  in the queue.  Producers should ideally be cooperatively managed or
+ *  the queue insertion should be within non-cancellable sections.
+ *
+ * Queue state
+ * ===========
+ *
+ *  When polling the queue, three states can be observed: 'empty', 'non-empty',
+ *  and 'inconsistent'.  Three polling results are defined, respectively:
+ *
+ *   * MPSC_QUEUE_EMPTY: the queue is empty.
+ *   * MPSC_QUEUE_ITEM: an item was available and has been removed.
+ *   * MPSC_QUEUE_RETRY: the queue is inconsistent.
+ *
+ *  If 'MPSC_QUEUE_RETRY' is returned, then a producer has not yet finished
+ *  writing to the queue and the list of nodes is not coherent.  The consumer
+ *  can retry shortly to check if the producer has finished.
+ *
+ *  This behavior is the reason the removal function is called
+ *  'mpsc_queue_poll()'.
+ *
+ */
+
+struct mpsc_queue_node {
+    ATOMIC(struct mpsc_queue_node *) next;
+};
+
+struct mpsc_queue {
+    ATOMIC(struct mpsc_queue_node *) head;
+    ATOMIC(struct mpsc_queue_node *) tail;
+    struct mpsc_queue_node stub;
+    struct ovs_mutex read_lock;
+};
+
+#define MPSC_QUEUE_INITIALIZER(Q) { \
+    .head = ATOMIC_VAR_INIT(&(Q)->stub), \
+    .tail = ATOMIC_VAR_INIT(&(Q)->stub), \
+    .stub = { .next = ATOMIC_VAR_INIT(NULL) }, \
+    .read_lock = OVS_MUTEX_INITIALIZER, \
+}
+
+/* Consumer API. */
+
+/* Initialize the queue. Not necessary is 'MPSC_QUEUE_INITIALIZER' was used. */
+void mpsc_queue_init(struct mpsc_queue *queue);
+/* The reader lock must be released prior to destroying the queue. */
+void mpsc_queue_destroy(struct mpsc_queue *queue);
+
+/* Acquire and release the consumer lock. */
+#define mpsc_queue_acquire(q) do { \
+        ovs_mutex_lock(&(q)->read_lock); \
+    } while (0)
+#define mpsc_queue_release(q) do { \
+        ovs_mutex_unlock(&(q)->read_lock); \
+    } while (0)
+
+enum mpsc_queue_poll_result {
+    /* Queue is empty. */
+    MPSC_QUEUE_EMPTY,
+    /* Polling the queue returned an item. */
+    MPSC_QUEUE_ITEM,
+    /* Data has been enqueued but one or more producer thread have not
+     * finished writing it. The queue is in an inconsistent state.
+     * Retrying shortly, if the producer threads are still active, will
+     * return the data.
+     */
+    MPSC_QUEUE_RETRY,
+};
+
+/* Set 'node' to a removed item from the queue if 'MPSC_QUEUE_ITEM' is
+ * returned, otherwise 'node' is not set.
+ */
+enum mpsc_queue_poll_result mpsc_queue_poll(struct mpsc_queue *queue,
+                                            struct mpsc_queue_node **node)
+    OVS_REQUIRES(queue->read_lock);
+
+/* Pop an element if there is any in the queue. */
+struct mpsc_queue_node *mpsc_queue_pop(struct mpsc_queue *queue)
+    OVS_REQUIRES(queue->read_lock);
+
+/* Insert at the front of the queue. Only the consumer can do it. */
+void mpsc_queue_push_front(struct mpsc_queue *queue,
+                           struct mpsc_queue_node *node)
+    OVS_REQUIRES(queue->read_lock);
+
+/* Get the current queue tail. */
+struct mpsc_queue_node *mpsc_queue_tail(struct mpsc_queue *queue)
+    OVS_REQUIRES(queue->read_lock);
+
+/* Get the next element of a node. */
+struct mpsc_queue_node *mpsc_queue_next(struct mpsc_queue *queue,
+                                        struct mpsc_queue_node *prev)
+    OVS_REQUIRES(queue->read_lock);
+
+#define MPSC_QUEUE_FOR_EACH(node, queue) \
+    for (node = mpsc_queue_tail(queue); node != NULL; \
+         node = mpsc_queue_next((queue), node))
+
+#define MPSC_QUEUE_FOR_EACH_POP(node, queue) \
+    for (node = mpsc_queue_pop(queue); node != NULL; \
+         node = mpsc_queue_pop(queue))
+
+/* Producer API. */
+
+void mpsc_queue_insert(struct mpsc_queue *queue, struct mpsc_queue_node *node);
+
+#endif /* MPSC_QUEUE_H */

tests/automake.mk
@@ -472,6 +472,7 @@ tests_ovstest_SOURCES = \
 	tests/test-list.c \
 	tests/test-lockfile.c \
 	tests/test-multipath.c \
+	tests/test-mpsc-queue.c \
 	tests/test-netflow.c \
 	tests/test-odp.c \
 	tests/test-ofpbuf.c \

tests/library.at
@@ -265,3 +265,8 @@ AT_SKIP_IF([test "$IS_WIN32" = "yes"])
 AT_SKIP_IF([test "$IS_BSD" = "yes"])
 AT_CHECK([ovstest test-netlink-policy ll_addr], [0])
 AT_CLEANUP
+
+AT_SETUP([mpsc-queue module])
+AT_CHECK([ovstest test-mpsc-queue check], [0], [....
+])
+AT_CLEANUP

commit 2a6c0d850bc2af6480b129773fdbe52ab0894f61

mpsc-queue: Fix interrupted poll result
If a queue insertion and poll are interleaved in the following manner:

  1. Poll begins: stop right before inserting the stub back.
  2. Insert begins: stop before updating the head.
  3. Poll ends: insert the stub then return its result.
  4. Insert ends: sync the head.

Then (3) will return MPSC_QUEUE_EMPTY, while there is a node in the
process of being inserted. Instead MPSC_QUEUE_RETRY is expected.

The bug was found with the help of a formal verification tool.
Instrumentation of the mpsc-queue was done by Ran Ettinger.

Issue: OVS-3319306
Fixes: 5396ba5b21c4 ("mpsc-queue: Module for lock-free message passing.")
Reported-by: Ran Ettinger <rettinger@nvidia.com>
Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

lib/mpsc-queue.c
@@ -178,7 +178,7 @@ mpsc_queue_poll(struct mpsc_queue *queue, struct mpsc_queue_node **node)
         return MPSC_QUEUE_ITEM;
     }
 
-    return MPSC_QUEUE_EMPTY;
+    return MPSC_QUEUE_RETRY;
 }
 
 struct mpsc_queue_node *