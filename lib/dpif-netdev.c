elibritstein/OVS

commit 9d75992f92a23888d68e85bdbf6f93406c3f814b

metrics: Remove n_ from counter names
Previously, the internal style guide for metrics counters
prescribed that the entry name should start by 'n_', to
recognize a counter.

When aligning on the OpenMetrics standard, counters are
instead recognized by the '_total' suffix. Reading an entry as
'n_xxx_total' is not great, so the 'n_' is removed.

The macros designating the counter index in its set follows
the entry naming scheme, so all counter macros are also modified.

Issue: OVS-3289544
Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

lib/dpif-netdev.c
@@ -2551,11 +2551,11 @@ METRICS_COND(foreach_poll_threads, foreach_poll_threads_dbg,
              metrics_dbg_enabled);
 
 enum {
-    PMD_METRICS_N_PACKETS,
-    PMD_METRICS_N_RECIRC,
-    PMD_METRICS_N_HIT,
-    PMD_METRICS_N_MISSED,
-    PMD_METRICS_N_LOST,
+    PMD_METRICS_PACKETS,
+    PMD_METRICS_RECIRC,
+    PMD_METRICS_HIT,
+    PMD_METRICS_MISSED,
+    PMD_METRICS_LOST,
     PMD_METRICS_AVG_LOOKUPS_PER_HIT,
     PMD_METRICS_AVG_PACKETS_PER_BATCH,
     PMD_METRICS_AVG_RECIRC_PER_PACKET,
@@ -2637,11 +2637,11 @@ poll_threads_read_value(double *values, void *it)
                               (double) total_packets;
     }
 
-    values[PMD_METRICS_N_PACKETS] = stats[PMD_STAT_RECV];
-    values[PMD_METRICS_N_RECIRC] = stats[PMD_STAT_RECIRC];
-    values[PMD_METRICS_N_HIT] = n_hit;
-    values[PMD_METRICS_N_MISSED] = stats[PMD_STAT_MISS];
-    values[PMD_METRICS_N_LOST] = stats[PMD_STAT_LOST];
+    values[PMD_METRICS_PACKETS] = stats[PMD_STAT_RECV];
+    values[PMD_METRICS_RECIRC] = stats[PMD_STAT_RECIRC];
+    values[PMD_METRICS_HIT] = n_hit;
+    values[PMD_METRICS_MISSED] = stats[PMD_STAT_MISS];
+    values[PMD_METRICS_LOST] = stats[PMD_STAT_LOST];
 
     values[PMD_METRICS_AVG_LOOKUPS_PER_HIT] = lookups_per_hit;
     values[PMD_METRICS_AVG_PACKETS_PER_BATCH] = packets_per_batch;
@@ -2655,15 +2655,15 @@ poll_threads_read_value(double *values, void *it)
 
 METRICS_ENTRIES(foreach_poll_threads, poll_threads_entries,
     "poll_threads", poll_threads_read_value,
-    [PMD_METRICS_N_PACKETS] = METRICS_COUNTER(n_packets,
+    [PMD_METRICS_PACKETS] = METRICS_COUNTER(packets,
         "Number of received packets."),
-    [PMD_METRICS_N_RECIRC] = METRICS_COUNTER(n_recirculations,
+    [PMD_METRICS_RECIRC] = METRICS_COUNTER(recirculations,
         "Number of executed packet recirculations."),
-    [PMD_METRICS_N_HIT] = METRICS_COUNTER(n_hit,
+    [PMD_METRICS_HIT] = METRICS_COUNTER(hit,
         "Number of flow table matches."),
-    [PMD_METRICS_N_MISSED] = METRICS_COUNTER(n_missed,
+    [PMD_METRICS_MISSED] = METRICS_COUNTER(missed,
         "Number of flow table misses and upcall succeeded."),
-    [PMD_METRICS_N_LOST] = METRICS_COUNTER(n_lost,
+    [PMD_METRICS_LOST] = METRICS_COUNTER(lost,
         "Number of flow table misses and upcall failed."),
     [PMD_METRICS_AVG_LOOKUPS_PER_HIT] = METRICS_GAUGE(lookups_per_hit,
         "Average number of lookups per flow table hit."),

lib/dpif-metrics.c
@@ -63,38 +63,38 @@ METRICS_COLLECTION(dpif, foreach_dpif, do_foreach_dpif, "datapath");
 METRICS_COLLECTION(dpif, foreach_dpif_nolabel, do_foreach_dpif, NULL);
 
 enum {
-    DPIF_N_DESTROY,
-    DPIF_N_PORT_ADD,
-    DPIF_N_PORT_DEL,
-    DPIF_N_FLOW_FLUSH,
-    DPIF_N_FLOW_GET,
-    DPIF_N_FLOW_PUT,
-    DPIF_N_FLOW_DEL,
-    DPIF_N_EXECUTE,
-    DPIF_N_PURGE,
-    DPIF_N_EXECUTE_WITH_HELP,
-    DPIF_N_METER_SET,
-    DPIF_N_METER_GET,
-    DPIF_N_METER_DEL,
+    DPIF_DESTROY,
+    DPIF_PORT_ADD,
+    DPIF_PORT_DEL,
+    DPIF_FLOW_FLUSH,
+    DPIF_FLOW_GET,
+    DPIF_FLOW_PUT,
+    DPIF_FLOW_DEL,
+    DPIF_EXECUTE,
+    DPIF_PURGE,
+    DPIF_EXECUTE_WITH_HELP,
+    DPIF_METER_SET,
+    DPIF_METER_GET,
+    DPIF_METER_DEL,
 };
 
 static void
 dpif_read_value(double *values, void *it OVS_UNUSED)
 {
     char *names[] = {
-        [DPIF_N_DESTROY] = "dpif_destroy",
-        [DPIF_N_PORT_ADD] = "dpif_port_add",
-        [DPIF_N_PORT_DEL] = "dpif_port_del",
-        [DPIF_N_FLOW_FLUSH] = "dpif_flow_flush",
-        [DPIF_N_FLOW_GET] = "dpif_flow_get",
-        [DPIF_N_FLOW_PUT] = "dpif_flow_put",
-        [DPIF_N_FLOW_DEL] = "dpif_flow_del",
-        [DPIF_N_EXECUTE] = "dpif_execute",
-        [DPIF_N_PURGE] = "dpif_purge",
-        [DPIF_N_EXECUTE_WITH_HELP] = "dpif_execute_with_help",
-        [DPIF_N_METER_SET] = "dpif_meter_set",
-        [DPIF_N_METER_GET] = "dpif_meter_get",
-        [DPIF_N_METER_DEL] = "dpif_meter_del",
+        [DPIF_DESTROY] = "dpif_destroy",
+        [DPIF_PORT_ADD] = "dpif_port_add",
+        [DPIF_PORT_DEL] = "dpif_port_del",
+        [DPIF_FLOW_FLUSH] = "dpif_flow_flush",
+        [DPIF_FLOW_GET] = "dpif_flow_get",
+        [DPIF_FLOW_PUT] = "dpif_flow_put",
+        [DPIF_FLOW_DEL] = "dpif_flow_del",
+        [DPIF_EXECUTE] = "dpif_execute",
+        [DPIF_PURGE] = "dpif_purge",
+        [DPIF_EXECUTE_WITH_HELP] = "dpif_execute_with_help",
+        [DPIF_METER_SET] = "dpif_meter_set",
+        [DPIF_METER_GET] = "dpif_meter_get",
+        [DPIF_METER_DEL] = "dpif_meter_del",
     };
     size_t i;
 
@@ -112,31 +112,31 @@ dpif_read_value(double *values, void *it OVS_UNUSED)
 METRICS_COND(dpif, dpif_dbg, metrics_dbg_enabled);
 METRICS_ENTRIES(dpif_dbg, dpif_entries,
     "dpif", dpif_read_value,
-    [DPIF_N_DESTROY] = METRICS_COUNTER(n_destroy,
+    [DPIF_DESTROY] = METRICS_COUNTER(destroy,
         "Number of datapath deletion done."),
-    [DPIF_N_PORT_ADD] = METRICS_COUNTER(n_port_add,
+    [DPIF_PORT_ADD] = METRICS_COUNTER(port_add,
         "Number of port add operations done in all dpif."),
-    [DPIF_N_PORT_DEL] = METRICS_COUNTER(n_port_del,
+    [DPIF_PORT_DEL] = METRICS_COUNTER(port_del,
         "Number of port del operations done in all dpif."),
-    [DPIF_N_FLOW_FLUSH] = METRICS_COUNTER(n_flow_flush,
+    [DPIF_FLOW_FLUSH] = METRICS_COUNTER(flow_flush,
         "Number of flow flush operations done in all dpif."),
-    [DPIF_N_FLOW_GET] = METRICS_COUNTER(n_flow_get,
+    [DPIF_FLOW_GET] = METRICS_COUNTER(flow_get,
         "Number of flow queries done in all dpif."),
-    [DPIF_N_FLOW_PUT] = METRICS_COUNTER(n_flow_put,
+    [DPIF_FLOW_PUT] = METRICS_COUNTER(flow_put,
         "Number of flow addition or modification in all dpif."),
-    [DPIF_N_FLOW_DEL] = METRICS_COUNTER(n_flow_del,
+    [DPIF_FLOW_DEL] = METRICS_COUNTER(flow_del,
         "Number of flow deletion operations done in all dpif."),
-    [DPIF_N_EXECUTE] = METRICS_COUNTER(n_execute,
+    [DPIF_EXECUTE] = METRICS_COUNTER(execute,
         "Number of 'execute' calls made on packets in all dpif."),
-    [DPIF_N_PURGE] = METRICS_COUNTER(n_purge,
+    [DPIF_PURGE] = METRICS_COUNTER(purge,
         "Number of purge done in all dpif."),
-    [DPIF_N_EXECUTE_WITH_HELP] = METRICS_COUNTER(n_execute_with_help,
+    [DPIF_EXECUTE_WITH_HELP] = METRICS_COUNTER(execute_with_help,
         "Number of 'execute' split between userspace and dpif for all dpif."),
-    [DPIF_N_METER_SET] = METRICS_COUNTER(n_meter_set,
+    [DPIF_METER_SET] = METRICS_COUNTER(meter_set,
         "Number of addition or modification of a meter in all dpif."),
-    [DPIF_N_METER_GET] = METRICS_COUNTER(n_meter_get,
+    [DPIF_METER_GET] = METRICS_COUNTER(meter_get,
         "Number of meter queries in all dpif."),
-    [DPIF_N_METER_DEL] = METRICS_COUNTER(n_meter_del,
+    [DPIF_METER_DEL] = METRICS_COUNTER(meter_del,
         "Number of meter deletions in all dpif."),
 );
 

lib/metrics.c
@@ -277,18 +277,18 @@ struct metrics_class *metrics_classes[METRICS_N_NODE_TYPE] = {
 METRICS_SUBSYSTEM(metrics);
 
 enum {
-    METRICS_HIST_N_READ_ERRORS,
+    METRICS_HIST_READ_ERRORS,
 };
 
 static void
 metrics_entries_read_value(double *values, void *it OVS_UNUSED)
 {
-    values[METRICS_HIST_N_READ_ERRORS] = n_failed_histogram_reads;
+    values[METRICS_HIST_READ_ERRORS] = n_failed_histogram_reads;
 }
 
 METRICS_ENTRIES(metrics, metrics_entries,
     "metrics", metrics_entries_read_value,
-    [METRICS_HIST_N_READ_ERRORS] = METRICS_COUNTER(histogram_n_read_errors,
+    [METRICS_HIST_READ_ERRORS] = METRICS_COUNTER(histogram_read_errors,
         "Number of histogram reads that could not resolve without "
         "inconsistencies."),
 );

ofproto/ofproto-dpif-metrics.c
@@ -47,12 +47,12 @@ METRICS_COLLECTION(ofproto_dpif, foreach_dpif_backer,
                    do_foreach_dpif_backer, "name");
 
 enum {
-    OF_DATAPATH_N_HIT,
-    OF_DATAPATH_N_MISSED,
-    OF_DATAPATH_N_LOST,
+    OF_DATAPATH_HIT,
+    OF_DATAPATH_MISSED,
+    OF_DATAPATH_LOST,
     OF_DATAPATH_N_FLOWS,
-    OF_DATAPATH_N_CACHE_HIT,
-    OF_DATAPATH_N_MASK_HIT,
+    OF_DATAPATH_CACHE_HIT,
+    OF_DATAPATH_MASK_HIT,
     OF_DATAPATH_N_MASKS,
 };
 
@@ -64,28 +64,28 @@ datapath_read_value(double *values, void *it)
 
     dpif_get_dp_stats(backer->dpif, &dp_stats);
 
-    values[OF_DATAPATH_N_HIT] = MAX_IS_ZERO(dp_stats.n_hit);
-    values[OF_DATAPATH_N_MISSED] = MAX_IS_ZERO(dp_stats.n_missed);
-    values[OF_DATAPATH_N_LOST] = MAX_IS_ZERO(dp_stats.n_lost);
+    values[OF_DATAPATH_HIT] = MAX_IS_ZERO(dp_stats.n_hit);
+    values[OF_DATAPATH_MISSED] = MAX_IS_ZERO(dp_stats.n_missed);
+    values[OF_DATAPATH_LOST] = MAX_IS_ZERO(dp_stats.n_lost);
     values[OF_DATAPATH_N_FLOWS] = MAX_IS_ZERO(dp_stats.n_flows);
-    values[OF_DATAPATH_N_CACHE_HIT] = MAX_IS_ZERO(dp_stats.n_cache_hit);
-    values[OF_DATAPATH_N_MASK_HIT] = MAX_IS_ZERO(dp_stats.n_mask_hit);
+    values[OF_DATAPATH_CACHE_HIT] = MAX_IS_ZERO(dp_stats.n_cache_hit);
+    values[OF_DATAPATH_MASK_HIT] = MAX_IS_ZERO(dp_stats.n_mask_hit);
     values[OF_DATAPATH_N_MASKS] = MAX_IS_ZERO(dp_stats.n_masks);
 }
 
 METRICS_ENTRIES(foreach_dpif_backer, datapath_entries,
     "datapath", datapath_read_value,
-    [OF_DATAPATH_N_HIT] = METRICS_COUNTER(n_hit,
+    [OF_DATAPATH_HIT] = METRICS_COUNTER(hit,
         "Number of flow table matches."),
-    [OF_DATAPATH_N_MISSED] = METRICS_COUNTER(n_missed,
+    [OF_DATAPATH_MISSED] = METRICS_COUNTER(missed,
         "Number of flow table misses."),
-    [OF_DATAPATH_N_LOST] = METRICS_COUNTER(n_lost,
+    [OF_DATAPATH_LOST] = METRICS_COUNTER(lost,
         "Number of misses not sent to userspace."),
     [OF_DATAPATH_N_FLOWS] = METRICS_GAUGE(n_flows,
         "Number of flows present."),
-    [OF_DATAPATH_N_CACHE_HIT] = METRICS_COUNTER(n_cache_hit,
+    [OF_DATAPATH_CACHE_HIT] = METRICS_COUNTER(cache_hit,
         "Number of mega flow mask cache hits for flow table matches."),
-    [OF_DATAPATH_N_MASK_HIT] = METRICS_COUNTER(n_mask_hit,
+    [OF_DATAPATH_MASK_HIT] = METRICS_COUNTER(mask_hit,
         "Number of mega flow masks visited for flow table matches."),
     [OF_DATAPATH_N_MASKS] = METRICS_GAUGE(n_masks,
         "Number of mega flow masks."),

ofproto/ofproto-dpif-upcall.c
@@ -3761,10 +3761,10 @@ METRICS_COLLECTION(ofproto_dpif, foreach_udpif,
                    do_foreach_udpif, "name");
 
 enum {
-    OF_DPIF_N_PACKETS,
-    OF_DPIF_N_BYTES,
-    OF_DPIF_N_OFL_PACKETS,
-    OF_DPIF_N_OFL_BYTES,
+    OF_DPIF_PACKETS,
+    OF_DPIF_BYTES,
+    OF_DPIF_OFL_PACKETS,
+    OF_DPIF_OFL_BYTES,
     OF_DPIF_N_HANDLERS,
     OF_DPIF_N_REVALIDATORS,
 };
@@ -3788,23 +3788,23 @@ udpif_read_value(double *values, void *it)
         ovs_mutex_unlock(&udpif->revalidators[i].stats_lock);
     }
 
-    values[OF_DPIF_N_PACKETS] = n_packets;
-    values[OF_DPIF_N_BYTES] = n_bytes;
-    values[OF_DPIF_N_OFL_PACKETS] = n_ofl_packets;
-    values[OF_DPIF_N_OFL_BYTES] = n_ofl_bytes;
+    values[OF_DPIF_PACKETS] = n_packets;
+    values[OF_DPIF_BYTES] = n_bytes;
+    values[OF_DPIF_OFL_PACKETS] = n_ofl_packets;
+    values[OF_DPIF_OFL_BYTES] = n_ofl_bytes;
     values[OF_DPIF_N_HANDLERS] = udpif->n_handlers;
     values[OF_DPIF_N_REVALIDATORS] = udpif->n_revalidators;
 }
 
 METRICS_ENTRIES(foreach_udpif, udpif_entries,
     "datapath", udpif_read_value,
-    [OF_DPIF_N_PACKETS] = METRICS_COUNTER(n_packets,
+    [OF_DPIF_PACKETS] = METRICS_COUNTER(packets,
         "Number of packets processed in total on this datapath."),
-    [OF_DPIF_N_OFL_PACKETS] = METRICS_COUNTER(n_offloaded_packets,
+    [OF_DPIF_OFL_PACKETS] = METRICS_COUNTER(offloaded_packets,
         "Number of packets processed in hardware on this datapath."),
-    [OF_DPIF_N_BYTES] = METRICS_COUNTER(n_bytes,
+    [OF_DPIF_BYTES] = METRICS_COUNTER(bytes,
         "Number of bytes processed in total on this datapath."),
-    [OF_DPIF_N_OFL_BYTES] = METRICS_COUNTER(n_offloaded_bytes,
+    [OF_DPIF_OFL_BYTES] = METRICS_COUNTER(offloaded_bytes,
         "Number of bytes processed in hardware on this datapath."),
     [OF_DPIF_N_HANDLERS] = METRICS_GAUGE(n_handlers,
         "Number of upcall handler threads."),

commit 53e086e36f083c3e6c7219dc36196b1f8d2a09aa

metrics: Remove last n_ from counter names
Some names were missed.

Issue: OVS-3289544
Fixes: 9d75992f92a2 ("metrics: Remove n_ from counter names")
Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

lib/dpif-netdev.c
@@ -2686,21 +2686,21 @@ METRICS_ENTRIES(foreach_poll_threads, poll_threads_entries,
 
 enum {
     PMD_METRICS_SIMPLE_N_ENTRIES,
-    PMD_METRICS_SIMPLE_N_HIT,
-    PMD_METRICS_SIMPLE_N_MISS,
-    PMD_METRICS_SIMPLE_N_UPDATES,
+    PMD_METRICS_SIMPLE_HIT,
+    PMD_METRICS_SIMPLE_MISS,
+    PMD_METRICS_SIMPLE_UPDATE,
     PMD_METRICS_EMC_N_ENTRIES,
-    PMD_METRICS_EMC_N_HIT,
-    PMD_METRICS_EMC_N_MISS,
-    PMD_METRICS_EMC_N_UPDATES,
+    PMD_METRICS_EMC_HIT,
+    PMD_METRICS_EMC_MISS,
+    PMD_METRICS_EMC_UPDATE,
     PMD_METRICS_SMC_N_ENTRIES,
-    PMD_METRICS_SMC_N_HIT,
-    PMD_METRICS_SMC_N_MISS,
-    PMD_METRICS_SMC_N_UPDATES,
+    PMD_METRICS_SMC_HIT,
+    PMD_METRICS_SMC_MISS,
+    PMD_METRICS_SMC_UPDATE,
     PMD_METRICS_CLS_N_ENTRIES,
-    PMD_METRICS_CLS_N_HIT,
-    PMD_METRICS_CLS_N_MISS,
-    PMD_METRICS_CLS_N_UPDATES,
+    PMD_METRICS_CLS_HIT,
+    PMD_METRICS_CLS_MISS,
+    PMD_METRICS_CLS_UPDATE,
     PMD_METRICS_N_CACHE_ENTRIES,
 };
 
@@ -2718,21 +2718,21 @@ poll_threads_cache_read_value(double *values, void *it)
 
     values[PMD_METRICS_SIMPLE_N_ENTRIES] =
         cmap_count(&pmd->simple_match_table);
-    values[PMD_METRICS_SIMPLE_N_HIT] = stats[PMD_STAT_SIMPLE_HIT];
-    values[PMD_METRICS_SIMPLE_N_MISS] = stats[PMD_STAT_SIMPLE_MISS];
-    values[PMD_METRICS_SIMPLE_N_UPDATES] = stats[PMD_STAT_SIMPLE_UPDATE];
+    values[PMD_METRICS_SIMPLE_HIT] = stats[PMD_STAT_SIMPLE_HIT];
+    values[PMD_METRICS_SIMPLE_MISS] = stats[PMD_STAT_SIMPLE_MISS];
+    values[PMD_METRICS_SIMPLE_UPDATE] = stats[PMD_STAT_SIMPLE_UPDATE];
 
     values[PMD_METRICS_EMC_N_ENTRIES] =
         emc_cache_count(&(pmd->flow_cache).emc_cache);
-    values[PMD_METRICS_EMC_N_HIT] = stats[PMD_STAT_EXACT_HIT];
-    values[PMD_METRICS_EMC_N_MISS] = stats[PMD_STAT_EXACT_MISS];
-    values[PMD_METRICS_EMC_N_UPDATES] = stats[PMD_STAT_EXACT_UPDATE];
+    values[PMD_METRICS_EMC_HIT] = stats[PMD_STAT_EXACT_HIT];
+    values[PMD_METRICS_EMC_MISS] = stats[PMD_STAT_EXACT_MISS];
+    values[PMD_METRICS_EMC_UPDATE] = stats[PMD_STAT_EXACT_UPDATE];
 
     values[PMD_METRICS_SMC_N_ENTRIES] =
         smc_cache_count(&(pmd->flow_cache).smc_cache);
-    values[PMD_METRICS_SMC_N_HIT] = stats[PMD_STAT_SMC_HIT];
-    values[PMD_METRICS_SMC_N_MISS] = stats[PMD_STAT_SMC_MISS];
-    values[PMD_METRICS_SMC_N_UPDATES] = stats[PMD_STAT_SMC_UPDATE];
+    values[PMD_METRICS_SMC_HIT] = stats[PMD_STAT_SMC_HIT];
+    values[PMD_METRICS_SMC_MISS] = stats[PMD_STAT_SMC_MISS];
+    values[PMD_METRICS_SMC_UPDATE] = stats[PMD_STAT_SMC_UPDATE];
 
     pmd_n_cls_rules = 0;
     CMAP_FOR_EACH (cls, node, &pmd->classifiers) {
@@ -2740,10 +2740,10 @@ poll_threads_cache_read_value(double *values, void *it)
     }
 
     values[PMD_METRICS_CLS_N_ENTRIES] = pmd_n_cls_rules;
-    values[PMD_METRICS_CLS_N_HIT] = stats[PMD_STAT_MASKED_HIT];
-    values[PMD_METRICS_CLS_N_MISS] = stats[PMD_STAT_MASKED_LOOKUP] -
+    values[PMD_METRICS_CLS_HIT] = stats[PMD_STAT_MASKED_HIT];
+    values[PMD_METRICS_CLS_MISS] = stats[PMD_STAT_MASKED_LOOKUP] -
                                      stats[PMD_STAT_MASKED_HIT];
-    values[PMD_METRICS_CLS_N_UPDATES] = stats[PMD_STAT_MASKED_UPDATE];
+    values[PMD_METRICS_CLS_UPDATE] = stats[PMD_STAT_MASKED_UPDATE];
 }
 
 /* Use a single point of definition for the cache entries to enforce
@@ -2753,38 +2753,38 @@ poll_threads_cache_read_value(double *values, void *it)
     /* Simple match cache. */                                          \
     [PMD_METRICS_SIMPLE_N_ENTRIES] = METRICS_GAUGE(simple_n_entries,   \
         "Number of entries in the simple match cache."),               \
-    [PMD_METRICS_SIMPLE_N_HIT] = METRICS_COUNTER(simple_n_hit,         \
+    [PMD_METRICS_SIMPLE_HIT] = METRICS_COUNTER(simple_hit,             \
         "Number of lookup hit in the simple match cache."),            \
-    [PMD_METRICS_SIMPLE_N_MISS] = METRICS_COUNTER(simple_n_miss,       \
+    [PMD_METRICS_SIMPLE_MISS] = METRICS_COUNTER(simple_miss,           \
         "Number of lookup miss in the simple match cache."),           \
-    [PMD_METRICS_SIMPLE_N_UPDATES] = METRICS_COUNTER(simple_n_updates, \
+    [PMD_METRICS_SIMPLE_UPDATE] = METRICS_COUNTER(simple_update,       \
         "Number of updates of the simple match cache."),               \
     /* Exact match cache. */                                           \
     [PMD_METRICS_EMC_N_ENTRIES] = METRICS_GAUGE(emc_n_entries,         \
         "Number of entries in the exact match cache."),                \
-    [PMD_METRICS_EMC_N_HIT] = METRICS_COUNTER(emc_n_hit,               \
+    [PMD_METRICS_EMC_HIT] = METRICS_COUNTER(emc_hit,                   \
         "Number of lookup hit in the exact match cache."),             \
-    [PMD_METRICS_EMC_N_MISS] = METRICS_COUNTER(emc_n_miss,             \
+    [PMD_METRICS_EMC_MISS] = METRICS_COUNTER(emc_miss,                 \
         "Number of lookup miss in the exact match cache."),            \
-    [PMD_METRICS_EMC_N_UPDATES] = METRICS_COUNTER(emc_n_updates,       \
+    [PMD_METRICS_EMC_UPDATE] = METRICS_COUNTER(emc_update,             \
         "Number of updates of the exact match cache."),                \
     /* Signature match cache. */                                       \
     [PMD_METRICS_SMC_N_ENTRIES] = METRICS_GAUGE(smc_n_entries,         \
         "Number of entries in the signature match cache."),            \
-    [PMD_METRICS_SMC_N_HIT] = METRICS_COUNTER(smc_n_hit,               \
+    [PMD_METRICS_SMC_HIT] = METRICS_COUNTER(smc_hit,                   \
         "Number of lookup hit in the signature match cache."),         \
-    [PMD_METRICS_SMC_N_MISS] = METRICS_COUNTER(smc_n_miss,             \
+    [PMD_METRICS_SMC_MISS] = METRICS_COUNTER(smc_miss,                 \
         "Number of lookup miss in the signature match cache."),        \
-    [PMD_METRICS_SMC_N_UPDATES] = METRICS_COUNTER(smc_n_updates,       \
+    [PMD_METRICS_SMC_UPDATE] = METRICS_COUNTER(smc_update,             \
         "Number of updates of the signature match cache."),            \
     /* Datapath classifiers. */                                        \
     [PMD_METRICS_CLS_N_ENTRIES] = METRICS_GAUGE(cls_n_entries,         \
         "Number of entries in the datapath classifiers."),             \
-    [PMD_METRICS_CLS_N_HIT] = METRICS_COUNTER(cls_n_hit,               \
+    [PMD_METRICS_CLS_HIT] = METRICS_COUNTER(cls_hit,                   \
         "Number of lookup hit in the datapath classifiers."),          \
-    [PMD_METRICS_CLS_N_MISS] = METRICS_COUNTER(cls_n_miss,             \
+    [PMD_METRICS_CLS_MISS] = METRICS_COUNTER(cls_miss,                 \
         "Number of lookup miss in the datapath classifiers."),         \
-    [PMD_METRICS_CLS_N_UPDATES] = METRICS_COUNTER(cls_n_updates,       \
+    [PMD_METRICS_CLS_UPDATE] = METRICS_COUNTER(cls_update,             \
         "Number of updates of the datapath classifiers."),
 
 METRICS_ENTRIES(foreach_poll_threads_dbg, poll_threads_cache_dbg_entries,