elibritstein/OVS

commit 6dd214adb97d82d07391d2731f5fa965d075d863

netdev-offload-dpdk: Support hairpin offload
Upon init of a netdev, create a hairpin fixed flow on NIC domain:
flow create 1 ingress priority 0 group 0x0 pattern mark id spec 1 / end
actions queue index 1 / end

For output to the same netdev as itself (e.g. hairpin), set the hairpin
mark and jump to a miss table on FDB. The packet will miss, and go to
the NIC domain, in which it will hit the hairpin fixed flow (above).

flow create 0 ingress priority 0 group 0x0 transfer pattern port_id id
is 1 / end actions count shared 0 identifier 0 / set_mac_dst mac_addr
00:11:22:33:44:55 / mark id 1 / jump group 0xff000000 / end

Issue: 3079257
Change-Id: Iedc1260e1eece411b6649e32b0300adbd7d56ea9
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -213,6 +213,7 @@ struct netdev_offload_dpdk_data {
     struct ovsthread_once ct_tables_once;
     struct fixed_rule ct_nat_miss;
     struct fixed_rule zone_flows[2][2][MAX_ZONE_ID + 1];
+    struct fixed_rule hairpin;
 };
 
 static int
@@ -2733,6 +2734,14 @@ dump_flow_action(struct ds *s, struct ds *s_extra,
         ds_put_cstr(s, "/ ");
     } else if (actions->type == RTE_FLOW_ACTION_TYPE_VOID) {
         ds_put_cstr(s, "void / ");
+    } else if (actions->type == RTE_FLOW_ACTION_TYPE_QUEUE) {
+        const struct rte_flow_action_queue *queue = actions->conf;
+
+        ds_put_cstr(s, "queue ");
+        if (queue) {
+            ds_put_format(s, "index %d ", queue->index);
+        }
+        ds_put_cstr(s, "/ ");
     } else {
         ds_put_format(s, "unknown rte flow action (%d)\n", actions->type);
     }
@@ -4218,6 +4227,19 @@ add_port_id_action(struct flow_actions *actions,
     add_flow_action(actions, RTE_FLOW_ACTION_TYPE_PORT_ID, port_id);
 }
 
+static void
+add_hairpin_action(struct flow_actions *actions)
+{
+    struct rte_flow_action_mark *mark = per_thread_xzalloc(sizeof *mark);
+    struct rte_flow_action_jump *jump = per_thread_xzalloc (sizeof *jump);
+
+    mark->id = HAIRPIN_FLOW_MARK;
+    add_flow_action(actions, RTE_FLOW_ACTION_TYPE_MARK, mark);
+
+    jump->group = MISS_TABLE_ID;
+    add_flow_action(actions, RTE_FLOW_ACTION_TYPE_JUMP, jump);
+}
+
 static int
 get_netdev_by_port(struct netdev *netdev,
                    const struct nlattr *nla,
@@ -4259,7 +4281,11 @@ add_output_action(struct netdev *netdev,
     if (get_netdev_by_port(netdev, nla, &outdev_id, &outdev)) {
         return -1;
     }
-    add_port_id_action(actions, outdev_id);
+    if (netdev == outdev) {
+        add_hairpin_action(actions);
+    } else {
+        add_port_id_action(actions, outdev_id);
+    }
 
     netdev_close(outdev);
     return ret;
@@ -6218,6 +6244,48 @@ err:
 }
 
 static void
+hairpin_uninit(struct netdev *netdev, unsigned int tid,
+               struct fixed_rule *fr)
+{
+    fixed_rule_uninit(netdev, tid, fr);
+}
+
+static int
+hairpin_init(struct netdev *netdev, unsigned int tid,
+             struct fixed_rule *fr)
+{
+    struct rte_flow_attr attr = { .ingress = 1, };
+    struct rte_flow_item_mark hp_mark;
+    struct flow_patterns patterns = {
+        .items = (struct rte_flow_item []) {
+            { .type = RTE_FLOW_ITEM_TYPE_MARK, .spec = &hp_mark, },
+            { .type = RTE_FLOW_ITEM_TYPE_END, },
+        },
+        .cnt = 2,
+    };
+    struct rte_flow_action_queue hp_queue;
+    struct flow_actions actions = {
+        .actions = (struct rte_flow_action []) {
+            { .type = RTE_FLOW_ACTION_TYPE_QUEUE, .conf = &hp_queue, },
+            { .type = RTE_FLOW_ACTION_TYPE_END, },
+        },
+        .cnt = 2,
+    };
+    struct rte_flow_error error;
+
+    hp_mark.id = HAIRPIN_FLOW_MARK;
+    hp_queue.index = netdev->n_rxq;
+
+    fr->flow = create_rte_flow(netdev, &attr, &patterns, &actions, &error);
+    fr->creation_tid = tid;
+
+    if (fr->flow == NULL) {
+        return -1;
+    }
+    return 0;
+}
+
+static void
 ct_tables_uninit(struct netdev *netdev, unsigned int tid)
 {
     struct netdev_offload_dpdk_data *data;
@@ -6231,6 +6299,7 @@ ct_tables_uninit(struct netdev *netdev, unsigned int tid)
 
     ct_nat_miss_uninit(netdev, tid, &data->ct_nat_miss);
     ct_zones_uninit(netdev, tid, data);
+    hairpin_uninit(netdev, tid, &data->hairpin);
 }
 
 static int
@@ -6251,6 +6320,9 @@ ct_tables_init(struct netdev *netdev, unsigned int tid)
         ret = ct_nat_miss_init(netdev, tid, &data->ct_nat_miss);
         if (!ret) {
             ret = ct_zones_init(netdev, tid, data);
+        }
+        if (!ret) {
+            ret = hairpin_init(netdev, tid, &data->hairpin);
         }
         ovsthread_once_done(&data->ct_tables_once);
         if (ret) {

lib/netdev-offload.h
@@ -137,7 +137,8 @@ netdev_offload_thread_id(void)
 }
 
 #define INVALID_FLOW_MARK 0
-#define MIN_FLOW_MARK 1
+#define HAIRPIN_FLOW_MARK 1
+#define MIN_FLOW_MARK 2
 #define MAX_FLOW_MARK (UINT32_MAX - 1)
 #define NB_FLOW_MARK (MAX_FLOW_MARK - MIN_FLOW_MARK + 1)
 

tests/dpif-netdev.at
@@ -428,7 +428,7 @@ skb_priority(0),skb_mark(0),ct_state(0),ct_zone(0),ct_mark(0),ct_label(0),recirc
    # Check that flow successfully offloaded.
    OVS_WAIT_UNTIL([grep "succeed to add netdev flow" ovs-vswitchd.log])
    AT_CHECK([filter_hw_flow_install < ovs-vswitchd.log | strip_xout], [0], [dnl
-p1: flow put[[create]]: flow match: recirc_id=0,eth,ip,in_port=1,vlan_tci=0x0000/0x1fff,nw_frag=no, mark: 1
+p1: flow put[[create]]: flow match: recirc_id=0,eth,ip,in_port=1,vlan_tci=0x0000/0x1fff,nw_frag=no, mark: 2
 ])
    # Check that datapath flow installed successfully.
    AT_CHECK([filter_flow_install < ovs-vswitchd.log | strip_xout], [0], [dnl
@@ -439,7 +439,7 @@ recirc_id(0),in_port(1),packet_type(ns=0,id=0),eth_type(0x0800),ipv4(frag=no), a
 
    # Check for succesfull packet matching with installed offloaded flow.
    AT_CHECK([filter_hw_packet_netdev_dummy < ovs-vswitchd.log | strip_xout], [0], [dnl
-p1: packet: ip,vlan_tci=0x0000,dl_src=00:06:07:08:09:0a,dl_dst=00:01:02:03:04:05,nw_src=127.0.0.1,nw_dst=127.0.0.1,nw_proto=0,nw_tos=0,nw_ecn=0,nw_ttl=64 matches with flow: recirc_id=0,eth,ip,vlan_tci=0x0000/0x1fff,nw_frag=no with mark: 1
+p1: packet: ip,vlan_tci=0x0000,dl_src=00:06:07:08:09:0a,dl_dst=00:01:02:03:04:05,nw_src=127.0.0.1,nw_dst=127.0.0.1,nw_proto=0,nw_tos=0,nw_ecn=0,nw_ttl=64 matches with flow: recirc_id=0,eth,ip,vlan_tci=0x0000/0x1fff,nw_frag=no with mark: 2
 ])
 
    ovs-appctl revalidator/wait
@@ -456,7 +456,7 @@ recirc_id(0),in_port(1),packet_type(ns=0,id=0),eth_type(0x0800),ipv4(frag=no), p
    # Check that flow successfully deleted from HW.
    OVS_WAIT_UNTIL([grep "succeed to delete netdev flow" ovs-vswitchd.log])
    AT_CHECK([filter_hw_flow_del < ovs-vswitchd.log | strip_xout], [0], [dnl
-p1: flow del: mark: 1
+p1: flow del: mark: 2
 ])
    OVS_VSWITCHD_STOP
    AT_CLEANUP])
@@ -495,7 +495,7 @@ packet_type(ns=0,id=0),eth(src=00:06:07:08:09:0a,dst=00:01:02:03:04:05),eth_type
    # Check that flow successfully offloaded.
    OVS_WAIT_UNTIL([grep "succeed to add netdev flow" ovs-vswitchd.log])
    AT_CHECK([filter_hw_flow_install < ovs-vswitchd.log | strip_xout], [0], [dnl
-p1: flow put[[create]]: flow match: recirc_id=0,eth,udp,in_port=1,dl_vlan=99,nw_src=127.0.0.1,nw_frag=no,tp_dst=82, mark: 1
+p1: flow put[[create]]: flow match: recirc_id=0,eth,udp,in_port=1,dl_vlan=99,nw_src=127.0.0.1,nw_frag=no,tp_dst=82, mark: 2
 ])
    # Check that datapath flow installed successfully.
    AT_CHECK([filter_flow_install < ovs-vswitchd.log | strip_xout], [0], [dnl
@@ -507,7 +507,7 @@ recirc_id(0),in_port(1),packet_type(ns=0,id=0),eth_type(0x8100),vlan(vid=99,pcp=
    # Check for succesfull packet matching with installed offloaded flow.
    AT_CHECK([filter_hw_packet_netdev_dummy < ovs-vswitchd.log | strip_xout], [0], [dnl
 p1: packet: udp,dl_vlan=99,dl_vlan_pcp=7,vlan_tci1=0x0000,dl_src=00:06:07:08:09:0a,dl_dst=00:01:02:03:04:05,nw_src=127.0.0.1,nw_dst=127.0.0.1,nw_tos=0,nw_ecn=0,nw_ttl=64,tp_src=81,tp_dst=82 dnl
-matches with flow: recirc_id=0,eth,udp,dl_vlan=99,nw_src=127.0.0.1,nw_frag=no,tp_dst=82 with mark: 1
+matches with flow: recirc_id=0,eth,udp,dl_vlan=99,nw_src=127.0.0.1,nw_frag=no,tp_dst=82 with mark: 2
 ])
 
    ovs-appctl revalidator/wait
@@ -525,7 +525,7 @@ packets:1, bytes:64, used:0.0s, actions:set(ipv4(src=192.168.0.7)),set(udp(dst=3
    # Check that flow successfully deleted from HW.
    OVS_WAIT_UNTIL([grep "succeed to delete netdev flow" ovs-vswitchd.log])
    AT_CHECK([filter_hw_flow_del < ovs-vswitchd.log | strip_xout], [0], [dnl
-p1: flow del: mark: 1
+p1: flow del: mark: 2
 ])
 
    # Check that ip address and udp port were correctly modified in output packets.
@@ -572,7 +572,7 @@ packet_type(ns=0,id=0),eth(src=00:06:07:08:09:0a,dst=00:01:02:03:04:05),eth_type
    # Check that flow successfully offloaded.
    OVS_WAIT_UNTIL([grep "succeed to add netdev flow" ovs-vswitchd.log])
    AT_CHECK([filter_hw_flow_install < ovs-vswitchd.log | strip_xout], [0], [dnl
-p1: flow put[[create]]: flow match: recirc_id=0,eth,arp,in_port=1,dl_vlan=99,dl_vlan_pcp=7, mark: 1
+p1: flow put[[create]]: flow match: recirc_id=0,eth,arp,in_port=1,dl_vlan=99,dl_vlan_pcp=7, mark: 2
 ])
    # Check that datapath flow installed successfully.
    AT_CHECK([filter_flow_install < ovs-vswitchd.log | strip_xout], [0], [dnl
@@ -584,7 +584,7 @@ recirc_id(0),in_port(1),packet_type(ns=0,id=0),eth_type(0x8100),vlan(vid=99,pcp=
    # Check for succesfull packet matching with installed offloaded flow.
    AT_CHECK([filter_hw_packet_netdev_dummy < ovs-vswitchd.log | strip_xout], [0], [dnl
 p1: packet: arp,dl_vlan=99,dl_vlan_pcp=7,vlan_tci1=0x0000,dl_src=00:06:07:08:09:0a,dl_dst=00:01:02:03:04:05,arp_spa=127.0.0.1,arp_tpa=127.0.0.1,arp_op=1,arp_sha=00:0b:0c:0d:0e:0f,arp_tha=00:00:00:00:00:00 dnl
-matches with flow: recirc_id=0,eth,arp,dl_vlan=99,dl_vlan_pcp=7 with mark: 1
+matches with flow: recirc_id=0,eth,arp,dl_vlan=99,dl_vlan_pcp=7 with mark: 2
 ])
 
    ovs-appctl revalidator/wait
@@ -602,7 +602,7 @@ packets:1, bytes:64, used:0.0s, actions:pop_vlan,push_vlan(vid=11,pcp=7),1
    # Check that flow successfully deleted from HW.
    OVS_WAIT_UNTIL([grep "succeed to delete netdev flow" ovs-vswitchd.log])
    AT_CHECK([filter_hw_flow_del < ovs-vswitchd.log | strip_xout], [0], [dnl
-p1: flow del: mark: 1
+p1: flow del: mark: 2
 ])
 
    # Check that VLAN ID was correctly modified in output packets.

commit 1b56a3c5b2dec15c2b9882e771bef5ddf55ca013

netdev-offload-dpdk: Fix hairpin flow removal
Hairpin flow removal use fixed_rule_uninit() that assumed removing
transfer flows. For hairpin flows it is wrong. Fix it.

Issue: OVS-3334627
Fixes: 6dd214adb97d ("netdev-offload-dpdk: Support hairpin offload")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -6286,13 +6286,13 @@ netdev_offload_dpdk_ct_counter_query(struct netdev *netdev,
 
 static void
 fixed_rule_uninit(struct netdev *netdev, unsigned int tid,
-                  struct fixed_rule *fr)
+                  struct fixed_rule *fr, bool is_esw)
 {
     if (fr->creation_tid != tid || !fr->flow) {
         return;
     }
 
-    netdev_offload_dpdk_destroy_flow(netdev, fr->flow, NULL, true);
+    netdev_offload_dpdk_destroy_flow(netdev, fr->flow, NULL, is_esw);
     fr->flow = NULL;
 }
 
@@ -6300,7 +6300,7 @@ static void
 ct_nat_miss_uninit(struct netdev *netdev, unsigned int tid,
                    struct fixed_rule *fr)
 {
-    fixed_rule_uninit(netdev, tid, fr);
+    fixed_rule_uninit(netdev, tid, fr, true);
 }
 
 static int
@@ -6333,7 +6333,7 @@ ct_zones_uninit(struct netdev *netdev, unsigned int tid,
             for (zone_id = MIN_ZONE_ID; zone_id <= MAX_ZONE_ID; zone_id++) {
                 fr = &data->zone_flows[nat][i][zone_id];
 
-                fixed_rule_uninit(netdev, tid, fr);
+                fixed_rule_uninit(netdev, tid, fr, true);
             }
         }
     }
@@ -6468,7 +6468,7 @@ static void
 hairpin_uninit(struct netdev *netdev, unsigned int tid,
                struct fixed_rule *fr)
 {
-    fixed_rule_uninit(netdev, tid, fr);
+    fixed_rule_uninit(netdev, tid, fr, false);
 }
 
 static int