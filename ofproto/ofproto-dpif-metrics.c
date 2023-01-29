elibritstein/OVS

commit 2d54a7d46ea377185de526d154227cbe04b1625b

ofproto-dpif-metrics: Use accurate dpif packet stats
Accessing all rules without discrimination through udpif keys
is misleading, as many packets are being recirculated in a datapath.

As a result, packets getting through multiple flows are counted
as many times, throwing off raw numbers and ratios.

Modify the metrics to use the ofproto stats directly and separate
in two sets for 'general processing' and 'output processing' done.

Issue: OVS-3325592
Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

ofproto/ofproto-dpif-metrics.c
@@ -54,13 +54,26 @@ enum {
     OF_DATAPATH_CACHE_HIT,
     OF_DATAPATH_MASK_HIT,
     OF_DATAPATH_N_MASKS,
+    OF_DATAPATH_PACKETS,
+    OF_DATAPATH_BYTES,
+    OF_DATAPATH_OFL_PACKETS,
+    OF_DATAPATH_OFL_BYTES,
+    OF_DATAPATH_TX_PACKETS,
+    OF_DATAPATH_TX_BYTES,
+    OF_DATAPATH_TX_OFL_PACKETS,
+    OF_DATAPATH_TX_OFL_BYTES,
 };
 
 static void
 datapath_read_value(double *values, void *it)
 {
     const struct dpif_backer *backer = it;
+    const struct shash_node **ofprotos;
     struct dpif_dp_stats dp_stats;
+    struct pkt_stats sum_tx_stats;
+    struct pkt_stats sum_stats;
+    struct shash ofproto_shash;
+    size_t i;
 
     dpif_get_dp_stats(backer->dpif, &dp_stats);
 
@@ -71,6 +84,36 @@ datapath_read_value(double *values, void *it)
     values[OF_DATAPATH_CACHE_HIT] = MAX_IS_ZERO(dp_stats.n_cache_hit);
     values[OF_DATAPATH_MASK_HIT] = MAX_IS_ZERO(dp_stats.n_mask_hit);
     values[OF_DATAPATH_N_MASKS] = MAX_IS_ZERO(dp_stats.n_masks);
+
+    memset(&sum_tx_stats, 0, sizeof sum_tx_stats);
+    memset(&sum_stats, 0, sizeof sum_stats);
+    shash_init(&ofproto_shash);
+    ofprotos = ofproto_dpif_get_ofprotos(&ofproto_shash);
+    for (i = 0; i < shash_count(&ofproto_shash); i++) {
+        struct ofproto_dpif *ofproto = ofprotos[i]->data;
+        struct pkt_stats stats;
+        struct pkt_stats tx_stats;
+
+        if (ofproto->backer != backer) {
+            continue;
+        }
+
+        ofproto_get_pkt_stats(&ofproto->up, &stats, &tx_stats);
+        pkt_stats_add(&sum_tx_stats, tx_stats);
+        pkt_stats_add(&sum_stats, stats);
+    }
+    shash_destroy(&ofproto_shash);
+    free(ofprotos);
+
+    values[OF_DATAPATH_PACKETS] = sum_stats.n_packets;
+    values[OF_DATAPATH_BYTES] = sum_stats.n_bytes;
+    values[OF_DATAPATH_OFL_PACKETS] = sum_stats.n_offload_packets;
+    values[OF_DATAPATH_OFL_BYTES] = sum_stats.n_offload_bytes;
+
+    values[OF_DATAPATH_TX_PACKETS] = sum_tx_stats.n_packets;
+    values[OF_DATAPATH_TX_BYTES] = sum_tx_stats.n_bytes;
+    values[OF_DATAPATH_TX_OFL_PACKETS] = sum_tx_stats.n_offload_packets;
+    values[OF_DATAPATH_TX_OFL_BYTES] = sum_tx_stats.n_offload_bytes;
 }
 
 METRICS_ENTRIES(foreach_dpif_backer, datapath_entries,
@@ -89,6 +132,24 @@ METRICS_ENTRIES(foreach_dpif_backer, datapath_entries,
         "Number of mega flow masks visited for flow table matches."),
     [OF_DATAPATH_N_MASKS] = METRICS_GAUGE(n_masks,
         "Number of mega flow masks."),
+    [OF_DATAPATH_PACKETS] = METRICS_COUNTER(packets,
+        "Number of packets processed in total on this datapath."),
+    [OF_DATAPATH_BYTES] = METRICS_COUNTER(bytes,
+        "Number of bytes processed in total on this datapath."),
+    [OF_DATAPATH_OFL_PACKETS] = METRICS_COUNTER(offloaded_packets,
+        "Number of packets processed in hardware on this datapath."),
+    [OF_DATAPATH_OFL_BYTES] = METRICS_COUNTER(offloaded_bytes,
+        "Number of bytes processed in hardware on this datapath."),
+    [OF_DATAPATH_TX_PACKETS] = METRICS_COUNTER(tx_packets,
+        "Number of packets emitted in total from this datapath."),
+    [OF_DATAPATH_TX_BYTES] = METRICS_COUNTER(tx_bytes,
+        "Number of bytes emitted in total from this datapath."),
+    [OF_DATAPATH_TX_OFL_PACKETS] = METRICS_COUNTER(tx_offloaded_packets,
+        "Total number of packets emitted from this datapath and fully "
+        "processed in hardware."),
+    [OF_DATAPATH_TX_OFL_BYTES] = METRICS_COUNTER(tx_offloaded_bytes,
+        "Total number of bytes emitted from this datapath and fully "
+        "processed in hardware."),
 );
 
 METRICS_DECLARE(udpif_entries);

ofproto/ofproto-dpif-upcall.c
@@ -3761,10 +3761,6 @@ METRICS_COLLECTION(ofproto_dpif, foreach_udpif,
                    do_foreach_udpif, "name");
 
 enum {
-    OF_DPIF_PACKETS,
-    OF_DPIF_BYTES,
-    OF_DPIF_OFL_PACKETS,
-    OF_DPIF_OFL_BYTES,
     OF_DPIF_N_HANDLERS,
     OF_DPIF_N_REVALIDATORS,
 };
@@ -3772,40 +3768,14 @@ enum {
 static void
 udpif_read_value(double *values, void *it)
 {
-    uint64_t n_packets, n_ofl_packets;
-    uint64_t n_bytes, n_ofl_bytes;
     struct udpif *udpif = it;
-    size_t i;
 
-    n_packets = n_ofl_packets = 0;
-    n_bytes = n_ofl_bytes = 0;
-    for (i = 0; i < udpif->n_revalidators; i++) {
-        ovs_mutex_lock(&udpif->revalidators[i].stats_lock);
-        n_packets += udpif->revalidators[i].n_packets;
-        n_bytes += udpif->revalidators[i].n_bytes;
-        n_ofl_packets += udpif->revalidators[i].n_offloaded_packets;
-        n_ofl_bytes += udpif->revalidators[i].n_offloaded_bytes;
-        ovs_mutex_unlock(&udpif->revalidators[i].stats_lock);
-    }
-
-    values[OF_DPIF_PACKETS] = n_packets;
-    values[OF_DPIF_BYTES] = n_bytes;
-    values[OF_DPIF_OFL_PACKETS] = n_ofl_packets;
-    values[OF_DPIF_OFL_BYTES] = n_ofl_bytes;
     values[OF_DPIF_N_HANDLERS] = udpif->n_handlers;
     values[OF_DPIF_N_REVALIDATORS] = udpif->n_revalidators;
 }
 
 METRICS_ENTRIES(foreach_udpif, udpif_entries,
     "datapath", udpif_read_value,
-    [OF_DPIF_PACKETS] = METRICS_COUNTER(packets,
-        "Number of packets processed in total on this datapath."),
-    [OF_DPIF_OFL_PACKETS] = METRICS_COUNTER(offloaded_packets,
-        "Number of packets processed in hardware on this datapath."),
-    [OF_DPIF_BYTES] = METRICS_COUNTER(bytes,
-        "Number of bytes processed in total on this datapath."),
-    [OF_DPIF_OFL_BYTES] = METRICS_COUNTER(offloaded_bytes,
-        "Number of bytes processed in hardware on this datapath."),
     [OF_DPIF_N_HANDLERS] = METRICS_GAUGE(n_handlers,
         "Number of upcall handler threads."),
     [OF_DPIF_N_REVALIDATORS] = METRICS_GAUGE(n_revalidators,

ofproto/ofproto-dpif.c
@@ -6149,8 +6149,8 @@ ofproto_unixctl_mcast_snooping_show(struct unixctl_conn *conn,
 /* Store the current ofprotos in 'ofproto_shash'.  Returns a sorted list
  * of the 'ofproto_shash' nodes.  It is the responsibility of the caller
  * to destroy 'ofproto_shash' and free the returned value. */
-static const struct shash_node **
-get_ofprotos(struct shash *ofproto_shash)
+const struct shash_node **
+ofproto_dpif_get_ofprotos(struct shash *ofproto_shash)
 {
     const struct ofproto_dpif *ofproto;
 
@@ -6174,7 +6174,7 @@ ofproto_unixctl_dpif_dump_dps(struct unixctl_conn *conn, int argc OVS_UNUSED,
     int i;
 
     shash_init(&ofproto_shash);
-    sorted_ofprotos = get_ofprotos(&ofproto_shash);
+    sorted_ofprotos = ofproto_dpif_get_ofprotos(&ofproto_shash);
     for (i = 0; i < shash_count(&ofproto_shash); i++) {
         const struct shash_node *node = sorted_ofprotos[i];
         ds_put_format(&ds, "%s\n", node->name);
@@ -6359,7 +6359,7 @@ dpif_show_backer(const struct dpif_backer *backer, struct ds *ds)
                   dpif_name(backer->dpif), dp_stats.n_hit, dp_stats.n_missed);
 
     shash_init(&ofproto_shash);
-    ofprotos = get_ofprotos(&ofproto_shash);
+    ofprotos = ofproto_dpif_get_ofprotos(&ofproto_shash);
     for (i = 0; i < shash_count(&ofproto_shash); i++) {
         struct ofproto_dpif *ofproto = ofprotos[i]->data;
         const struct shash_node **ports;
@@ -6466,7 +6466,7 @@ dpif_reset_backer_stats(struct dpif_backer *backer)
     size_t i;
 
     shash_init(&ofproto_shash);
-    ofprotos = get_ofprotos(&ofproto_shash);
+    ofprotos = ofproto_dpif_get_ofprotos(&ofproto_shash);
     for (i = 0; i < shash_count(&ofproto_shash); i++) {
         struct ofproto_dpif *ofproto = ofprotos[i]->data;
         struct pkt_stats stats;

ofproto/ofproto-dpif.h
@@ -366,6 +366,9 @@ struct ofproto_dpif *ofproto_dpif_lookup_by_uuid(const struct uuid *uuid);
 
 ovs_version_t ofproto_dpif_get_tables_version(struct ofproto_dpif *);
 
+const struct shash_node **
+ofproto_dpif_get_ofprotos(struct shash *ofproto_shash);
+
 void ofproto_dpif_credit_table_stats(struct ofproto_dpif *, uint8_t table_id,
                                      uint64_t n_matches, uint64_t n_misses);
 

utilities/ovs-metrics.in
@@ -154,14 +154,14 @@ def metrics_watch():
 
     entries = [
         Entry('sw-pkts', 'packet',
-              ['ovs_vswitchd_datapath_packets_total',
-               'ovs_vswitchd_datapath_offloaded_packets_total'],
+              ['ovs_vswitchd_datapath_tx_packets_total',
+               'ovs_vswitchd_datapath_tx_offloaded_packets_total'],
               derive='sw-pps'),
         Entry('sw-conns', 'conn',
               ['ovs_vswitchd_conntrack_n_connections'],
               derive='sw-cps'),
         Entry('hw-pkts', 'packet',
-              ['ovs_vswitchd_datapath_offloaded_packets_total'],
+              ['ovs_vswitchd_datapath_tx_offloaded_packets_total'],
               derive='hw-pps'),
         Entry('hw-conns', 'conn',
               ['ovs_vswitchd_datapath_hw_offload_n_ct_bidir'],

commit 5772e3b0a100017ef8091447466496d94515069d

ofproto: Fix uninitialized stats access 581308, 581314
581308 Uninitialized scalar variable
The variable will contain an arbitrary value left from earlier computations.
In datapath_read_value: Use of an uninitialized variable (CWE-457)

581314 Uninitialized scalar variable
The variable will contain an arbitrary value left from earlier computations.
In datapath_read_value: Use of an uninitialized variable (CWE-457)

Coverity issue: CID 581308
Coverity issue: CID 581314
Issue: OVS-3325592
Fixes: 2d54a7d46ea3 ("ofproto-dpif-metrics: Use accurate dpif packet stats")
Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

ofproto/ofproto-dpif-metrics.c
@@ -91,13 +91,15 @@ datapath_read_value(double *values, void *it)
     ofprotos = ofproto_dpif_get_ofprotos(&ofproto_shash);
     for (i = 0; i < shash_count(&ofproto_shash); i++) {
         struct ofproto_dpif *ofproto = ofprotos[i]->data;
-        struct pkt_stats stats;
         struct pkt_stats tx_stats;
+        struct pkt_stats stats;
 
         if (ofproto->backer != backer) {
             continue;
         }
 
+        memset(&tx_stats, 0, sizeof tx_stats);
+        memset(&stats, 0, sizeof stats);
         ofproto_get_pkt_stats(&ofproto->up, &stats, &tx_stats);
         pkt_stats_add(&sum_tx_stats, tx_stats);
         pkt_stats_add(&sum_stats, stats);