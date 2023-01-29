elibritstein/OVS

commit ab26f3866326c35ee7afc80c6a76f748eb384485

ofproto-dpif: Add a dpif stats reset function
As some DPIF stats are shown on the 'dpif/show' command,
introduce a command to reset those stats when it makes sense.

Reset the packet counters for all dpif to 0, by memorizing their current
value and substracting that value when querying.

Issue: OVS-3325592
Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

ofproto/ofproto-dpif.c
@@ -6413,6 +6413,7 @@ dpif_show_backer(const struct dpif_backer *backer, struct ds *ds)
             double pkts_ratio;
 
             ofproto_get_pkt_stats(&ofproto->up, &stats);
+            pkt_stats_sub(&stats, ofproto->stats_start);
 
             pkts_ratio = 0.0;
             if (stats.n_packets != 0.0) {
@@ -6455,6 +6456,48 @@ ofproto_unixctl_dpif_show(struct unixctl_conn *conn, int argc OVS_UNUSED,
 
     unixctl_command_reply(conn, ds_cstr(&ds));
     ds_destroy(&ds);
+}
+
+static void
+dpif_reset_backer_stats(struct dpif_backer *backer)
+{
+    const struct shash_node **ofprotos;
+    struct shash ofproto_shash;
+    size_t i;
+
+    shash_init(&ofproto_shash);
+    ofprotos = get_ofprotos(&ofproto_shash);
+    for (i = 0; i < shash_count(&ofproto_shash); i++) {
+        struct ofproto_dpif *ofproto = ofprotos[i]->data;
+        struct pkt_stats stats;
+
+        if (ofproto->backer != backer) {
+            continue;
+        }
+
+        ofproto_get_pkt_stats(&ofproto->up, &stats);
+        ofproto->stats_start = stats;
+    }
+    shash_destroy(&ofproto_shash);
+    free(ofprotos);
+}
+
+static void
+ofproto_unixctl_dpif_reset_stats(struct unixctl_conn *conn,
+                                 int argc OVS_UNUSED,
+                                 const char *argv[] OVS_UNUSED,
+                                 void *aux OVS_UNUSED)
+{
+    const struct shash_node **backers;
+    int i;
+
+    backers = shash_sort(&all_dpif_backers);
+    for (i = 0; i < shash_count(&all_dpif_backers); i++) {
+        dpif_reset_backer_stats(backers[i]->data);
+    }
+    free(backers);
+
+    unixctl_command_reply(conn, "DPIF statistics reset.");
 }
 
 static void
@@ -6628,6 +6671,8 @@ ofproto_unixctl_init(void)
                              ofproto_unixctl_dpif_dump_dps, NULL);
     unixctl_command_register("dpif/show", "", 0, 0, ofproto_unixctl_dpif_show,
                              NULL);
+    unixctl_command_register("dpif/reset-stats", "", 0, 0,
+                             ofproto_unixctl_dpif_reset_stats, NULL);
     unixctl_command_register("dpif/show-dp-features", "bridge", 1, 1,
                              ofproto_unixctl_dpif_show_dp_features, NULL);
     unixctl_command_register("dpif/dump-flows",

ofproto/ofproto-dpif.h
@@ -331,6 +331,11 @@ struct ofproto_dpif {
     struct ovs_mutex stats_mutex;
     struct netdev_stats stats OVS_GUARDED; /* To account packets generated and
                                             * consumed in userspace. */
+    struct pkt_stats stats_start; /* Packet stats starting point. Contains the
+                                   * 'up' ofproto stats when resetting from
+                                   * dpif perspective.
+                                   * Written and read from the main thread so
+                                   * no lock necessary. */
 
     /* Spanning tree. */
     struct stp *stp;

ofproto/ofproto-provider.h
@@ -91,6 +91,17 @@ pkt_stats_add(struct pkt_stats *dst, struct pkt_stats src)
                                            src.n_offload_bytes);
 }
 
+static inline void
+pkt_stats_sub(struct pkt_stats *dst, struct pkt_stats src)
+{
+    dst->n_packets = ovs_u64_safesub(dst->n_packets, src.n_packets);
+    dst->n_bytes = ovs_u64_safesub(dst->n_bytes, src.n_bytes);
+    dst->n_offload_packets = ovs_u64_safesub(dst->n_offload_packets,
+                                             src.n_offload_packets);
+    dst->n_offload_bytes = ovs_u64_safesub(dst->n_offload_bytes,
+                                           src.n_offload_bytes);
+}
+
 /* An OpenFlow switch.
  *
  * With few exceptions, ofproto implementations may look at these fields but

commit 13c1fd8a7b46e79115b09ff9c7c98ca72f67ebbf

ofproto: Fix uninitialized stats access 581309, 581310
581309 Uninitialized scalar variable
The variable will contain an arbitrary value left from earlier computations.
In dpif_reset_backer_stats: Use of an uninitialized variable (CWE-457)

581310 Uninitialized scalar variable
The variable will contain an arbitrary value left from earlier computations.
In dpif_show_backer: Use of an uninitialized variable (CWE-457)

Coverity issue: CID 581309
Coverity issue: CID 581310
Issue: OVS-3325592
Fixes: ab26f3866326 ("ofproto-dpif: Add a dpif stats reset function")
Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

ofproto/ofproto-dpif.c
@@ -6412,6 +6412,7 @@ dpif_show_backer(const struct dpif_backer *backer, struct ds *ds)
             double bytes_ratio;
             double pkts_ratio;
 
+            memset(&stats, 0, sizeof stats);
             ofproto_get_pkt_stats(&ofproto->up, &stats, NULL);
             pkt_stats_sub(&stats, ofproto->stats_start);
 
@@ -6475,6 +6476,7 @@ dpif_reset_backer_stats(struct dpif_backer *backer)
             continue;
         }
 
+        memset(&stats, 0, sizeof stats);
         ofproto_get_pkt_stats(&ofproto->up, &stats, NULL);
         ofproto->stats_start = stats;
     }