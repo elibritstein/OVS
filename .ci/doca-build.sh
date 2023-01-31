1c477a59ee6c15610d06b198dae8c8fe8a109720
Ariel Levkovich <lariel@nvidia.com>
2023-01-31 18:34:29 +0200

    acinclude.m4: Add '--with-doca' option.
    Add a new option to build ovs with doca by specifying '--with-doca' in the
    configure line.
    
    This flag must be used along with '--with-dpdk'. Otherwise the configure step
    will fail.
    
    An example:
    
    ./configure --prefix=/usr --localstatedir=/var --sysconfdir=/etc \
        --with-dpdk=static --with-doca=static
    
    Issue: OVS-3352409
    Co-authored-by: Salem Sol <salems@nvidia.com>
    Signed-off-by: Salem Sol <salems@nvidia.com>
    Change-Id: I39d274e4b5ebe043954680ea163316947dbe97c4
    Co-authored-by: Eli Britstein <elibr@nvidia.com>
    Signed-off-by: Eli Britstein <elibr@nvidia.com>
    Signed-off-by: Ariel Levkovich <lariel@nvidia.com>

---
+++ .ci/doca-build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -o errexit
+set -x
+
+CFLAGS_FOR_OVS="-g -O2"
+EXTRA_OPTS="--enable-Werror"
+JOBS=${JOBS:-"-j4"}
+
+DOCA_LINK="${DOCA_LINK:-static}"
+
+for pc_dir in $(find /opt/mellanox -name pkgconfig -type d 2>/dev/null); do
+    PKG_CONFIG_PATH="${pc_dir}:${PKG_CONFIG_PATH}"
+done
+export PKG_CONFIG_PATH
+
+if [ "$DOCA_LINK" = "shared" ]; then
+    DOCA_LIB=$(find /opt/mellanox -name pkgconfig -type d 2>/dev/null \
+               | head -1 | sed 's|/pkgconfig$||')
+    export LD_LIBRARY_PATH="${DOCA_LIB}:${LD_LIBRARY_PATH}"
+fi
+sudo ldconfig
+
+EXTRA_OPTS="$EXTRA_OPTS --with-dpdk=$DOCA_LINK --with-doca=$DOCA_LINK"
+
+if [ "$DOCA_LINK" = "shared" ]; then
+    EXTRA_OPTS="$EXTRA_OPTS --enable-shared"
+fi
+
+./boot.sh
+./configure CFLAGS="${CFLAGS_FOR_OVS}" $EXTRA_OPTS
+make $JOBS
+
+if ! vswitchd/ovs-vswitchd -V 2>&1 | grep -q 'DOCA'; then
+    echo "Expected 'DOCA' in ovs-vswitchd -V output for DOCA build." >&2
+    vswitchd/ovs-vswitchd -V || true
+    exit 1
+fi
+
+export DISTCHECK_CONFIGURE_FLAGS="$EXTRA_OPTS"
+make distcheck ${JOBS} CFLAGS="${CFLAGS_FOR_OVS}" \
+    TESTSUITEFLAGS="${JOBS} ${TEST_RANGE}" RECHECK=yes
