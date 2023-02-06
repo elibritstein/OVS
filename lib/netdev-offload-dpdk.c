elibritstein/OVS

commit 9860c1b12a211a1c2694cd1accfb68ddad2b8d5b

netdev-offload-dpdk: Support raw_encap header rewrite
Similarly to the previous commit (support vxlan header rewrite), add
support for raw_encap too.
With raw_encap, there is only a buffer of raw-data. Partially parse the
buffer and return fictive rte_flow_item pointer.

Issue: OVS-3342629
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -5413,6 +5413,61 @@ find_vxlan_spec(struct vxlan_data *vxlan_data, enum rte_flow_item_type type)
 }
 
 static void *
+find_raw_encap_spec(struct raw_encap_data *raw_encap_data,
+                    enum rte_flow_item_type type)
+{
+    struct ovs_16aligned_ip6_hdr *ipv6;
+    struct udp_header *udp = NULL;
+    struct vlan_header *vlan;
+    struct eth_header *eth;
+    struct ip_header *ipv4;
+    uint8_t *next_hdr;
+    uint16_t proto;
+
+    eth = (struct eth_header *) raw_encap_data->conf.data;
+    if (type == RTE_FLOW_ITEM_TYPE_ETH) {
+        return eth;
+    }
+
+    next_hdr = (uint8_t *) (eth + 1);
+    proto = htons(eth->eth_type);
+    /* VLAN skipping */
+    while (eth_type_vlan(ntohs(proto))) {
+        vlan = (struct vlan_header *) next_hdr;
+        proto = htons(vlan->vlan_next_type);
+        next_hdr += sizeof *vlan;
+    }
+
+    if (proto == RTE_ETHER_TYPE_IPV4) {
+        ipv4 = (struct ip_header *) next_hdr;
+        if (type == RTE_FLOW_ITEM_TYPE_IPV4) {
+            return ipv4;
+        }
+        if (ipv4->ip_proto != IPPROTO_UDP) {
+            return NULL;
+        }
+        udp = (struct udp_header *) (ipv4 + 1);
+    }
+
+    if (proto == RTE_ETHER_TYPE_IPV6) {
+        ipv6 = (struct ovs_16aligned_ip6_hdr *) next_hdr;
+        if (type == RTE_FLOW_ITEM_TYPE_IPV6) {
+            return ipv6;
+        }
+        if (ipv6->ip6_nxt != IPPROTO_UDP) {
+            return NULL;
+        }
+        udp = (struct udp_header *) (ipv6 + 1);
+    }
+
+    if (udp && type == RTE_FLOW_ITEM_TYPE_UDP) {
+        return udp;
+    }
+
+    return NULL;
+}
+
+static void *
 find_encap_spec(struct vxlan_data *vxlan_data,
                 struct raw_encap_data *raw_encap_data,
                 enum rte_flow_item_type type)
@@ -5426,9 +5481,12 @@ find_encap_spec(struct vxlan_data *vxlan_data,
         }
         return rv;
     }
-
     if (raw_encap_data) {
-        return NULL;
+        rv = find_raw_encap_spec(raw_encap_data, type);
+        if (rv == NULL) {
+            VLOG_DBG_RL(&rl, "Could not find raw_encap spec type=%d", type);
+        }
+        return rv;
     }
 
     OVS_NOT_REACHED();

commit 03e1f48e2b7fd0236691ea353b2d0682a8e1b5cf

netdev-offload-dpdk: Fix cast-align warnings in find_raw_encap_spec()
The function parses a raw header to layer headers and introduced the
below warnings. Fix them.

lib/netdev-offload-dpdk.c: In function 'find_raw_encap_spec':
lib/netdev-offload-dpdk.c:5427:11: error: cast increases required alignment of target type [-Werror=cast-align]
 5427 |     eth = (struct eth_header *) raw_encap_data->conf.data;
      |           ^
lib/netdev-offload-dpdk.c:5436:16: error: cast increases required alignment of target type [-Werror=cast-align]
 5436 |         vlan = (struct vlan_header *) next_hdr;
      |                ^
lib/netdev-offload-dpdk.c:5442:16: error: cast increases required alignment of target type [-Werror=cast-align]
 5442 |         ipv4 = (struct ip_header *) next_hdr;
      |                ^
lib/netdev-offload-dpdk.c:5453:16: error: cast increases required alignment of target type [-Werror=cast-align]
 5453 |         ipv6 = (struct ovs_16aligned_ip6_hdr *) next_hdr;
      |                ^

Issue: OVS-3356968
Fixes: 9860c1b12a21 ("netdev-offload-dpdk: Support raw_encap header rewrite")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -5424,7 +5424,7 @@ find_raw_encap_spec(struct raw_encap_data *raw_encap_data,
     uint8_t *next_hdr;
     uint16_t proto;
 
-    eth = (struct eth_header *) raw_encap_data->conf.data;
+    eth = ALIGNED_CAST(struct eth_header *, raw_encap_data->conf.data);
     if (type == RTE_FLOW_ITEM_TYPE_ETH) {
         return eth;
     }
@@ -5433,13 +5433,13 @@ find_raw_encap_spec(struct raw_encap_data *raw_encap_data,
     proto = htons(eth->eth_type);
     /* VLAN skipping */
     while (eth_type_vlan(ntohs(proto))) {
-        vlan = (struct vlan_header *) next_hdr;
+        vlan = ALIGNED_CAST(struct vlan_header *, next_hdr);
         proto = htons(vlan->vlan_next_type);
         next_hdr += sizeof *vlan;
     }
 
     if (proto == RTE_ETHER_TYPE_IPV4) {
-        ipv4 = (struct ip_header *) next_hdr;
+        ipv4 = ALIGNED_CAST(struct ip_header *, next_hdr);
         if (type == RTE_FLOW_ITEM_TYPE_IPV4) {
             return ipv4;
         }
@@ -5450,7 +5450,7 @@ find_raw_encap_spec(struct raw_encap_data *raw_encap_data,
     }
 
     if (proto == RTE_ETHER_TYPE_IPV6) {
-        ipv6 = (struct ovs_16aligned_ip6_hdr *) next_hdr;
+        ipv6 = ALIGNED_CAST(struct ovs_16aligned_ip6_hdr *, next_hdr);
         if (type == RTE_FLOW_ITEM_TYPE_IPV6) {
             return ipv6;
         }