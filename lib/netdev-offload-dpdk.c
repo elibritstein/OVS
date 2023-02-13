elibritstein/OVS

commit 07f330365200091e9a51a509c887b721c6747c84

netdev-offload-dpdk: Callbacks for priv ref/unref
Call new callbacks for ref/unref with the priv handle, as a pre-step
towards introducing default miss rule in tables.

Issue: 2110233
Change-Id: If0cac7df5b4ef1b1d3a14dbe74973c0ec3e5af0f
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/netdev-offload-dpdk.c
@@ -319,6 +319,10 @@ ufid_to_rte_flow_disassociate(struct ufid_to_rte_flow_data *data)
  * "id_alloc" is used to allocate an id for a new data.
  * "id_free" is used to free an id for the last data release.
  * "data_size" is the size of the data in the elements.
+ * "priv_size" is the size of the priv data in the elements. priv is just
+ *     allocated with the object, and used by users.
+ * "priv_ref" is called upon a reference to the priv.
+ * "priv_unref" is called upon a un-reference to the priv.
  */
 struct context_metadata {
     const char *name;
@@ -332,6 +336,9 @@ struct context_metadata {
     void (*id_free)(uint32_t id);
     size_t data_size;
     bool delayed_release;
+    size_t priv_size;
+    int (*priv_ref)(void *priv, void *priv_arg, uint32_t id);
+    void (*priv_unref)(void *priv);
 };
 
 struct context_release_item;
@@ -344,8 +351,10 @@ struct context_data {
     struct cmap_node associated_i2d_node;
     uint32_t associated_i2d_hash;
     void *data;
+    void *priv;
     uint32_t id;
     struct ovs_refcount refcount;
+    uint32_t priv_refcount;
 };
 
 static bool
@@ -372,10 +381,12 @@ context_data_ref(struct context_metadata *md,
 static int
 get_context_data_id_by_data(struct context_metadata *md,
                             struct context_data *data_req,
+                            void *priv_arg,
                             uint32_t *id)
 {
     struct context_data *data_cur;
     size_t dhash, ihash;
+    size_t data_size;
     struct ds s;
 
     ds_init(&s);
@@ -397,6 +408,20 @@ get_context_data_id_by_data(struct context_metadata *md,
                         data_cur->id);
             ds_destroy(&s);
             *id = data_cur->id;
+            if (md->priv_ref) {
+                ovs_mutex_lock(&md->maps_lock);
+                if (data_cur->priv_refcount == 0) {
+                    int ret;
+
+                    ret = md->priv_ref(data_cur->priv, priv_arg, *id);
+                    if (ret) {
+                        ovs_mutex_unlock(&md->maps_lock);
+                        return -1;
+                    }
+                }
+                data_cur->priv_refcount++;
+                ovs_mutex_unlock(&md->maps_lock);
+            }
             return 0;
         }
     }
@@ -405,17 +430,24 @@ get_context_data_id_by_data(struct context_metadata *md,
     if (!data_cur) {
         goto err;
     }
-    data_cur->data = xmalloc(md->data_size);
+    data_size = ROUND_UP(md->data_size, 8);
+    data_cur->data = xmalloc(data_size + md->priv_size);
     if (!data_cur->data) {
         goto err_data_alloc;
     }
+    data_cur->priv = (uint8_t *) data_cur->data + data_size;
     memcpy(data_cur->data, data_req->data, md->data_size);
     ovs_refcount_init(&data_cur->refcount);
+    data_cur->priv_refcount = 1;
     data_cur->id = md->id_alloc();
     if (data_cur->id == 0) {
         goto err_id_alloc;
     }
     ovs_mutex_lock(&md->maps_lock);
+    if (md->priv_ref && md->priv_ref(data_cur->priv, priv_arg, data_cur->id)) {
+        ovs_mutex_unlock(&md->maps_lock);
+        goto err_id_alloc;
+    }
     data_cur->d2i_hash = dhash;
     cmap_insert(&md->d2i_map, &data_cur->d2i_node, dhash);
     ihash = hash_add(0, data_cur->id);
@@ -571,6 +603,14 @@ context_delayed_release(struct context_metadata *md, uint32_t id,
     item->id = id;
     item->data = data;
     item->associated = associated;
+    if (md->priv_unref) {
+        ovs_mutex_lock(&md->maps_lock);
+        if (item->data->priv_refcount == 1) {
+            md->priv_unref(item->data->priv);
+        }
+        item->data->priv_refcount--;
+        ovs_mutex_unlock(&md->maps_lock);
+    }
     if (!md->delayed_release) {
         context_release(item);
         return;
@@ -858,7 +898,7 @@ get_label_id(ovs_u128 *ct_label, uint32_t *ct_label_id)
         *ct_label_id = 0;
         return 0;
     }
-    return get_context_data_id_by_data(&label_id_md, &label_id_context,
+    return get_context_data_id_by_data(&label_id_md, &label_id_context, NULL,
                                        ct_label_id);
 }
 
@@ -929,7 +969,7 @@ get_zone_id(uint16_t ct_zone, uint32_t *ct_zone_id)
         .data = &ct_zone,
     };
 
-    return get_context_data_id_by_data(&zone_id_md, &zone_id_context,
+    return get_context_data_id_by_data(&zone_id_md, &zone_id_context, NULL,
                                        ct_zone_id);
 }
 
@@ -1003,7 +1043,7 @@ get_table_id(odp_port_t vport, uint32_t recirc_id, uint32_t *table_id)
         return 0;
     }
 
-    return get_context_data_id_by_data(&table_id_md, &table_id_context,
+    return get_context_data_id_by_data(&table_id_md, &table_id_context, NULL,
                                        table_id);
 }
 
@@ -1083,7 +1123,8 @@ get_ct_ctx_id(struct ct_miss_ctx *ct_miss_ctx_data, uint32_t *ct_ctx_id)
         .data = ct_miss_ctx_data,
     };
 
-    return get_context_data_id_by_data(&ct_miss_ctx_md, &ct_ctx, ct_ctx_id);
+    return get_context_data_id_by_data(&ct_miss_ctx_md, &ct_ctx, NULL,
+                                       ct_ctx_id);
 }
 
 static void
@@ -1190,7 +1231,7 @@ get_tnl_id(struct flow_tnl *tnl_key, struct flow_tnl *tnl_mask,
         *tnl_id = 0;
         return 0;
     }
-    return get_context_data_id_by_data(&tnl_md, &tnl_ctx, tnl_id);
+    return get_context_data_id_by_data(&tnl_md, &tnl_ctx, NULL, tnl_id);
 }
 
 static void
@@ -1239,7 +1280,7 @@ get_flow_miss_ctx_id(struct flow_miss_ctx *flow_ctx_data,
         .data = flow_ctx_data,
     };
 
-    return get_context_data_id_by_data(&flow_miss_ctx_md, &flow_ctx,
+    return get_context_data_id_by_data(&flow_miss_ctx_md, &flow_ctx, NULL,
                                        miss_ctx_id);
 }
 

commit 4ff9d2dbba5ea45fffbb700bacbec7f9d1961b9b

netdev-offload-dpdk: Fix context-metadata priv references
The priv refcount is meant to be exactly aligned on the
data refcount, however by using two separate release time,
it can differ.

  data  priv
     1     1    first alloc
     n     n    nth alloc
     n   n-1    delayed put
   n+1     n    re-alloc

To prevent this issue completely, use a single refcount
between data and priv. The callbacks 'priv_ref' and 'priv_unref'
are misnamed: they are not taking any ref on priv_refcount, but
initializing and uninitializing the priv on first and last
priv_refcount change.

So, the priv callbacks are also renamed, and receive additional
constraints: it must be possible to call priv_init any number
of time and only the first call should allocate anything.

Both table and flow_miss_ctx are impacted.

Fixes: 07f330365200 ("netdev-offload-dpdk: Callbacks for priv ref/unref")
Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -455,8 +455,12 @@ ufid_to_rte_flow_disassociate(struct ufid_to_rte_flow_data *data)
  * "data_size" is the size of the data in the elements.
  * "priv_size" is the size of the priv data in the elements. priv is just
  *     allocated with the object, and used by users.
- * "priv_ref" is called upon a reference to the priv.
- * "priv_unref" is called upon a un-reference to the priv.
+ * "priv_init" is called upon the first reference to a priv.
+ * "priv_uninit" is called upon the last dereference to a priv.
+ *               It should be possible to then call 'priv_init' again
+ *               to re-initialize the priv in a correct state.
+ * "priv_init_done" is used to protect against multiple calls to
+ *                  priv_init.
  */
 struct context_metadata {
     const char *name;
@@ -471,8 +475,9 @@ struct context_metadata {
     size_t data_size;
     bool delayed_release;
     size_t priv_size;
-    int (*priv_ref)(void *priv, void *priv_arg, uint32_t id);
-    void (*priv_unref)(void *priv);
+    int (*priv_init)(void *priv, void *priv_arg, uint32_t id);
+    void (*priv_uninit)(void *priv);
+    bool priv_init_done;
 };
 
 struct context_release_item;
@@ -488,7 +493,6 @@ struct context_data {
     void *priv;
     uint32_t id;
     struct ovs_refcount refcount;
-    uint32_t priv_refcount;
 };
 
 static int
@@ -522,18 +526,16 @@ get_context_data_id_by_data(struct context_metadata *md,
                         data_cur->id);
             ds_destroy(&s);
             *id = data_cur->id;
-            if (md->priv_ref) {
-                ovs_mutex_lock(&md->maps_lock);
-                if (data_cur->priv_refcount == 0) {
-                    int ret;
+            if (md->priv_init && !md->priv_init_done) {
+                int ret;
 
-                    ret = md->priv_ref(data_cur->priv, priv_arg, *id);
-                    if (ret) {
-                        ovs_mutex_unlock(&md->maps_lock);
-                        return -1;
-                    }
+                ovs_mutex_lock(&md->maps_lock);
+                ret = md->priv_init(data_cur->priv, priv_arg, *id);
+                if (ret) {
+                    ovs_mutex_unlock(&md->maps_lock);
+                    return -1;
                 }
-                data_cur->priv_refcount++;
+                md->priv_init_done = true;
                 ovs_mutex_unlock(&md->maps_lock);
             }
             return 0;
@@ -556,13 +558,13 @@ get_context_data_id_by_data(struct context_metadata *md,
     data_cur->priv = (uint8_t *) data_cur->data + data_size;
     memcpy(data_cur->data, data_req->data, md->data_size);
     ovs_refcount_init(&data_cur->refcount);
-    data_cur->priv_refcount = 1;
     data_cur->id = alloc_id;
     ovs_mutex_lock(&md->maps_lock);
-    if (md->priv_ref && md->priv_ref(data_cur->priv, priv_arg, alloc_id)) {
+    if (md->priv_init && md->priv_init(data_cur->priv, priv_arg, alloc_id)) {
         ovs_mutex_unlock(&md->maps_lock);
         goto err_priv_ref;
     }
+    md->priv_init_done = true;
     data_cur->d2i_hash = dhash;
     cmap_insert(&md->d2i_map, &data_cur->d2i_node, dhash);
     ihash = hash_add(0, data_cur->id);
@@ -731,13 +733,17 @@ context_delayed_release(struct context_metadata *md, uint32_t id,
     item->id = id;
     item->data = data;
     item->associated = associated;
-    if (md->priv_unref) {
-        ovs_mutex_lock(&md->maps_lock);
-        if (item->data->priv_refcount == 1) {
-            md->priv_unref(item->data->priv);
+    if (md->priv_uninit && md->priv_init_done) {
+        if (ovs_refcount_read(&data->refcount) == 1) {
+            /* Immediately uninit the priv, even if the
+             * data release is delayed. If another object takes
+             * a ref on the data, the priv will then be re-initialized.
+             */
+            ovs_mutex_lock(&md->maps_lock);
+            md->priv_uninit(item->data->priv);
+            md->priv_init_done = false;
+            ovs_mutex_unlock(&md->maps_lock);
         }
-        item->data->priv_refcount--;
-        ovs_mutex_unlock(&md->maps_lock);
     }
     if (!md->delayed_release) {
         context_release(item);
@@ -1181,13 +1187,15 @@ get_table_id(odp_port_t vport,
              uint32_t *table_id);
 
 static int
-table_id_ctx_ref(void *priv_, void *priv_arg_, uint32_t table_id)
+table_id_ctx_init(void *priv_, void *priv_arg_, uint32_t table_id)
 {
     struct table_id_data *priv_arg = priv_arg_;
     struct table_id_ctx_priv *priv = priv_;
     uint32_t e2e_table_id;
 
-    priv->netdev = NULL;
+    if (priv->netdev != NULL) {
+        return 0;
+    }
 
     if (!netdev_is_e2e_cache_enabled() || priv_arg->recirc_id != 0) {
        return 0;
@@ -1209,7 +1217,7 @@ table_id_ctx_ref(void *priv_, void *priv_arg_, uint32_t table_id)
 }
 
 static void
-table_id_ctx_unref(void *priv_)
+table_id_ctx_uninit(void *priv_)
 {
     struct table_id_ctx_priv *priv = priv_;
 
@@ -1219,6 +1227,7 @@ table_id_ctx_unref(void *priv_)
 
     netdev_offload_dpdk_destroy_flow(priv->netdev, priv->miss_flow, NULL, true);
     netdev_close(priv->netdev);
+    priv->netdev = NULL;
 }
 
 static struct context_metadata table_id_md = {
@@ -1231,8 +1240,8 @@ static struct context_metadata table_id_md = {
     .id_free = table_id_free,
     .data_size = sizeof(struct table_id_data),
     .priv_size = sizeof(struct table_id_ctx_priv),
-    .priv_ref = table_id_ctx_ref,
-    .priv_unref = table_id_ctx_unref,
+    .priv_init = table_id_ctx_init,
+    .priv_uninit = table_id_ctx_uninit,
 };
 
 static int
@@ -1583,10 +1592,14 @@ struct flow_miss_ctx_priv {
 };
 
 static int
-flow_miss_ctx_ref(void *priv_, void *priv_arg_, uint32_t mark_id)
+flow_miss_ctx_init(void *priv_, void *priv_arg_, uint32_t mark_id)
 {
     struct flow_miss_ctx_priv_arg *priv_arg = priv_arg_;
     struct flow_miss_ctx_priv *priv = priv_;
+
+    if (priv->netdev != NULL) {
+        return 0;
+    }
 
     priv->netdev = netdev_ref(priv_arg->netdev);
     priv->miss_flow = add_miss_flow(priv->netdev, priv_arg->table_id,
@@ -1602,7 +1615,7 @@ flow_miss_ctx_ref(void *priv_, void *priv_arg_, uint32_t mark_id)
 }
 
 static void
-flow_miss_ctx_unref(void *priv_)
+flow_miss_ctx_uninit(void *priv_)
 {
     struct flow_miss_ctx_priv *priv = priv_;
 
@@ -1612,6 +1625,7 @@ flow_miss_ctx_unref(void *priv_)
 
     netdev_offload_dpdk_destroy_flow(priv->netdev, priv->miss_flow, NULL, true);
     netdev_close(priv->netdev);
+    priv->netdev = NULL;
 }
 
 static struct context_metadata flow_miss_ctx_md = {
@@ -1627,8 +1641,8 @@ static struct context_metadata flow_miss_ctx_md = {
     .data_size = sizeof(struct flow_miss_ctx),
     .delayed_release = true,
     .priv_size = sizeof(struct flow_miss_ctx_priv),
-    .priv_ref = flow_miss_ctx_ref,
-    .priv_unref = flow_miss_ctx_unref,
+    .priv_init = flow_miss_ctx_init,
+    .priv_uninit = flow_miss_ctx_uninit,
 };
 
 static int