be0f879e9fd89700a1251b48a2171ba6de1ca283
Gaetan Rivet <gaetanr@nvidia.com>
2023-02-15 19:59:55 +0100

    util: Introduce OVS_ASSERT_PACKED macro
    Add a macro to statically assert whether a structure contains padding.
    
    Issue: OVS-3352411
    Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

--- lib/util.h
+++ lib/util.h
@@ -133,6 +133,18 @@ ovs_prefetch_range(const void *start, size_t size)
 typedef uint32_t HANDLE;
 #endif
 
+/* Define a type with this macro to enforce during
+ * compilation that it has no padding, i.e. packed.
+ * It *DOES NOT* pack the type, only statically check
+ * that it is already. Either the layout must be
+ * made to fit, or explicit padding must be used.
+ */
+#define OVS_ASSERT_PACKED(TYPE, MEMBERS) \
+    TYPE { \
+        MEMBERS \
+    }; \
+    BUILD_ASSERT_DECL(sizeof(TYPE) == sizeof(OVS_PACKED(struct { MEMBERS })))
+
 #ifdef  __cplusplus
 extern "C" {
 #endif
