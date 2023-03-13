elibritstein/OVS

commit 1a9c166423355c7780ab6c51037b371b66526858

dpdk-offload-doca: Introduce pipe_ctx handling
Implement pipe_ctx logic to manage creation and deletion of control_pipes,
map each group_id to a control_pipe.

The pipe handles are managed by a ref_count.

Signed-off-by: Salem Sol <salems@nvidia.com>

lib/dpdk-offload-doca.c
@@ -23,6 +23,130 @@
 #include "ovs-doca.h"
 #include "dp-packet.h"
 #include "dpdk-offload-provider.h"
+#include "openvswitch/vlog.h"
+#include "offload-metadata.h"
+#include "util.h"
+
+VLOG_DEFINE_THIS_MODULE(dpdk_offload_doca);
+
+OVS_ASSERT_PACKED(struct doca_ctl_pipe_key,
+    uint32_t group_id;
+    uint32_t esw_mgr_port_id;
+);
+
+struct doca_ctl_pipe_ctx {
+    struct netdev *netdev;
+    struct doca_flow_pipe *pipe;
+};
+
+struct doca_ctl_pipe_arg {
+    struct netdev *netdev;
+    struct doca_flow_pipe_cfg cfg;
+};
+
+static int
+doca_ctl_pipe_ctx_init(void *ctx_, void *arg_, uint32_t id OVS_UNUSED)
+{
+    struct doca_ctl_pipe_ctx *ctx = ctx_;
+    struct doca_ctl_pipe_arg *arg = arg_;
+    int ret;
+
+    ret = doca_flow_pipe_create(&arg->cfg, NULL, NULL, &ctx->pipe);
+    if (ret) {
+        VLOG_ERR("%s: Failed to create ctl pipe: %d (%s)",
+                 netdev_get_name(arg->netdev), ret, doca_get_error_string(ret));
+    }
+    return ret;
+}
+
+static void
+doca_ctl_pipe_ctx_uninit(void *ctx_)
+{
+    struct doca_ctl_pipe_ctx *ctx = ctx_;
+
+    doca_flow_pipe_destroy(ctx->pipe);
+    ctx->pipe = NULL;
+}
+
+static struct ds *
+dump_doca_ctl_pipe_ctx(struct ds *s, void *key_, void *ctx_, void *arg_ OVS_UNUSED)
+{
+    struct doca_ctl_pipe_key *key = key_;
+    struct doca_ctl_pipe_ctx *ctx = ctx_;
+
+    if (ctx) {
+        ds_put_format(s, "pipe=%p, ", ctx->pipe);
+    }
+    ds_put_format(s, "group_id=%"PRIu32", ", key->group_id);
+
+    return s;
+}
+
+static struct offload_metadata *doca_ctl_pipe_md;
+
+static void
+doca_ctl_pipe_md_init(void)
+{
+    static struct ovsthread_once init_once = OVSTHREAD_ONCE_INITIALIZER;
+
+    if (ovsthread_once_start(&init_once)) {
+        struct offload_metadata_parameters params = {
+            .priv_size = sizeof(struct doca_ctl_pipe_ctx),
+            .priv_init = doca_ctl_pipe_ctx_init,
+            .priv_uninit = doca_ctl_pipe_ctx_uninit,
+        };
+        unsigned int nb_thread = netdev_offload_thread_nb();
+
+        doca_ctl_pipe_md = offload_metadata_create(nb_thread, "doca_ctl_pipe",
+                                                   sizeof(struct doca_ctl_pipe_key),
+                                                   dump_doca_ctl_pipe_ctx,
+                                                   params);
+
+        ovsthread_once_done(&init_once);
+    }
+}
+
+OVS_UNUSED
+static struct doca_ctl_pipe_ctx *
+doca_ctl_pipe_ctx_ref(struct netdev *netdev,
+                      uint32_t group_id)
+{
+    struct doca_ctl_pipe_key key = {
+        .group_id = group_id,
+        .esw_mgr_port_id = netdev_dpdk_get_esw_mgr_port_id(netdev),
+    };
+    struct doca_ctl_pipe_arg arg = {
+        .netdev = netdev,
+    };
+    char pipe_name[50];
+    bool is_root;
+
+    /* The pipe for recirc = 0 without any tunnel involved is
+     * global and shared among devices on the esw. It is a root pipe.
+     */
+    is_root = (group_id == 0);
+    snprintf(pipe_name, sizeof pipe_name, "OVS_CTL_PIPE_%" PRIu32, group_id);
+
+    memset(&arg.cfg, 0, sizeof arg.cfg);
+    arg.cfg.attr.name = pipe_name;
+    arg.cfg.attr.type = DOCA_FLOW_PIPE_CONTROL;
+    arg.cfg.attr.is_root = is_root;
+    arg.cfg.port = doca_flow_port_switch_get();
+
+    doca_ctl_pipe_md_init();
+    return offload_metadata_priv_get(doca_ctl_pipe_md, &key, &arg, NULL, true);
+}
+
+OVS_UNUSED
+static void
+doca_ctl_pipe_ctx_unref(struct doca_ctl_pipe_ctx *ctx)
+{
+    doca_ctl_pipe_md_init();
+    offload_metadata_priv_unref(doca_ctl_pipe_md,
+                                netdev_offload_thread_id(),
+                                ctx);
+}
+
 
 static struct reg_field *
 dpdk_offload_doca_get_reg_fields(void)

commit 8a1c163e7561f33f90242b16dd97a6cfe49951b1

dpdk-offload-doca: Remove netdev from pipe ctx
The 'netdev' field in doca_ctl_pipe_ctx is never set.
The one place where it is used is made simpler by accessing it through
parameters. Not keeping the netdev within the pipe ctx is also
arguably more correct, as we don't take a reference on it
and it should not be stored if possible.

Fixes: 1a9c16642335 ("dpdk-offload-doca: Introduce pipe_ctx handling")
Fixes: 06c5162fb242 ("dpdk-offload-doca: Implement DOCA provider")
Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

lib/dpdk-offload-doca.c
@@ -45,7 +45,6 @@ OVS_ASSERT_PACKED(struct doca_ctl_pipe_key,
 );
 
 struct doca_ctl_pipe_ctx {
-    struct netdev *netdev;
     struct doca_flow_pipe *pipe;
 };
 
@@ -574,7 +573,8 @@ doca_translate_actions(struct netdev *netdev OVS_UNUSED,
 }
 
 static struct doca_flow_pipe_entry *
-create_doca_flow_entry(struct doca_ctl_pipe_ctx *self_pipe,
+create_doca_flow_entry(struct netdev *netdev,
+                       struct doca_ctl_pipe_ctx *self_pipe,
                        uint32_t prio,
                        struct doca_flow_match *spec,
                        struct doca_flow_match *mask,
@@ -586,7 +586,6 @@ create_doca_flow_entry(struct doca_ctl_pipe_ctx *self_pipe,
 {
     unsigned int tid = netdev_offload_thread_id();
     struct doca_flow_pipe *pipe = self_pipe->pipe;
-    struct netdev *netdev = self_pipe->netdev;
     struct doca_flow_pipe_entry *entry;
     doca_error_t err;
 
@@ -651,7 +650,7 @@ dpdk_offload_doca_create(struct netdev *netdev,
 
     /* insert rule */
     prio = (hndl->next_group == MISS_TABLE_ID);
-    hndl->flow = create_doca_flow_entry(pipe_ctx, prio, &spec, &mask,
+    hndl->flow = create_doca_flow_entry(netdev, pipe_ctx, prio, &spec, &mask,
                                         &dacts, &dacts_descs, &monitor, &fwd, error);
     if (!hndl->flow) {
         error->type = RTE_FLOW_ERROR_TYPE_HANDLE;