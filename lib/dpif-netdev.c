elibritstein/OVS

commit 60187817e700e24381da49fd40ba075cc29725a2

dpif-netdev: Switch to CT offload API
Now that the netdev-offload are implemented in
at least one provider, switch to the dedicated CT offload API
to offload connections.

Issue: OVS-3267621
Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

lib/dpif-netdev.c
@@ -3917,22 +3917,32 @@ dp_netdev_ct_add(struct ct_flow_offload_item *ct_offload,
 }
 
 static int
-dp_netdev_ct_offload_del(struct ct_flow_offload_item *ct_offload)
+dp_netdev_ct_offload(struct ct_flow_offload_item *ct_offload)
 {
     struct dp_netdev *dp = ct_offload->dp;
     const char *dpif_type_str = dpif_normalize_type(dp->class->type);
-    struct netdev *port;
+    struct netdev *netdev;
     int ret;
 
-    port = netdev_ports_get(ct_offload->ct_match.odp_port, dpif_type_str);
-    if (!port) {
+    netdev = netdev_ports_get(ct_offload->ct_match.odp_port, dpif_type_str);
+    if (OVS_UNLIKELY(!netdev)) {
         return ENODEV;
     }
 
     ovs_rwlock_rdlock(&dp->port_rwlock);
-    ret = netdev_flow_del(port, &ct_offload->ufid, NULL);
+    switch (ct_offload->op) {
+    case DP_NETDEV_FLOW_OFFLOAD_OP_ADD:
+        ret = netdev_conn_add(netdev, ct_offload);
+        break;
+    case DP_NETDEV_FLOW_OFFLOAD_OP_DEL:
+        ret = netdev_conn_del(netdev, ct_offload);
+        break;
+    case DP_NETDEV_FLOW_OFFLOAD_OP_MOD:
+    default:
+        OVS_NOT_REACHED();
+    }
     ovs_rwlock_unlock(&dp->port_rwlock);
-    netdev_close(port);
+    netdev_close(netdev);
 
     return ret;
 }
@@ -3965,13 +3975,11 @@ dp_offload_ct(struct dp_offload_thread_item *item)
         switch (ct_offload[dir].op) {
         case DP_NETDEV_FLOW_OFFLOAD_OP_ADD:
             op = "add";
-            ret = dp_netdev_ct_add(&ct_offload[dir],
-                                   dp_netdev_ct_offload_add_cb);
-
+            ret = dp_netdev_ct_offload(&ct_offload[dir]);
             break;
         case DP_NETDEV_FLOW_OFFLOAD_OP_DEL:
             op = "delete";
-            ret = dp_netdev_ct_offload_del(&ct_offload[dir]);
+            ret = dp_netdev_ct_offload(&ct_offload[dir]);
             if (ret == ENODEV) {
                 /* If the port was previously deleted, its offloads
                  * have been flushed. Count as deletion. */
@@ -4270,27 +4278,28 @@ dp_netdev_ct_offload_del_item(struct ct_flow_offload_item *ct_offload)
 }
 
 static int
-dpif_netdev_get_flow_offload_status(const struct dp_netdev *dp,
-                                    struct dp_netdev_flow *netdev_flow,
-                                    struct dpif_flow_stats *stats,
-                                    struct dpif_flow_attrs *attrs,
-                                    long long now,
-                                    long long prev_now);
-static int
 dp_netdev_ct_offload_active(struct ct_flow_offload_item *offload,
                             long long now, long long prev_now)
 {
-    struct dp_netdev_flow netdev_flow;
     struct dpif_flow_stats stats;
-    struct dpif_flow_attrs attrs;
+    const struct dp_netdev *dp;
+    struct netdev *netdev;
     int ret;
 
-    memset(&netdev_flow, 0, sizeof netdev_flow);
-    *CONST_CAST(odp_port_t *, &netdev_flow.flow.in_port.odp_port) =
-        offload->ct_match.odp_port;
-    *CONST_CAST(ovs_u128 *, &netdev_flow.mega_ufid) = offload->ufid;
-    ret = dpif_netdev_get_flow_offload_status(offload->dp, &netdev_flow,
-                                              &stats, &attrs, now, prev_now);
+    if (!netdev_is_flow_api_enabled()) {
+        return EINVAL;
+    }
+
+    dp = offload->dp;
+    netdev = netdev_ports_get(offload->ct_match.odp_port,
+                              dpif_normalize_type(dp->class->type));
+    if (!netdev) {
+        return ENODEV;
+    }
+
+    ret = netdev_conn_stats(netdev, offload, &stats, NULL, now);
+    netdev_close(netdev);
+
     if (ret) {
         return ret;
     }

commit cfb8b5dc1166b546611d9f59b3fe665e7a8e942f

dpif-netdev: Query e2e-cache stats if MT query fails
With the new ct-offload-api, the offloaded connection stats is queried only
using their MT ufids.
e2e-cache flows ufids are different from their MT coutnerparts and require a
recalc of the ufid before query, this was previously done in
e2e_cache_get_merged_flows_stats() but the call to this function was removed
when we moved to ct-offload-api. Fix it.

Issue: OVS-3387634
Fixes: 60187817e ("dpif-netdev: Switch to CT offload API")
Signed-off-by: Salem Sol <salems@nvidia.com>

lib/dpif-netdev.c
@@ -4292,8 +4292,19 @@ dp_netdev_ct_offload_active(struct ct_flow_offload_item *offload,
     }
 
     ret = netdev_conn_stats(netdev, offload, &stats, NULL, now);
-    netdev_close(netdev);
+    if (ret && dp_netdev_e2e_cache_enabled) {
+        /* We failed to find the conn in multi-table offloads and e2e-cache is enabled.
+         * Query e2e as well. */
+        if (e2e_cache_get_merged_flows_stats(netdev, NULL, NULL,
+                                             &offload->ufid, &stats, NULL,
+                                             now, prev_now)) {
+            /* Query was successful, this conn is within the e2e merged-flow DB.
+             * 'stats' contains valid info now. */
+            ret = 0;
+        }
+    }
 
+    netdev_close(netdev);
     if (ret) {
         return ret;
     }