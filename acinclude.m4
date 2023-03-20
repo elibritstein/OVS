elibritstein/OVS

commit 2a489a602e06c82e26c337a7d9293ce238d28bf7

tc: Add vxlan gbp option flower match offload
Add TC offload support for filtering vxlan tunnels with gbp option

Issue: 3309899
Change-Id: Ifd051f02d44b36bbcaa13063a2fb22adc0e22794
Signed-off-by: Gavin Li <gavinl@nvidia.com>
Reviewed-by: Gavi Teitz <gavi@nvidia.com>

include/linux/pkt_cls.h
@@ -270,6 +270,10 @@ enum {
 					 * TCA_TUNNEL_KEY_ENC_OPTS_GENEVE
 					 * attributes
 					 */
+	TCA_FLOWER_KEY_ENC_OPTS_VXLAN,  /* Nested
+					 * TCA_TUNNEL_KEY_ENC_OPTS_VXLAN
+					 * attributes
+					 */
 	__TCA_FLOWER_KEY_ENC_OPTS_MAX,
 };
 
@@ -286,6 +290,15 @@ enum {
 
 #define TCA_FLOWER_KEY_ENC_OPT_GENEVE_MAX \
 		(__TCA_FLOWER_KEY_ENC_OPT_GENEVE_MAX - 1)
+
+enum {
+	TCA_FLOWER_KEY_ENC_OPT_VXLAN_UNSPEC,
+	TCA_FLOWER_KEY_ENC_OPT_VXLAN_GBP,               /* u32 */
+	__TCA_FLOWER_KEY_ENC_OPT_VXLAN_MAX,
+};
+
+#define TCA_FLOWER_KEY_ENC_OPT_VXLAN_MAX \
+		(__TCA_FLOWER_KEY_ENC_OPT_VXLAN_MAX - 1)
 
 enum {
 	TCA_FLOWER_KEY_FLAGS_IS_FRAGMENT = (1 << 0),

lib/netdev-offload-tc.c
@@ -1076,6 +1076,15 @@ parse_tc_flower_to_match(const struct netdev *netdev,
             match_set_tun_tp_dst_masked(match, flower->key.tunnel.tp_dst,
                                         flower->mask.tunnel.tp_dst);
         }
+        if (flower->mask.tunnel.gbp.id) {
+            match_set_tun_gbp_id_masked(match, flower->key.tunnel.gbp.id,
+                                        flower->mask.tunnel.gbp.id);
+        }
+        if (flower->mask.tunnel.gbp.flags) {
+            match_set_tun_gbp_flags_masked(match,
+                                           flower->key.tunnel.gbp.flags,
+                                           flower->mask.tunnel.gbp.flags);
+        }
 
         if (!strcmp(netdev_get_type(netdev), "geneve")) {
             flower_tun_opt_to_match(match, flower);
@@ -2304,6 +2313,9 @@ netdev_tc_flow_put(struct netdev *netdev, struct match *match,
         flower.key.tunnel.ttl = tnl->ip_ttl;
         flower.key.tunnel.tp_src = tnl->tp_src;
         flower.key.tunnel.tp_dst = tnl->tp_dst;
+        flower.key.tunnel.gbp.id = tnl->gbp_id;
+        flower.key.tunnel.gbp.flags = tnl->gbp_flags;
+        flower.key.tunnel.gbp.id_present = !!tnl_mask->gbp_id;
 
         flower.mask.tunnel.ipv4.ipv4_src = tnl_mask->ip_src;
         flower.mask.tunnel.ipv4.ipv4_dst = tnl_mask->ip_dst;
@@ -2318,6 +2330,9 @@ netdev_tc_flow_put(struct netdev *netdev, struct match *match,
          * Degrading the flow down to exact match for now as a workaround. */
         flower.mask.tunnel.tp_dst = OVS_BE16_MAX;
         flower.mask.tunnel.id = (tnl->flags & FLOW_TNL_F_KEY) ? tnl_mask->tun_id : 0;
+        flower.mask.tunnel.gbp.id = tnl_mask->gbp_id;
+        flower.mask.tunnel.gbp.flags = tnl_mask->gbp_flags;
+        flower.mask.tunnel.gbp.id_present = !!tnl_mask->gbp_id;
 
         memset(&tnl_mask->ip_src, 0, sizeof tnl_mask->ip_src);
         memset(&tnl_mask->ip_dst, 0, sizeof tnl_mask->ip_dst);
@@ -2329,6 +2344,8 @@ netdev_tc_flow_put(struct netdev *netdev, struct match *match,
         memset(&tnl_mask->tp_dst, 0, sizeof tnl_mask->tp_dst);
 
         memset(&tnl_mask->tun_id, 0, sizeof tnl_mask->tun_id);
+        memset(&tnl_mask->gbp_id, 0, sizeof tnl_mask->gbp_id);
+        memset(&tnl_mask->gbp_flags, 0, sizeof tnl_mask->gbp_flags);
         tnl_mask->flags &= ~FLOW_TNL_F_KEY;
 
         /* XXX: This is wrong!  We're ignoring DF and CSUM flags configuration

lib/tc.c
@@ -39,6 +39,7 @@
 #include "byte-order.h"
 #include "netlink-socket.h"
 #include "netlink.h"
+#include "odp-util.h"
 #include "openvswitch/ofpbuf.h"
 #include "openvswitch/util.h"
 #include "openvswitch/vlog.h"
@@ -698,6 +699,38 @@ nl_parse_geneve_key(const struct nlattr *in_nlattr,
 }
 
 static int
+nl_parse_vxlan_key(const struct nlattr *in_nlattr,
+                   struct tc_flower_tunnel *tunnel)
+{
+    const struct ofpbuf *msg;
+    struct nlattr *nla;
+    struct ofpbuf buf;
+    uint32_t gbp_raw;
+    size_t left;
+
+    nl_attr_get_nested(in_nlattr, &buf);
+    msg = &buf;
+
+    NL_ATTR_FOR_EACH (nla, left, ofpbuf_at(msg, 0, 0), msg->size) {
+        uint16_t type = nl_attr_type(nla);
+
+        switch (type) {
+        case TCA_FLOWER_KEY_ENC_OPT_VXLAN_GBP:
+            gbp_raw = nl_attr_get_u32(nla);
+            odp_decode_gbp_raw(gbp_raw, &tunnel->gbp.id,
+                               &tunnel->gbp.flags);
+            tunnel->gbp.id_present = true;
+            break;
+        default:
+            VLOG_ERR_RL(&error_rl, "failed to parse vxlan tun options");
+            return EINVAL;
+        }
+    }
+
+    return 0;
+}
+
+static int
 nl_parse_flower_tunnel_opts(struct nlattr *options,
                             struct tc_flower_tunnel *tunnel)
 {
@@ -715,6 +748,13 @@ nl_parse_flower_tunnel_opts(struct nlattr *options,
         switch (type) {
         case TCA_FLOWER_KEY_ENC_OPTS_GENEVE:
             err = nl_parse_geneve_key(nla, &tunnel->metadata);
+            if (err) {
+                return err;
+            }
+
+            break;
+        case TCA_FLOWER_KEY_ENC_OPTS_VXLAN:
+            err = nl_parse_vxlan_key(nla, tunnel);
             if (err) {
                 return err;
             }
@@ -3037,11 +3077,12 @@ nl_msg_put_flower_tunnel_opts(struct ofpbuf *request, uint16_t type,
     int len, cnt = 0;
 
     len = metadata->present.len;
-    if (!len) {
+    if (!len && !tunnel->gbp.id_present) {
         return;
     }
 
     outer = nl_msg_start_nested(request, type);
+    /* Geneve */
     while (len) {
         opt = &metadata->opts.gnv[cnt];
         inner = nl_msg_start_nested(request, TCA_FLOWER_KEY_ENC_OPTS_GENEVE);
@@ -3055,6 +3096,17 @@ nl_msg_put_flower_tunnel_opts(struct ofpbuf *request, uint16_t type,
         cnt += sizeof(struct geneve_opt) / 4 + opt->length;
         len -= sizeof(struct geneve_opt) + opt->length * 4;
 
+        nl_msg_end_nested(request, inner);
+    }
+
+    /* VxLAN GBP */
+    if (tunnel->gbp.id_present) {
+        uint32_t gbp_raw;
+
+        gbp_raw = odp_encode_gbp_raw(tunnel->gbp.flags, tunnel->gbp.id);
+        inner = nl_msg_start_nested(request, TCA_FLOWER_KEY_ENC_OPTS_VXLAN);
+
+        nl_msg_put_u32(request, TCA_FLOWER_KEY_ENC_OPT_VXLAN_GBP, gbp_raw);
         nl_msg_end_nested(request, inner);
     }
     nl_msg_end_nested(request, outer);

lib/tc.h
@@ -91,6 +91,12 @@ struct tc_cookie {
     size_t len;
 };
 
+struct tc_tunnel_gbp {
+    ovs_be16 id;
+    uint8_t flags;
+    bool id_present;
+};
+
 struct tc_flower_tunnel {
     struct {
         ovs_be32 ipv4_src;
@@ -104,6 +110,7 @@ struct tc_flower_tunnel {
     uint8_t ttl;
     ovs_be16 tp_src;
     ovs_be16 tp_dst;
+    struct tc_tunnel_gbp gbp;
     ovs_be64 id;
     struct tun_metadata metadata;
 };

commit fe48a27d42fac6b72df3921725ba3476a33da89d

inlcude/pkt_cls: Check for VxLAN GBP definitions
Some kernels in use by MLNX_OFED have not followed the kernel upstream
when porting various features to pkt_cls.h. This breaks OVS's checkpoint
methodology when determining whether to use the kernel's header file or
the local compat file.

Specifically, VxLAN GBP definitions are not present in all kernels used
by MLNX_OFED. Temporarily resolve this breakage by additionally checking
for the VxLAN GBP definitions in pkt_cls.h.

Once the missing kernel support has been properly ported, this patch can
be reverted.

Issue: 3406616
Fixes: 2a489a602e06 ("tc: Add vxlan gbp option flower match offload")
Change-Id: I500f6cf2ab227f4f5cb24f9d9ea3ed335a415531
Signed-off-by: Gavi Teitz <gavi@nvidia.com>

acinclude.m4
@@ -319,6 +319,13 @@ AC_DEFUN([OVS_CHECK_LINUX_TC], [
     ])],
     [AC_DEFINE([HAVE_TCA_STATS_PKT64], [1],
                [Define to 1 if TCA_STATS_PKT64 is available.])])
+
+  AC_COMPILE_IFELSE([
+    AC_LANG_PROGRAM([#include <linux/pkt_cls.h>], [
+        int x = TCA_FLOWER_KEY_ENC_OPTS_VXLAN;
+    ])],
+    [AC_DEFINE([HAVE_TCA_FLOWER_KEY_ENC_OPTS_VXLAN], [1],
+               [Define to 1 if TCA_FLOWER_KEY_ENC_OPTS_VXLAN is available.])])
 ])
 
 dnl OVS_CHECK_LINUX_SCTP_CT

include/linux/pkt_cls.h
@@ -1,7 +1,7 @@
 #ifndef __LINUX_PKT_CLS_WRAPPER_H
 #define __LINUX_PKT_CLS_WRAPPER_H 1
 
-#if defined(__KERNEL__) || defined(HAVE_TCA_POLICE_PKTRATE64)
+#if defined(__KERNEL__) || (defined(HAVE_TCA_POLICE_PKTRATE64) && defined(HAVE_TCA_FLOWER_KEY_ENC_OPTS_VXLAN))
 #include_next <linux/pkt_cls.h>
 #else
 