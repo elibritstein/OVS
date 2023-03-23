elibritstein/OVS

commit 0f2d638c41b182aade32adf8dec2a06185884428

netdev-offload-dpdk: Abstract flow info action
Flow information is done as 'mark' in rte provider, and 'meta' in doca
provider.
Instead of translating directly the low level MARK action, introduce an
abstract flow info action, translated properly in each provider.

The helper get_packet_reg_field() is refactored and becomes public, to
be used by providers according to their own packet metadata fields.

Issue: OVS-3400884
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -498,6 +498,12 @@ static struct reg_field reg_fields[] = {
         .offset = 0,
         .mask = 0x0000FFFF,
     },
+    [REG_FIELD_FLOW_INFO] = {
+        .type = REG_TYPE_MARK,
+        .index = 0,
+        .offset = 0,
+        .mask = 0x00FFFFFF,
+    },
 };
 
 static struct reg_field *
@@ -2427,6 +2433,16 @@ dpdk_offload_rte_create(struct netdev *netdev,
                         struct rte_flow_action *actions,
                         struct rte_flow_error *error)
 {
+    struct rte_flow_action *a;
+
+    for (a = actions; a->type != RTE_FLOW_ACTION_TYPE_END; a++) {
+        int act_type = a->type;
+
+        if (act_type == OVS_RTE_FLOW_ACTION_TYPE_FLOW_INFO) {
+            a->type = RTE_FLOW_ACTION_TYPE_MARK;
+        }
+    }
+
     return netdev_dpdk_rte_flow_create(netdev, attr, items, actions, error);
 }
 
@@ -3099,39 +3115,43 @@ parse_flow_tnl_match(struct netdev *tnldev,
     return ret;
 }
 
-static int
-get_packet_reg_field(struct dp_packet *packet, uint8_t reg_field_id,
+int
+get_packet_reg_field(struct dp_packet *packet,
+                     struct reg_field *reg_field,
                      uint32_t *val)
 {
-    struct reg_field *reg_field;
     uint32_t mark = 0;
     uint32_t meta;
 
-    if (reg_field_id >= REG_FIELD_NUM) {
-        VLOG_ERR("unkonwn reg id %d", reg_field_id);
-        return -1;
+    if (reg_field->type == REG_TYPE_META) {
+        if (!dp_packet_get_meta(packet, &meta)) {
+            return -1;
+        }
+
+        meta >>= reg_field->offset;
+        meta &= reg_field->mask;
+
+        if (meta == 0) {
+            dp_packet_has_flow_mark(packet, &mark);
+            VLOG_ERR_RL(&rl, "port %d, recirc=%d, mark=%d, has meta 0",
+                        packet->md.in_port.odp_port, packet->md.recirc_id,
+                        mark);
+            return -1;
+        }
+
+        *val = meta;
+        return 0;
     }
-    reg_field = &offload->reg_fields()[reg_field_id];
-    if (reg_field->type != REG_TYPE_META) {
-        VLOG_ERR("reg id %d is not meta", reg_field_id);
-        return -1;
-    }
-    if (!dp_packet_get_meta(packet, &meta)) {
+
+    if (reg_field->type == REG_TYPE_MARK) {
+        if (dp_packet_has_flow_mark(packet, &mark)) {
+            *val = mark;
+            return 0;
+        }
         return -1;
     }
 
-    meta >>= reg_field->offset;
-    meta &= reg_field->mask;
-
-    if (meta == 0) {
-        dp_packet_has_flow_mark(packet, &mark);
-        VLOG_ERR_RL(&rl, "port %d, recirc=%d: packet reg field id %d is 0, mark=%d",
-                    packet->md.in_port.odp_port, packet->md.recirc_id, reg_field_id, mark);
-        return -1;
-    }
-
-    *val = meta;
-    return 0;
+    OVS_NOT_REACHED();
 }
 
 static int
@@ -3189,6 +3209,7 @@ add_pattern_match_reg_field(struct flow_patterns *patterns,
         add_flow_pattern(patterns, RTE_FLOW_ITEM_TYPE_META, meta_spec,
                          meta_mask, NULL);
         break;
+    case REG_TYPE_MARK:
     default:
         VLOG_ERR("unkonwn reg type (%d) for reg field %d", reg_field->type,
                  reg_field_id);
@@ -3234,6 +3255,7 @@ add_action_set_reg_field(struct flow_actions *actions,
         set_meta->mask = reg_mask;
         add_flow_action(actions, RTE_FLOW_ACTION_TYPE_SET_META, set_meta);
         break;
+    case REG_TYPE_MARK:
     default:
         VLOG_ERR("unkonwn reg type (%d) for reg field %d", reg_field->type,
                  reg_field_id);
@@ -4328,7 +4350,7 @@ add_miss_flow(struct netdev *netdev,
     struct rte_flow_action_mark miss_mark;
     struct flow_actions miss_actions = {
         .actions = (struct rte_flow_action []) {
-            { .type = RTE_FLOW_ACTION_TYPE_MARK, .conf = &miss_mark },
+            { .type = OVS_RTE_FLOW_ACTION_TYPE_FLOW_INFO, .conf = &miss_mark },
             { .type = RTE_FLOW_ACTION_TYPE_JUMP, .conf = &miss_jump },
             { .type = RTE_FLOW_ACTION_TYPE_END, },
         },
@@ -4648,7 +4670,8 @@ parse_ct_actions(struct netdev *netdev,
                     per_thread_xzalloc(sizeof *mark);
 
                 mark->id = act_resources->flow_id;
-                add_flow_action(actions, RTE_FLOW_ACTION_TYPE_MARK, mark);
+                add_flow_action(actions, OVS_RTE_FLOW_ACTION_TYPE_FLOW_INFO,
+                                mark);
             }
             add_jump_action(actions, POSTCT_TABLE_ID);
         } else {
@@ -4912,7 +4935,8 @@ create_pre_post_ct(struct netdev *netdev,
         act_resources->associated_flow_id = true;
     }
     pre_ct_mark.id = act_resources->flow_id;
-    add_flow_action(&pre_ct_actions, RTE_FLOW_ACTION_TYPE_MARK, &pre_ct_mark);
+    add_flow_action(&pre_ct_actions, OVS_RTE_FLOW_ACTION_TYPE_FLOW_INFO,
+                    &pre_ct_mark);
     pre_ct_jump.group = ct_table_id;
     add_flow_action(&pre_ct_actions, RTE_FLOW_ACTION_TYPE_JUMP, &pre_ct_jump);
     add_flow_action(&pre_ct_actions, RTE_FLOW_ACTION_TYPE_END, NULL);
@@ -6023,10 +6047,13 @@ rte_get_packet_recovery_info(struct dp_packet *packet,
                              struct dpdk_offload_recovery_info *info)
 {
     memset(info, 0, sizeof *info);
-    if (dp_packet_has_flow_mark(packet, &info->flow_miss_id)) {
-        get_packet_reg_field(packet, REG_FIELD_CT_CTX, &info->ct_miss_id);
+    if (get_packet_reg_field(packet, &reg_fields[REG_FIELD_FLOW_INFO],
+                             &info->flow_miss_id)) {
+        get_packet_reg_field(packet, &reg_fields[REG_FIELD_CT_CTX],
+                             &info->ct_miss_id);
     } else {
-        get_packet_reg_field(packet, REG_FIELD_SFLOW_CTX, &info->sflow_id);
+        get_packet_reg_field(packet, &reg_fields[REG_FIELD_SFLOW_CTX],
+                             &info->sflow_id);
     }
 }
 

lib/dpdk-offload-doca.c
@@ -200,6 +200,12 @@ static struct reg_field reg_fields[] = {
         .offset = 0,
         .mask = 0x0000FFFF,
     },
+    [REG_FIELD_FLOW_INFO] = {
+        .type = REG_TYPE_META,
+        .index = 0,
+        .offset = 0,
+        .mask = 0x00FFFFFF,
+    },
 };
 
 static struct reg_field *
@@ -563,7 +569,7 @@ doca_translate_actions(struct netdev *netdev OVS_UNUSED,
             if (doca_translate_vxlan_encap(actions, dacts)) {
                 return -1;
             }
-        } else if (act_type == RTE_FLOW_ACTION_TYPE_MARK) {
+        } else if (act_type == OVS_RTE_FLOW_ACTION_TYPE_FLOW_INFO) {
             const struct rte_flow_action_mark *mark = actions->conf;
 
             dacts->meta.pkt_meta = mark->id;
@@ -776,14 +782,9 @@ static void
 dpdk_offload_doca_get_pkt_recover_info(struct dp_packet *p,
                                        struct dpdk_offload_recovery_info *info)
 {
-    uint32_t flow_miss_id;
-
-    memset(info, 0x0, sizeof *info);
-    if (!dp_packet_get_meta(p, &flow_miss_id)) {
-        return;
-    }
-
-    info->flow_miss_id = flow_miss_id;
+    memset(info, 0, sizeof *info);
+    get_packet_reg_field(p, &reg_fields[REG_FIELD_FLOW_INFO],
+                         &info->flow_miss_id);
 }
 
 static int

lib/dpdk-offload-provider.h
@@ -34,6 +34,11 @@
 #define MAX_TABLE_ID     0xf0000000
 #define NUM_TABLE_ID     (MAX_TABLE_ID - MIN_TABLE_ID + 1)
 
+/* Proprietary rte-flow action enums. */
+enum {
+    OVS_RTE_FLOW_ACTION_TYPE_FLOW_INFO = INT_MIN,
+};
+
 struct dpdk_offload_recovery_info {
     uint32_t flow_miss_id;
     uint32_t ct_miss_id;
@@ -48,12 +53,14 @@ enum dpdk_reg_id {
     REG_FIELD_TUN_INFO,
     REG_FIELD_CT_CTX,
     REG_FIELD_SFLOW_CTX,
+    REG_FIELD_FLOW_INFO,
     REG_FIELD_NUM,
 };
 
 enum reg_type {
     REG_TYPE_TAG,
     REG_TYPE_META,
+    REG_TYPE_MARK,
 };
 
 struct reg_field {
@@ -109,5 +116,10 @@ struct dpdk_offload_api {
 
 extern struct dpdk_offload_api dpdk_offload_api_rte;
 extern struct dpdk_offload_api dpdk_offload_api_doca;
+
+int
+get_packet_reg_field(struct dp_packet *packet,
+                     struct reg_field *reg_field,
+                     uint32_t *val);
 
 #endif /* DPDK_OFFLOAD_PROVIDER_H */

commit f7c89908f3fc14db43226e01d7f42ccc890c0041

netdev-offload-dpdk: Add log dumps for proprietary enums
For RTE, the enums are replaced by dpdk ones. For doca not. Add dumps
for them.

Fixes: 0f2d638c41b1 ("netdev-offload-dpdk: Abstract flow info action")
Fixes: e85810cd5f55 ("netdev-offload-dpdk: Abstract ct info action")
Fixes: b3ec1283412e ("netdev-offload-dpdk: Abstract flow info item")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -1910,11 +1910,23 @@ dump_flow_pattern(struct ds *s,
                               meta_spec->data, meta_mask->data, 0);
         }
         ds_put_cstr(s, "/ ");
-    } else if (item->type == OVS_RTE_FLOW_ITEM_TYPE(FLOW_INFO)) {
+    } else if (item->type == RTE_FLOW_ITEM_TYPE_MARK) {
         const struct rte_flow_item_mark *mark_spec = item->spec;
         const struct rte_flow_item_mark *mark_mask = item->mask;
 
         ds_put_cstr(s, "mark ");
+        if (mark_spec) {
+            ds_put_format(s, "id spec %d ", mark_spec->id);
+        }
+        if (mark_mask) {
+            ds_put_format(s, "id mask %d ", mark_mask->id);
+        }
+        ds_put_cstr(s, "/ ");
+    } else if (item->type == OVS_RTE_FLOW_ITEM_TYPE(FLOW_INFO)) {
+        const struct rte_flow_item_mark *mark_spec = item->spec;
+        const struct rte_flow_item_mark *mark_mask = item->mask;
+
+        ds_put_cstr(s, "flow-info ");
         if (mark_spec) {
             ds_put_format(s, "id spec %d ", mark_spec->id);
         }
@@ -2118,6 +2130,14 @@ dump_flow_action(struct ds *s, struct ds *s_extra,
             ds_put_format(s, "id %d ", mark->id);
         }
         ds_put_cstr(s, "/ ");
+    } else if (actions->type == OVS_RTE_FLOW_ACTION_TYPE(FLOW_INFO)) {
+        const struct rte_flow_action_mark *mark = actions->conf;
+
+        ds_put_cstr(s, "flow-info ");
+        if (mark) {
+            ds_put_format(s, "id %d ", mark->id);
+        }
+        ds_put_cstr(s, "/ ");
     } else if (actions->type == RTE_FLOW_ACTION_TYPE_COUNT) {
         ds_put_cstr(s, "count / ");
     } else if (actions->type == RTE_FLOW_ACTION_TYPE_PORT_ID) {
@@ -2261,6 +2281,15 @@ dump_flow_action(struct ds *s, struct ds *s_extra,
         const struct rte_flow_action_set_meta *meta = actions->conf;
 
         ds_put_cstr(s, "set_meta ");
+        if (meta) {
+            ds_put_format(s, "data 0x%08x mask 0x%08x ", meta->data,
+                          meta->mask);
+        }
+        ds_put_cstr(s, "/ ");
+    } else if (actions->type == OVS_RTE_FLOW_ACTION_TYPE(CT_INFO)) {
+        const struct rte_flow_action_set_meta *meta = actions->conf;
+
+        ds_put_cstr(s, "ct-info ");
         if (meta) {
             ds_put_format(s, "data 0x%08x mask 0x%08x ", meta->data,
                           meta->mask);