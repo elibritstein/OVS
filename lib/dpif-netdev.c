elibritstein/OVS

commit 7b00b0996001dc930abc810fa7b7764b266e8101

conntrack-offload: Set conn pointers as offload ufid
Change-Id: Id052e8e430ac32149119a850eabacab13013a147
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/dpif-netdev.c
@@ -501,8 +501,7 @@ static struct dp_offload_thread *dp_offload_threads = NULL;
 static void *dp_netdev_flow_offload_main(void *arg);
 
 static void
-dp_netdev_ct_offload_get_ufid(struct ct_flow_offload_item *offload,
-                              ovs_u128 *ufid);
+dp_netdev_ct_offload_get_ufid(ovs_u128 *ufid);
 static void
 dp_netdev_ct_offload_add_item(struct ct_flow_offload_item *ct_offload);
 static void
@@ -4245,14 +4244,11 @@ dp_netdev_offload_ct_enqueue(struct dp_offload_thread_item *item)
 }
 
 static void
-dp_netdev_ct_offload_get_ufid(struct ct_flow_offload_item *offload,
-                              ovs_u128 *ufid)
+dp_netdev_ct_offload_get_ufid(ovs_u128 *ufid)
 {
-    struct match match;
-
-    dp_netdev_fill_ct_match(&match, &offload->ct_match);
-    match.flow.in_port.odp_port = ODPP_NONE;
-    dp_netdev_get_mega_ufid(&match, ufid);
+    ufid->u64.lo = (uint64_t) ufid;
+    ufid->u64.hi = (uint64_t) ufid;
+    uuid_set_bits_v4((struct uuid *) ufid, UUID_ATTR_1);
 }
 
 static int

lib/conntrack-offload.c
@@ -219,7 +219,7 @@ e2e_cache_trace_add_ct(struct conntrack *ct,
     }
     if (!dir_info->e2e_flow) {
         dir_info->e2e_flow = true;
-        offload_class->conn_get_ufid(&item, &dir_info->ufid);
+        offload_class->conn_get_ufid(&dir_info->ufid);
         item.ufid = dir_info->ufid;
         offload_class->conn_e2e_add(&item);
     }
@@ -291,7 +291,6 @@ conntrack_offload_add_conn(struct conntrack *ct,
     }
     if ((flags & CT_OFFLOAD_BOTH) == CT_OFFLOAD_BOTH) {
         struct ovs_refcount *refcnt;
-        ovs_u128 *ufid[CT_DIR_NUM];
 
         for (dir = 0; dir < CT_DIR_NUM; dir ++) {
             if (conn->nat_conn &&
@@ -302,14 +301,9 @@ conntrack_offload_add_conn(struct conntrack *ct,
                 conn = conn->master_conn;
             }
             conntrack_offload_fill_item_add(&item[dir], conn, dir, now_us);
-            ufid[dir] = &conn->offloads.dir_info[dir].ufid;
+            offload_class->conn_get_ufid(&conn->offloads.dir_info[dir].ufid);
+            item[dir].ufid = conn->offloads.dir_info[dir].ufid;
         }
-        offload_class->conn_get_ufid(&item[CT_DIR_INIT],
-                                     &item[CT_DIR_INIT].ufid);
-        offload_class->conn_get_ufid(&item[CT_DIR_REP],
-                                     &item[CT_DIR_REP].ufid);
-        *ufid[CT_DIR_INIT] = item[CT_DIR_INIT].ufid;
-        *ufid[CT_DIR_REP] = item[CT_DIR_REP].ufid;
         refcnt = xmalloc(sizeof *refcnt);
         ovs_refcount_init(refcnt);
         ovs_refcount_ref(refcnt);

lib/conntrack-offload.h
@@ -61,7 +61,7 @@ struct ct_flow_offload_item {
 
 /* hw-offload callbacks */
 struct conntrack_offload_class {
-    void (*conn_get_ufid)(struct ct_flow_offload_item *, ovs_u128 *);
+    void (*conn_get_ufid)(ovs_u128 *);
     void (*conn_add)(struct ct_flow_offload_item *);
     void (*conn_del)(struct ct_flow_offload_item *);
     int (*conn_active)(struct ct_flow_offload_item *, long long now,

commit 66c3c36bd4f9493dba860604d342a7a688bd0f4d

dpif-netdev: Fix CT get_ufid function to enable good distribution
The cited commit just took the pointer value, without any hashing. When
using it to distribute among hw-offload threads, it gets poor
distribution.
Calculate some hash on it to enable good distribution.

Fixes: 7b00b0996001 ("conntrack-offload: Set conn pointers as offload ufid")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/dpif-netdev.c
@@ -4235,8 +4235,7 @@ dp_netdev_offload_ct_enqueue(struct dp_offload_thread_item *item)
 static void
 dp_netdev_ct_offload_get_ufid(ovs_u128 *ufid)
 {
-    ufid->u64.lo = (uint64_t) ufid;
-    ufid->u64.hi = (uint64_t) ufid;
+    ufid->u64.hi = ufid->u64.lo = hash_pointer(ufid, 0);
     uuid_set_bits_v4((struct uuid *) ufid, UUID_ATTR_1);
 }
 