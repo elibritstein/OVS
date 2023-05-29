elibritstein/OVS

commit f2a157802efa0e6d5df46697669d498621f01df8

netdev-offload-dpdk: Don't offload CT state and unsupported protocols flows
CT offload is supported for TCP/UDP protocols only.
There is no point to offload flows that match on other protocols
and CT state since they will always miss.

Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -3695,6 +3695,14 @@ parse_flow_match(struct netdev *netdev,
         !((match->wc.masks.ct_state & CS_NEW) &&
           (match->flow.ct_state & CS_NEW)) &&
         !(match->wc.masks.ct_state & OVS_CS_F_NAT_MASK)) {
+
+        if (act_vars->proto && act_vars->proto != IPPROTO_UDP &&
+            act_vars->proto != IPPROTO_TCP) {
+            VLOG_DBG_RL(&rl, "Unsupported CT offload for L4 protocol: 0x02%"
+                        PRIx8, act_vars->proto);
+            return -1;
+        }
+
         if ((!match->flow.recirc_id &&
              !(match->wc.masks.ct_state & match->flow.ct_state)) ||
             !add_pattern_match_reg_field(patterns, REG_FIELD_CT_STATE,

commit a069c49a23eabc65533a10bc70939b2d198d3d06

netdev-offload-dpdk: Fix debug print for unsupported CT protocols
Fixes: f2a157802efa ("netdev-offload-dpdk: Don't offload CT state and unsupported protocols flows")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -3667,7 +3667,7 @@ parse_flow_match(struct netdev *netdev,
 
         if (act_vars->proto && act_vars->proto != IPPROTO_UDP &&
             act_vars->proto != IPPROTO_TCP) {
-            VLOG_DBG_RL(&rl, "Unsupported CT offload for L4 protocol: 0x02%"
+            VLOG_DBG_RL(&rl, "Unsupported CT offload for L4 protocol: 0x%02"
                         PRIx8, act_vars->proto);
             return -1;
         }