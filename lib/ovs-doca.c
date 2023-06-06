elibritstein/OVS

commit c609b72d22f3b74b59863a804750362646cdc4b1

vswitchd: Add 'doca-init' flag
Add a new flag 'doca-init' to vswitch schema, this flag will enable doca-flow
offloads.

This flag can be set in the 'other_config' column, e.g.

    ovs-vsctl set o . other_config:doca-init=true

Default value for this flag is "false". And setting this flag to "true" also
implicitly Initializes DPDK if not already Initialized.

Issue: OVS-3356870
Signed-off-by: Salem Sol <salems@nvidia.com>
Signed-off-by: Ariel Levkovich <lariel@nvidia.com>

lib/ovs-doca.c
@@ -1,0 +1,148 @@
+/*
+ * Copyright (c) 2022 NVIDIA CORPORATION & AFFILIATES, ALL RIGHTS RESERVED.
+ *
+ * This software product is a proprietary product of NVIDIA CORPORATION &
+ * AFFILIATES (the "Company") and all right, title, and interest in and to the
+ * software product, including all associated intellectual property rights, are
+ * and shall remain exclusively with the Company.
+ *
+ * This software product is governed by the End User License Agreement
+ * provided with the software product.
+ *
+ */
+
+#include <config.h>
+
+#include <doca_version.h>
+#include <doca_flow.h>
+
+#include "dpdk.h"
+#include "netdev-offload.h"
+#include "netdev-offload-provider.h"
+#include "openvswitch/vlog.h"
+#include "ovs-doca.h"
+
+VLOG_DEFINE_THIS_MODULE(ovs_doca);
+
+/* Indicates successful initialization of DOCA. */
+static atomic_bool doca_initialized = ATOMIC_VAR_INIT(false);
+
+/* Maximum number of megaflows */
+#define OVS_DOCA_MAX_COUNTERS (1 << 16)
+
+#define MAX_PORT_STR_LEN 128
+
+bool
+ovs_doca_enabled(void)
+{
+    bool initialized;
+
+    atomic_read_relaxed(&doca_initialized, &initialized);
+    return initialized;
+}
+
+int
+ovs_doca_init(const struct smap *ovs_other_config)
+{
+    static struct ovsthread_once once_enable = OVSTHREAD_ONCE_INITIALIZER;
+    unsigned int nb_threads = DEFAULT_OFFLOAD_THREAD_NB;
+    struct doca_flow_cfg cfg = {};
+    static bool enabled = false;
+    doca_error_t err;
+
+    if (!ovsthread_once_start(&once_enable)) {
+        return 0;
+    }
+
+    if (!enabled && ovs_other_config &&
+        smap_get_bool(ovs_other_config, "doca-init", false)) {
+        /* Set dpdk-init to be true if not already set */
+        smap_replace(CONST_CAST(struct smap *, ovs_other_config),
+                     "dpdk-init", "true");
+        dpdk_init(ovs_other_config);
+
+        /* Due to limitation in doca, only one offload thread is currently
+         * supported.
+         * */
+        if (smap_get_uint(ovs_other_config,"n-offload-threads", 1) !=
+            nb_threads) {
+            smap_replace(CONST_CAST(struct smap *, ovs_other_config),
+                         "n-offload-threads", "1");
+            VLOG_WARN_ONCE("Only %u offload thread is currently supported with"
+                           "doca, ignoring n-offload-threads configuration",
+                           nb_threads);
+        }
+        cfg.queues = smap_get_uint(ovs_other_config, "n-offload-threads", 1);
+        cfg.resource.nb_counters = OVS_DOCA_MAX_COUNTERS;
+        cfg.mode_args = "switch,hws";
+        cfg.queue_depth = 32;
+
+        VLOG_INFO("DOCA Enabled - initializing...");
+        err = doca_flow_init(&cfg);
+        if (err) {
+            VLOG_ERR("Error initializing doca flow offload. Error %d (%s)\n",
+                    err, doca_get_error_string(err));
+
+            ovs_abort(err, "Cannot init DOCA");
+            return err;
+        }
+
+        enabled = true;
+        VLOG_INFO("DOCA Enabled - initialized");
+    }
+
+    ovsthread_once_done(&once_enable);
+    atomic_store_relaxed(&doca_initialized, enabled);
+
+    return 0;
+}
+
+void *
+ovs_doca_port_create(uint16_t port_id)
+{
+    struct doca_flow_port_cfg port_cfg;
+    char port_id_str[MAX_PORT_STR_LEN];
+    struct doca_flow_port *port;
+    doca_error_t err;
+
+    memset(&port_cfg, 0, sizeof(port_cfg));
+
+    port_cfg.port_id = port_id;
+    port_cfg.type = DOCA_FLOW_PORT_DPDK_BY_ID;
+    snprintf(port_id_str, MAX_PORT_STR_LEN, "%d", port_cfg.port_id);
+    port_cfg.devargs = port_id_str;
+
+    err = doca_flow_port_start(&port_cfg, &port);
+    if (err) {
+        VLOG_ERR("Failed to start doca flow port_id %"PRIu16". Error: %d (%s)",
+                 port_id, err, doca_get_error_string(err));
+
+        return NULL;
+    }
+
+    return (void *)port;
+}
+
+int
+ovs_doca_port_destroy(void *port)
+{
+    struct doca_flow_port *doca_port = port;
+
+    return doca_flow_port_stop(doca_port);
+}
+
+void
+ovs_doca_status(const struct ovsrec_open_vswitch *cfg)
+{
+    if (!cfg) {
+        return;
+    }
+    ovsrec_open_vswitch_set_doca_initialized(cfg, ovs_doca_enabled());
+    ovsrec_open_vswitch_set_doca_version(cfg, doca_version_runtime());
+}
+
+void
+ovs_doca_destroy(void)
+{
+    doca_flow_destroy();
+}

lib/automake.mk
@@ -277,6 +277,7 @@ lib_libopenvswitch_la_SOURCES = \
 	lib/ovs-atomic-pthreads.h \
 	lib/ovs-atomic-x86_64.h \
 	lib/ovs-atomic.h \
+	lib/ovs-doca.h \
 	lib/ovs-lldp.c \
 	lib/ovs-lldp.h \
 	lib/ovs-numa.c \
@@ -519,6 +520,14 @@ lib_libopenvswitch_la_SOURCES += \
 else
 lib_libopenvswitch_la_SOURCES += \
 	lib/dpdk-stub.c
+endif
+
+if DOCA_OFFLOAD
+lib_libopenvswitch_la_SOURCES += \
+	lib/ovs-doca.c
+else
+lib_libopenvswitch_la_SOURCES += \
+	lib/ovs-doca-stub.c
 endif
 
 if WIN32

lib/ovs-doca-stub.c
@@ -1,0 +1,60 @@
+/*
+ * Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES, ALL RIGHTS RESERVED.
+ *
+ * This software product is a proprietary product of NVIDIA CORPORATION &
+ * AFFILIATES (the "Company") and all right, title, and interest in and to the
+ * software product, including all associated intellectual property rights, are
+ * and shall remain exclusively with the Company.
+ *
+ * This software product is governed by the End User License Agreement
+ * provided with the software product.
+ *
+ */
+
+#include <config.h>
+
+#include "ovs-doca.h"
+#include "ovs-thread.h"
+#include "openvswitch/vlog.h"
+#include "smap.h"
+#include "vswitch-idl.h"
+
+VLOG_DEFINE_THIS_MODULE(doca);
+
+int
+ovs_doca_init(const struct smap *ovs_other_config OVS_UNUSED)
+{
+    return 0;
+}
+
+void *
+ovs_doca_port_create(uint16_t port_id OVS_UNUSED)
+{
+    return NULL;
+}
+
+int
+ovs_doca_port_destroy(void *port OVS_UNUSED)
+{
+    return 0;
+}
+
+void
+ovs_doca_status(const struct ovsrec_open_vswitch *cfg)
+{
+    if (cfg) {
+        ovsrec_open_vswitch_set_doca_initialized(cfg, false);
+        ovsrec_open_vswitch_set_doca_version(cfg, "none");
+    }
+}
+
+bool
+ovs_doca_enabled(void)
+{
+    return false;
+}
+
+void
+ovs_doca_destroy(void)
+{
+}

lib/ovs-doca.h
@@ -1,0 +1,39 @@
+/*
+ * Copyright (c) 2022 NVIDIA CORPORATION & AFFILIATES, ALL RIGHTS RESERVED.
+ *
+ * This software product is a proprietary product of NVIDIA CORPORATION &
+ * AFFILIATES (the "Company") and all right, title, and interest in and to the
+ * software product, including all associated intellectual property rights, are
+ * and shall remain exclusively with the Company.
+ *
+ * This software product is governed by the End User License Agreement
+ * provided with the software product.
+ *
+ */
+
+#ifndef OVS_DOCA_H_
+#define OVS_DOCA_H_
+
+#include <config.h>
+
+#include "smap.h"
+#include "vswitch-idl.h"
+
+bool
+ovs_doca_enabled(void);
+
+int
+ovs_doca_init(const struct smap *ovs_other_config);
+
+void *
+ovs_doca_port_create(uint16_t port_id);
+
+int
+ovs_doca_port_destroy(void *port);
+
+void
+ovs_doca_status(const struct ovsrec_open_vswitch *cfg);
+
+void
+ovs_doca_destroy(void);
+#endif

vswitchd/bridge.c
@@ -49,6 +49,7 @@
 #include "openvswitch/ofpbuf.h"
 #include "openvswitch/vconn.h"
 #include "openvswitch/vlog.h"
+#include "ovs-doca.h"
 #include "ovs-lldp.h"
 #include "ovs-numa.h"
 #include "packets.h"
@@ -449,6 +450,8 @@ bridge_init(const char *remote)
     ovsdb_idl_omit(idl, &ovsrec_open_vswitch_col_system_version);
     ovsdb_idl_omit_alert(idl, &ovsrec_open_vswitch_col_dpdk_version);
     ovsdb_idl_omit_alert(idl, &ovsrec_open_vswitch_col_dpdk_initialized);
+    ovsdb_idl_omit_alert(idl, &ovsrec_open_vswitch_col_doca_version);
+    ovsdb_idl_omit_alert(idl, &ovsrec_open_vswitch_col_doca_initialized);
 
     ovsdb_idl_omit_alert(idl, &ovsrec_bridge_col_datapath_id);
     ovsdb_idl_omit_alert(idl, &ovsrec_bridge_col_datapath_version);
@@ -554,6 +557,7 @@ bridge_exit(bool delete_datapath)
     }
 
     ovsdb_idl_destroy(idl);
+    ovs_doca_destroy();
 }
 
 /* Looks at the list of managers in 'ovs_cfg' and extracts their remote IP
@@ -3155,6 +3159,7 @@ run_status_update(void)
             connectivity_seqno = seq;
             status_txn = ovsdb_idl_txn_create(idl);
             dpdk_status(cfg);
+            ovs_doca_status(cfg);
             HMAP_FOR_EACH (br, node, &all_bridges) {
                 struct port *port;
 
@@ -3292,6 +3297,7 @@ bridge_run(void)
     if (cfg) {
         netdev_set_flow_api_enabled(&cfg->other_config);
         dpdk_init(&cfg->other_config);
+        ovs_doca_init(&cfg->other_config);
         userspace_tso_init(&cfg->other_config);
     }
 

vswitchd/vswitch.ovsschema
@@ -1,6 +1,6 @@
 {"name": "Open_vSwitch",
- "version": "8.3.0",
- "cksum": "3781850481 26690",
+ "version": "8.3.1",
+ "cksum": "3548195427 26858",
  "tables": {
    "Open_vSwitch": {
      "columns": {
@@ -56,6 +56,11 @@
        "dpdk_initialized": {
          "type": "boolean"},
        "dpdk_version": {
+         "type": {"key": {"type": "string"},
+                  "min": 0, "max": 1}},
+       "doca_initialized": {
+         "type": "boolean"},
+       "doca_version": {
          "type": {"key": {"type": "string"},
                   "min": 0, "max": 1}}},
      "isRoot": true,

vswitchd/vswitch.xml
@@ -263,6 +263,20 @@
         </p>
       </column>
 
+      <column name="other_config" key="doca-init"
+              type='{"type": "boolean"}'>
+        <p>
+          Set this value to true to enable doca-flow hw offload.
+        </p>
+        <p>
+          The default value is <code>false</code>. Changing this value requires
+          restarting the daemon.
+        </p>
+        <p>
+          This is only relevant for userspace datapath.
+        </p>
+      </column>
+
       <column name="other_config" key="tc-policy"
               type='{"type": "string",
                      "enum": ["set", ["none", "skip_sw", "skip_hw"]]}'>
@@ -798,6 +812,11 @@
         true and the DPDK library is successfully initialized.
       </column>
 
+      <column name="doca_initialized">
+        True if <ref column="other_config" key="doca-init"/> is set to
+        true and the DOCA library is successfully initialized.
+      </column>
+
       <group title="Statistics">
         <p>
           The <code>statistics</code> column contains key-value pairs that
@@ -984,6 +1003,12 @@
       <column name="dpdk_version">
         <p>
           The version of the linked DPDK library.
+        </p>
+      </column>
+
+      <column name="doca_version">
+        <p>
+          The version of the linked DOCA Flow library.
         </p>
       </column>
 

commit fcd48ed9e52c81c511344cca6a43918fbfdc0d1e

ovs-doca: Fix clang mutex 'once_enable.mutex'
lib/ovs-doca.c:303:1: error: mutex 'once_enable.mutex' is not held on
 every path through here [-Werror,-Wthread-safety-analysis]

Fixes: c609b72d22f3 ("vswitchd: Add 'doca-init' flag")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/ovs-doca.c
@@ -249,6 +249,7 @@ ovs_doca_init(const struct smap *ovs_other_config)
         /* Create a logger backend that prints to the redirected log */
         err = doca_log_create_file_backend(log_stream, &doca_logger);
         if (err != DOCA_SUCCESS) {
+            ovsthread_once_done(&once_enable);
             return EXIT_FAILURE;
         }
     }
@@ -288,6 +289,7 @@ ovs_doca_init(const struct smap *ovs_other_config)
             VLOG_ERR("Error initializing doca flow offload. Error %d (%s)\n",
                     err, doca_get_error_string(err));
 
+            ovsthread_once_done(&once_enable);
             ovs_abort(err, "Cannot init DOCA");
             return err;
         }