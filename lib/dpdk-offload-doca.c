elibritstein/OVS

commit 06c5162fb2428a03a0bf3fb03f561b807b5157c9

dpdk-offload-doca: Implement DOCA provider
Implement dpdk-offload-privder API for doca flow.

With this commit basic offload support for the following is added:
    - Actions:
        - Drop.
        - Jump.
        - Count.
        - Forward to port.

    - Matches:
        - TCP.
        - UDP.
        - ICMP.
        - ETH.

Signed-off-by: Salem Sol <salems@nvidia.com>

lib/dpdk-offload-doca.c
@@ -25,9 +25,19 @@
 #include "dpdk-offload-provider.h"
 #include "openvswitch/vlog.h"
 #include "offload-metadata.h"
+#include "netdev-dpdk.h"
 #include "util.h"
 
 VLOG_DEFINE_THIS_MODULE(dpdk_offload_doca);
+static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(600, 600);
+
+struct doca_flow_handle {
+     struct doca_flow_pipe_entry *flow;
+     uint32_t group;
+     struct doca_ctl_pipe_ctx *self_pipe;
+     uint32_t next_group;
+     struct doca_ctl_pipe_ctx *next_pipe;
+};
 
 OVS_ASSERT_PACKED(struct doca_ctl_pipe_key,
     uint32_t group_id;
@@ -106,7 +116,6 @@ doca_ctl_pipe_md_init(void)
     }
 }
 
-OVS_UNUSED
 static struct doca_ctl_pipe_ctx *
 doca_ctl_pipe_ctx_ref(struct netdev *netdev,
                       uint32_t group_id)
@@ -137,7 +146,6 @@ doca_ctl_pipe_ctx_ref(struct netdev *netdev,
     return offload_metadata_priv_get(doca_ctl_pipe_md, &key, &arg, NULL, true);
 }
 
-OVS_UNUSED
 static void
 doca_ctl_pipe_ctx_unref(struct doca_ctl_pipe_ctx *ctx)
 {
@@ -147,39 +155,413 @@ doca_ctl_pipe_ctx_unref(struct doca_ctl_pipe_ctx *ctx)
                                 ctx);
 }
 
+static struct reg_field reg_fields[] = {
+    [REG_FIELD_CT_STATE] = {
+        .type = REG_TYPE_TAG,
+        .index = 0,
+        .offset = 0,
+        .mask = 0x000000FF,
+    },
+    [REG_FIELD_CT_ZONE] = {
+        .type = REG_TYPE_TAG,
+        .index = 0,
+        .offset = 8,
+        .mask = 0x000000FF,
+    },
+    [REG_FIELD_TUN_INFO] = {
+        .type = REG_TYPE_TAG,
+        .index = 0,
+        .offset = 16,
+        .mask = 0x0000FFFF,
+    },
+    [REG_FIELD_CT_MARK] = {
+        .type = REG_TYPE_TAG,
+        .index = 1,
+        .offset = 0,
+        .mask = 0xFFFFFFFF,
+    },
+    [REG_FIELD_CT_LABEL_ID] = {
+        .type = REG_TYPE_TAG,
+        .index = 2,
+        .offset = 0,
+        .mask = 0xFFFFFFFF,
+    },
+    [REG_FIELD_CT_CTX] = {
+        .type = REG_TYPE_META,
+        .index = 0,
+        .offset = 0,
+        .mask = 0x0000FFFF,
+    },
+    /* Since sFlow and CT will not work concurrently is it safe
+     * to have the reg_fields use the same bits for SFLOW_CTX and CT_CTX.
+     */
+    [REG_FIELD_SFLOW_CTX] = {
+        .type = REG_TYPE_META,
+        .index = 0,
+        .offset = 0,
+        .mask = 0x0000FFFF,
+    },
+};
 
 static struct reg_field *
 dpdk_offload_doca_get_reg_fields(void)
 {
-    return NULL;
+    return reg_fields;
+}
+
+static int
+doca_translate_items(struct netdev *netdev OVS_UNUSED,
+                     const struct rte_flow_item *items,
+                     struct doca_flow_match *doca_spec,
+                     struct doca_flow_match *doca_mask)
+{
+    struct doca_flow_header_format *doca_hdr_spec, *doca_hdr_mask;
+
+    doca_hdr_spec = &doca_spec->outer;
+    doca_hdr_mask = &doca_mask->outer;
+
+    for (; items->type != RTE_FLOW_ITEM_TYPE_END; items++) {
+        int item_type = items->type;
+
+        if (item_type == RTE_FLOW_ITEM_TYPE_PORT_ID) {
+            const struct rte_flow_item_port_id *spec = items->spec;
+
+            doca_spec->meta.port_meta = spec->id;
+            doca_mask->meta.port_meta = 0xFFFFFFFF;
+        } else if (item_type == RTE_FLOW_ITEM_TYPE_ETH) {
+            const struct rte_flow_item_eth *spec = items->spec;
+            const struct rte_flow_item_eth *mask = items->mask;
+
+            if (items->spec) {
+                memcpy(doca_hdr_spec->eth.dst_mac, &spec->dst,
+                       sizeof doca_hdr_spec->eth.dst_mac);
+                memcpy(doca_hdr_spec->eth.src_mac, &spec->src,
+                       sizeof doca_hdr_spec->eth.src_mac);
+                doca_hdr_spec->eth.type = spec->type;
+            }
+            if (items->mask) {
+                memcpy(doca_hdr_mask->eth.dst_mac, &mask->dst,
+                       sizeof doca_hdr_mask->eth.dst_mac);
+                memcpy(doca_hdr_mask->eth.src_mac, &mask->src,
+                       sizeof doca_hdr_mask->eth.src_mac);
+                doca_hdr_mask->eth.type = mask->type;
+            }
+        /* L3 */
+        } else if (item_type == RTE_FLOW_ITEM_TYPE_IPV4) {
+            const struct rte_flow_item_ipv4 *spec = items->spec;
+            const struct rte_flow_item_ipv4 *mask = items->mask;
+
+            doca_hdr_spec->l3_type = DOCA_FLOW_L3_TYPE_IP4;
+            if (items->spec) {
+                doca_hdr_spec->ip4.next_proto = spec->hdr.next_proto_id;
+                doca_hdr_spec->ip4.src_ip = spec->hdr.src_addr;
+                doca_hdr_spec->ip4.dst_ip = spec->hdr.dst_addr;
+                doca_hdr_spec->ip4.ttl = spec->hdr.time_to_live;
+                doca_hdr_spec->ip4.dscp_ecn = spec->hdr.type_of_service;
+            }
+
+            doca_hdr_mask->l3_type = DOCA_FLOW_L3_TYPE_IP4;
+            if (items->mask) {
+                doca_hdr_mask->ip4.next_proto = mask->hdr.next_proto_id;
+                doca_hdr_mask->ip4.src_ip = mask->hdr.src_addr;
+                doca_hdr_mask->ip4.dst_ip = mask->hdr.dst_addr;
+                doca_hdr_mask->ip4.ttl = mask->hdr.time_to_live;
+                doca_hdr_mask->ip4.dscp_ecn = mask->hdr.type_of_service;
+            }
+            /*TODO: handle ip frag checks */
+        } else if (item_type == RTE_FLOW_ITEM_TYPE_IPV6) {
+            const struct rte_flow_item_ipv6 *spec = items->spec;
+            const struct rte_flow_item_ipv6 *mask = items->mask;
+
+            doca_hdr_spec->l3_type = DOCA_FLOW_L3_TYPE_IP6;
+            if (items->spec) {
+                memcpy(doca_hdr_spec->ip6.dst_ip, spec->hdr.dst_addr,
+                       sizeof doca_hdr_spec->ip6.dst_ip);
+                memcpy(doca_hdr_spec->ip6.src_ip, spec->hdr.src_addr,
+                       sizeof doca_hdr_spec->ip6.src_ip);
+                doca_hdr_spec->ip6.next_proto = spec->hdr.proto;
+                doca_hdr_spec->ip6.dscp_ecn = spec->hdr.vtc_flow;
+                doca_hdr_spec->ip6.hop_limit = spec->hdr.hop_limits;
+            }
+
+            doca_hdr_mask->l3_type = DOCA_FLOW_L3_TYPE_IP6;
+            if (items->mask) {
+                memcpy(doca_hdr_mask->ip6.dst_ip, mask->hdr.dst_addr,
+                       sizeof doca_hdr_mask->ip6.dst_ip);
+                memcpy(doca_hdr_mask->ip6.src_ip, mask->hdr.src_addr,
+                       sizeof doca_hdr_mask->ip6.src_ip);
+                doca_hdr_mask->ip6.next_proto = mask->hdr.hop_limits;
+                doca_hdr_mask->ip6.dscp_ecn = mask->hdr.vtc_flow;
+                doca_hdr_mask->ip6.hop_limit = mask->hdr.hop_limits;
+            }
+        /* L4 */
+        } else if (item_type == RTE_FLOW_ITEM_TYPE_UDP) {
+            const struct rte_flow_item_udp *spec = items->spec;
+            const struct rte_flow_item_udp *mask = items->mask;
+
+            doca_hdr_spec->l4_type_ext = DOCA_FLOW_L4_TYPE_EXT_UDP;
+            if (items->spec) {
+                doca_hdr_spec->udp.l4_port.src_port = spec->hdr.src_port;
+                doca_hdr_spec->udp.l4_port.dst_port = spec->hdr.dst_port;
+            }
+
+            doca_hdr_mask->l4_type_ext = DOCA_FLOW_L4_TYPE_EXT_UDP;
+            if (items->mask) {
+                doca_hdr_mask->udp.l4_port.src_port = mask->hdr.src_port;
+                doca_hdr_mask->udp.l4_port.dst_port = mask->hdr.dst_port;
+            }
+        } else if (item_type ==  RTE_FLOW_ITEM_TYPE_TCP) {
+            const struct rte_flow_item_tcp *spec = items->spec;
+            const struct rte_flow_item_tcp *mask = items->mask;
+
+            doca_hdr_spec->l4_type_ext = DOCA_FLOW_L4_TYPE_EXT_TCP;
+            if (items->spec) {
+                doca_hdr_spec->tcp.l4_port.src_port = spec->hdr.src_port;
+                doca_hdr_spec->tcp.l4_port.dst_port = spec->hdr.dst_port;
+                doca_hdr_spec->tcp.flags = spec->hdr.tcp_flags;
+            }
+
+            doca_hdr_mask->l4_type_ext = DOCA_FLOW_L4_TYPE_EXT_TCP;
+            if (items->mask) {
+                doca_hdr_mask->tcp.l4_port.src_port = mask->hdr.src_port;
+                doca_hdr_mask->tcp.l4_port.dst_port = mask->hdr.dst_port;
+                doca_hdr_mask->tcp.flags = mask->hdr.tcp_flags;
+            }
+        } else if (item_type == RTE_FLOW_ITEM_TYPE_ICMP) {
+            const struct rte_flow_item_icmp *spec = items->spec;
+            const struct rte_flow_item_icmp *mask = items->mask;
+
+            doca_hdr_spec->l4_type_ext  = DOCA_FLOW_L4_TYPE_EXT_ICMP;
+            if (items->spec) {
+                doca_hdr_spec->icmp.type  = spec->hdr.icmp_type;
+                doca_hdr_spec->icmp.code  = spec->hdr.icmp_code;
+            }
+
+            doca_hdr_mask->l4_type_ext = DOCA_FLOW_L4_TYPE_EXT_ICMP;
+            if (items->mask) {
+                doca_hdr_mask->icmp.type  = mask->hdr.icmp_type;
+                doca_hdr_mask->icmp.code  = mask->hdr.icmp_code;
+            }
+        } else {
+            VLOG_DBG_RL(&rl, "item %d is not supported", item_type);
+            return -1;
+        }
+    }
+    return 0;
+}
+
+static int
+doca_translate_actions(struct netdev *netdev OVS_UNUSED,
+                       const struct rte_flow_action *actions,
+                       struct doca_flow_actions *dacts OVS_UNUSED,
+                       struct doca_flow_action_descs *acts_descs OVS_UNUSED,
+                       struct doca_flow_fwd *fwd,
+                       struct doca_flow_monitor *monitor,
+                       struct doca_flow_handle *hndl)
+{
+    for (; actions->type != RTE_FLOW_ACTION_TYPE_END; actions++) {
+        int act_type = actions->type;
+
+        if (act_type == RTE_FLOW_ACTION_TYPE_DROP) {
+            fwd->type = DOCA_FLOW_FWD_DROP;
+        } else if (act_type == RTE_FLOW_ACTION_TYPE_PORT_ID) {
+            const struct rte_flow_action_port_id *port_id = actions->conf;
+
+            fwd->type = DOCA_FLOW_FWD_PORT;
+            fwd->port_id = port_id->id;
+        } else if (act_type == RTE_FLOW_ACTION_TYPE_COUNT) {
+            monitor->flags |= DOCA_FLOW_MONITOR_COUNT;
+        } else if (act_type == RTE_FLOW_ACTION_TYPE_JUMP) {
+            const struct rte_flow_action_jump *jump = actions->conf;
+            struct doca_ctl_pipe_ctx *next_pipe;
+
+            next_pipe = doca_ctl_pipe_ctx_ref(netdev, jump->group);
+            if (!next_pipe) {
+                return -1;
+            }
+
+            fwd->type = DOCA_FLOW_FWD_PIPE;
+            fwd->next_pipe = next_pipe->pipe;
+            hndl->next_pipe = next_pipe;
+            hndl->next_group = jump->group;
+        } else {
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+static struct doca_flow_pipe_entry *
+create_doca_flow_entry(struct doca_ctl_pipe_ctx *self_pipe,
+                       uint32_t prio,
+                       struct doca_flow_match *spec,
+                       struct doca_flow_match *mask,
+                       struct doca_flow_actions *actions,
+                       struct doca_flow_action_descs *action_descs,
+                       struct doca_flow_monitor *monitor,
+                       struct doca_flow_fwd *fwd,
+                       struct rte_flow_error *error)
+{
+    unsigned int tid = netdev_offload_thread_id();
+    struct doca_flow_pipe *pipe = self_pipe->pipe;
+    struct netdev *netdev = self_pipe->netdev;
+    struct doca_flow_pipe_entry *entry;
+    doca_error_t err;
+
+    err = doca_flow_pipe_control_add_entry(tid, prio, pipe, spec, mask, actions,
+                                           action_descs, monitor, fwd, &entry);
+    if (err) {
+        VLOG_WARN_RL(&rl, "%s: Failed to create ctl pipe entry. Error: %d (%s)",
+                     netdev_get_name(netdev), err, doca_get_error_string(err));
+        error->type = RTE_FLOW_ERROR_TYPE_HANDLE;
+        error->message = doca_get_error_string(err);
+        return NULL;
+    }
+
+    return entry;
 }
 
 static struct rte_flow *
-dpdk_offload_doca_create(struct netdev *netdev OVS_UNUSED,
-                         const struct rte_flow_attr *attr OVS_UNUSED,
-                         const struct rte_flow_item *items OVS_UNUSED,
-                         const struct rte_flow_action *actions OVS_UNUSED,
-                         struct rte_flow_error *error OVS_UNUSED)
+dpdk_offload_doca_create(struct netdev *netdev,
+                         const struct rte_flow_attr *attr,
+                         const struct rte_flow_item *items,
+                         const struct rte_flow_action *actions,
+                         struct rte_flow_error *error)
 {
+    struct doca_flow_action_descs dacts_descs;
+    struct doca_ctl_pipe_ctx *pipe_ctx;
+    struct doca_flow_monitor monitor;
+    struct doca_flow_actions dacts;
+    struct doca_flow_handle *hndl;
+    struct doca_flow_match mask;
+    struct doca_flow_match spec;
+    struct doca_flow_fwd fwd;
+    uint32_t prio;
+
+    memset(&dacts_descs, 0x0, sizeof dacts_descs);
+    memset(&monitor, 0x0, sizeof monitor);
+    memset(&dacts, 0x0, sizeof dacts);
+    memset(&mask, 0x0, sizeof mask);
+    memset(&spec, 0x0, sizeof spec);
+    memset(&fwd, 0x0, sizeof fwd);
+
+    if (doca_translate_items(netdev, items, &spec, &mask)) {
+        return NULL;
+    }
+
+    hndl = xzalloc(sizeof *hndl);
+
+    /* get self table pointer */
+    pipe_ctx = doca_ctl_pipe_ctx_ref(netdev, attr->group);
+    if (!pipe_ctx) {
+        error->type = RTE_FLOW_ERROR_TYPE_UNSPECIFIED;
+        error->message = "Could not create table";
+        goto err_pipe;
+    }
+
+    /* parse actions */
+    if (doca_translate_actions(netdev, actions, &dacts, &dacts_descs,
+                               &fwd, &monitor, hndl)) {
+        error->type = RTE_FLOW_ERROR_TYPE_ACTION;
+        error->message = "Could not create actions";
+        goto err_actions;
+    }
+
+    /* insert rule */
+    prio = (hndl->next_group == MISS_TABLE_ID);
+    hndl->flow = create_doca_flow_entry(pipe_ctx, prio, &spec, &mask,
+                                        &dacts, &dacts_descs, &monitor, &fwd, error);
+    if (!hndl->flow) {
+        error->type = RTE_FLOW_ERROR_TYPE_HANDLE;
+        error->message = "Could not insert rule";
+        goto err_insert;
+    }
+
+    hndl->self_pipe = pipe_ctx;
+
+    return (struct rte_flow *) (void *) hndl;
+
+err_insert:
+    /* change to free doca flow resources function */
+    if (hndl->next_pipe) {
+        doca_ctl_pipe_ctx_unref(hndl->next_pipe);
+    }
+err_actions:
+    doca_ctl_pipe_ctx_unref(pipe_ctx);
+err_pipe:
+    free(hndl);
+
     return NULL;
+}
+
+static doca_error_t
+destroy_doca_flow_entry(struct doca_flow_pipe_entry *flow)
+{
+    unsigned int tid = netdev_offload_thread_id();
+
+    return doca_flow_pipe_rm_entry(tid, DOCA_FLOW_NO_WAIT, NULL, flow);
 }
 
 static int
 dpdk_offload_doca_destroy(struct netdev *netdev OVS_UNUSED,
-                          struct rte_flow *rte_flow OVS_UNUSED,
-                          struct rte_flow_error *error OVS_UNUSED,
+                          struct rte_flow *rte_flow,
+                          struct rte_flow_error *error,
                           bool esw_port_id OVS_UNUSED)
 {
-    return -1;
+    struct doca_flow_handle *hndl;
+    doca_error_t err;
+
+    hndl = (struct doca_flow_handle *) (void *) rte_flow;
+
+    err = destroy_doca_flow_entry(hndl->flow);
+    if (err) {
+        error->type = RTE_FLOW_ERROR_TYPE_HANDLE;
+        error->message = doca_get_error_string(err);
+        return -1;
+    }
+
+    if (hndl->next_pipe) {
+        doca_ctl_pipe_ctx_unref(hndl->next_pipe);
+    }
+
+    doca_ctl_pipe_ctx_unref(hndl->self_pipe);
+    free(hndl);
+
+    return 0;
 }
 
 static int
-dpdk_offload_doca_query_count(struct netdev *netdev OVS_UNUSED,
-                              struct rte_flow *rte_flow OVS_UNUSED,
-                              struct rte_flow_query_count *query OVS_UNUSED,
-                              struct rte_flow_error *error OVS_UNUSED)
+dpdk_offload_doca_query_count(struct netdev *netdev,
+                              struct rte_flow *rte_flow,
+                              struct rte_flow_query_count *query,
+                              struct rte_flow_error *error)
 {
-    return -1;
+    struct doca_flow_pipe_entry *doca_flow;
+    struct doca_flow_handle *hndl;
+    struct doca_flow_query stats;
+    doca_error_t err;
+
+    hndl = (struct doca_flow_handle *) (void *) rte_flow;
+    doca_flow = hndl->flow;
+
+    memset(query, 0, sizeof *query);
+    memset(&stats, 0, sizeof stats);
+
+    err = doca_flow_query_entry(doca_flow, &stats);
+    if (err) {
+        VLOG_DBG_RL(&rl, "%s: Failed to query doca_flow: %p. Error %d (%s)",
+                    netdev_get_name(netdev), doca_flow, err,
+                    doca_get_error_string(err));
+        error->type = RTE_FLOW_ERROR_TYPE_UNSPECIFIED;
+        error->message = doca_get_error_string(err);
+        return -1;
+    }
+
+    query->hits = stats.total_pkts;
+    query->bytes = stats.total_bytes;
+
+    return 0;
 }
 
 static struct rte_flow_action_handle *
@@ -208,26 +590,39 @@ dpdk_offload_doca_shared_query(int port_id OVS_UNUSED,
 }
 
 static void
-dpdk_offload_doca_get_pkt_recover_info(struct dp_packet *p OVS_UNUSED,
+dpdk_offload_doca_get_pkt_recover_info(struct dp_packet *p,
                                        struct dpdk_offload_recovery_info *info)
 {
-    memset(info, 0, sizeof *info);
-    return;
+    uint32_t flow_miss_id;
+
+    memset(info, 0x0, sizeof *info);
+    if (!dp_packet_get_meta(p, &flow_miss_id)) {
+        return;
+    }
+
+    info->flow_miss_id = flow_miss_id;
 }
 
 static int
-dpdk_offload_doca_netdev_data_destroy(void *data OVS_UNUSED)
+dpdk_offload_doca_netdev_data_destroy(void *data)
 {
-    return -1;
+    return netdev_dpdk_doca_port_destroy((struct netdev *) data);
 }
 
 static void
-dpdk_offload_doca_update_stats(struct dpif_flow_stats *stats OVS_UNUSED,
+dpdk_offload_doca_update_stats(struct dpif_flow_stats *stats,
                                struct dpif_flow_attrs *attrs,
-                               struct rte_flow_query_count *query OVS_UNUSED)
+                               struct rte_flow_query_count *query)
 {
     attrs->dp_layer = "doca";
-    return;
+
+    if (stats->n_packets != query->hits) {
+        query->hits_set = 1;
+        query->bytes_set = 1;
+    }
+
+    stats->n_packets = query->hits;
+    stats->n_bytes = query->bytes;
 }
 
 struct dpdk_offload_api dpdk_offload_api_doca = {

lib/dpdk-offload-provider.h
@@ -108,5 +108,6 @@ struct dpdk_offload_api {
 };
 
 extern struct dpdk_offload_api dpdk_offload_api_rte;
+extern struct dpdk_offload_api dpdk_offload_api_doca;
 
 #endif /* DPDK_OFFLOAD_PROVIDER_H */

lib/netdev-dpdk.c
@@ -1298,11 +1298,18 @@ dpdk_eth_dev_init(struct netdev *netdev)
         return -diag;
     }
 
-    diag = rte_eth_dev_start(dev->port_id);
-    if (diag) {
-        VLOG_ERR("Interface %s start error: %s", dev->up.name,
-                 rte_strerror(-diag));
-        return -diag;
+    if (ovs_doca_enabled()) {
+        diag = netdev_dpdk_doca_port_create(netdev);
+        if (diag) {
+            return -1;
+        }
+    } else {
+        diag = rte_eth_dev_start(dev->port_id);
+        if (diag) {
+            VLOG_ERR("Interface %s start error: %s", dev->up.name,
+                     rte_strerror(-diag));
+            return -diag;
+        }
     }
     dev->started = true;
 

lib/netdev-offload-dpdk.c
@@ -280,6 +280,7 @@ offload_data_destroy(struct netdev *netdev)
     cmap_destroy(&data->ufid_to_rte_flow);
     ovsrcu_postpone(offload_data_destroy__, data);
 
+    offload->netdev_data_destroy(netdev);
     ovsrcu_set(&netdev->hw_info.offload_data, NULL);
 }
 
@@ -5679,7 +5680,12 @@ offload_provider_api_init(void)
     static struct ovsthread_once init_once = OVSTHREAD_ONCE_INITIALIZER;
 
     if (ovsthread_once_start(&init_once)) {
-        offload = &dpdk_offload_api_rte;
+        if (ovs_doca_enabled()) {
+            offload = &dpdk_offload_api_doca;
+        } else {
+            offload = &dpdk_offload_api_rte;
+        }
+
         ovsthread_once_done(&init_once);
     }
 }

lib/ovs-doca-stub.c
@@ -19,6 +19,11 @@
 #include "smap.h"
 #include "vswitch-idl.h"
 
+#ifdef DPDK_NETDEV
+#include "dpdk-offload-provider.h"
+struct dpdk_offload_api dpdk_offload_api_doca = {0};
+#endif /* DPDK_NETDEV */
+
 VLOG_DEFINE_THIS_MODULE(doca);
 
 int

commit 98971e4e44301ddf1ef8638caa49f1a5387ada1b

dpdk-offload-doca: Remove OVS_UNUSED attribute of doca_translate_actions(netdev)
The argument is used. Remove the wrong OVS_UNUSED attribute.

Fixes: 06c5162fb242 ("dpdk-offload-doca: Implement DOCA provider")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/dpdk-offload-doca.c
@@ -987,7 +987,7 @@ doca_translate_vxlan_encap(const struct rte_flow_action *action,
 }
 
 static int
-doca_translate_actions(struct netdev *netdev OVS_UNUSED,
+doca_translate_actions(struct netdev *netdev,
                        struct doca_flow_match *spec,
                        const struct rte_flow_action *actions,
                        struct doca_flow_actions *dacts,