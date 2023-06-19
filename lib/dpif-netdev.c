elibritstein/OVS

commit 5ba0bfd2272ccee780b0a6bf9aac1ab884773459

netdev-offload-dpdk: Flush ESW members when flushing the ESW manager
Offload flows are applied on the ESW manager of each netdev. If it is
flushed, the offloads of its members should be flushed, as they depend
on it.

Issue: 3129943
Change-Id: Id02c0a6516f75be9ba0abd14c3c2c12ffcd46edd
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -5852,11 +5852,11 @@ static void
 ct_tables_uninit(struct netdev *netdev, unsigned int tid);
 
 static int
-netdev_offload_dpdk_flow_flush(struct netdev *netdev)
+netdev_offload_dpdk_flow_flush__(struct netdev *netdev)
 {
+    unsigned int tid = netdev_offload_thread_id();
     struct cmap *map = offload_data_map(netdev);
     struct ufid_to_rte_flow_data *data;
-    unsigned int tid = netdev_offload_thread_id();
 
     if (!map) {
         return -1;
@@ -5874,6 +5874,67 @@ netdev_offload_dpdk_flow_flush(struct netdev *netdev)
     }
 
     return 0;
+}
+
+struct flush_esw_members_aux {
+    struct netdev *esw_netdev;
+    int ret;
+};
+
+/* Upon flushing the ESW manager, its members netdevs should be flushed too,
+ * as their offloads are done on it.
+ */
+static bool
+flush_esw_members_cb(struct netdev *netdev,
+                     odp_port_t odp_port OVS_UNUSED,
+                     void *aux_)
+{
+    struct flush_esw_members_aux *aux = aux_;
+    struct netdev *esw_netdev;
+    int netdev_esw_mgr_pid;
+    int esw_mgr_pid;
+
+    esw_netdev = aux->esw_netdev;
+
+    /* Skip the ESW netdev itself. */
+    if (netdev == esw_netdev) {
+        return false;
+    }
+
+    netdev_esw_mgr_pid = netdev_dpdk_get_esw_mgr_port_id(netdev);
+    esw_mgr_pid = netdev_dpdk_get_esw_mgr_port_id(esw_netdev);
+
+    /* Skip a non-member. */
+    if (netdev_esw_mgr_pid == -1 || esw_mgr_pid == -1 ||
+        netdev_esw_mgr_pid != esw_mgr_pid) {
+        return false;
+    }
+
+    if (netdev_offload_dpdk_flow_flush__(netdev)) {
+        aux->ret = -1;
+        return true;
+    }
+
+    return false;
+}
+
+static int
+netdev_offload_dpdk_flow_flush(struct netdev *netdev)
+{
+    struct flush_esw_members_aux aux = {
+        .esw_netdev = netdev,
+        .ret = 0,
+    };
+
+    if (netdev_offload_dpdk_flow_flush__(netdev)) {
+        return -1;
+    }
+
+    if (!netdev_vport_is_vport_class(netdev->netdev_class)) {
+        netdev_ports_traverse(netdev->dpif_type, flush_esw_members_cb, &aux);
+    }
+
+    return aux.ret;
 }
 
 struct get_vport_netdev_aux {

commit 23cd09106d637339700a080773f71c4f05fb4b00

dpif-netdev: Implement flow flushing for proper port management upon deletion
To ensure the proper management of ports, it is essential to flush
port-related flows when a port is deleted. Additionally, when an
ESW manager port is deleted, its member flows should be flushed
along with its own flows. To address these requirements, implement
a dp_netdev_port_flow_flush routine that handles the necessary flow
flushing operations.

This commit allowed flows to be already flushed when getting to the
netdev-offload-dpdk layer, hence add an error in case we hit the
flush_esw_members routine.

Fixes: 5ba0bfd2272c ("netdev-offload-dpdk: Flush ESW members when flushing the ESW manager")
Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

lib/dpif-netdev.c
@@ -1015,6 +1015,9 @@ static void reconfigure_datapath(struct dp_netdev *dp)
 static bool dp_netdev_pmd_try_ref(struct dp_netdev_pmd_thread *pmd);
 static void dp_netdev_pmd_unref(struct dp_netdev_pmd_thread *pmd);
 static void dp_netdev_pmd_flow_flush(struct dp_netdev_pmd_thread *pmd);
+static void
+dp_netdev_port_flow_flush(struct dp_netdev *dp, struct dp_netdev_port *port);
+
 static void pmd_load_cached_ports(struct dp_netdev_pmd_thread *pmd)
     OVS_REQUIRES(pmd->port_mutex);
 static inline void
@@ -3244,6 +3247,7 @@ do_del_port(struct dp_netdev *dp, struct dp_netdev_port *port)
      * revalidator threads are still active and can still enqueue
      * offload modification or deletion. Managing those stray requests
      * is done in the offload threads. */
+    dp_netdev_port_flow_flush(dp, port);
     dp_netdev_offload_flush(dp, port);
     netdev_uninit_flow_api(port->netdev);
 
@@ -4754,18 +4758,52 @@ get_dpif_flow_status(const struct dp_netdev *dp,
                      struct dpif_flow_attrs *attrs);
 
 static void
-dp_netdev_pmd_flow_flush(struct dp_netdev_pmd_thread *pmd)
+dp_netdev_pmd_flow_flush__(struct dp_netdev_pmd_thread *pmd, struct dp_netdev_port *port)
 {
     struct dp_netdev_flow *netdev_flow;
 
     ovs_mutex_lock(&pmd->flow_mutex);
     CMAP_FOR_EACH (netdev_flow, node, &pmd->flow_table) {
+        odp_port_t flow_port_no = netdev_flow->flow.in_port.odp_port;
         struct dpif_flow_attrs attrs;
+
+        if (port != NULL && flow_port_no != port->port_no) {
+            continue;
+        }
 
         get_dpif_flow_status(pmd->dp, netdev_flow, NULL, &attrs);
         dp_netdev_pmd_remove_flow(pmd, netdev_flow, attrs.offloaded);
     }
     ovs_mutex_unlock(&pmd->flow_mutex);
+}
+
+static void
+dp_netdev_pmd_flow_flush(struct dp_netdev_pmd_thread *pmd)
+{
+    dp_netdev_pmd_flow_flush__(pmd, NULL);
+}
+
+static void
+dp_netdev_port_flow_flush(struct dp_netdev *dp, struct dp_netdev_port *port)
+{
+    struct dp_netdev_pmd_thread *pmd;
+
+    if (netdev_dpdk_is_esw_mgr(port->netdev)) {
+        struct dp_netdev_port *iter_port;
+        int esw_mgr_pid;
+
+        esw_mgr_pid = netdev_dpdk_get_esw_mgr_port_id(port->netdev);
+
+        HMAP_FOR_EACH (iter_port, node, &dp->ports) {
+            if (esw_mgr_pid == netdev_dpdk_get_esw_mgr_port_id(iter_port->netdev)) {
+                dp_netdev_port_flow_flush(dp, iter_port);
+            }
+        }
+    }
+
+    CMAP_FOR_EACH (pmd, node, &dp->poll_threads) {
+        dp_netdev_pmd_flow_flush__(pmd, port);
+    }
 }
 
 static int

lib/netdev-offload-dpdk.c
@@ -5962,7 +5962,13 @@ flush_esw_members_op(struct netdev *netdev,
                      odp_port_t odp_port OVS_UNUSED,
                      void *aux_)
 {
+    struct cmap *map = offload_data_map(netdev);
     struct esw_members_aux *aux = aux_;
+
+    if (!cmap_is_empty(map)) {
+        VLOG_ERR("Incomplete flush: %s should have been empty",
+                 netdev_get_name(netdev));
+    }
 
     if (flush_netdev_flows_in_related(netdev, netdev)) {
         aux->ret = -1;