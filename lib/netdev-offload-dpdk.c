elibritstein/OVS

commit 36a8ac4a99ac5eb138f06b08d395e4f0b3ee00b9

netdev-offload-dpdk: Support dp-hash action
Example OpenFlow:
ovs-ofctl add-group br-int0 group_id=1,type=select,
    bucket=watch_port=<DEST0>,output:<DEST0>,
    bucket=watch_port=<DEST1>,output:<DEST1>
ovs-ofctl add-flow br-int0 in_port=eth4,ip,nw_src=4.4.4.10,actions=group=1

The dp-flow that is create hash the form of:
M,recirc_id(0), actions:hash(sym_l4(0)),recirc(0x1)
For offload, hash action must be followed by a recirc actions, reject
otherwise.

Introduce a proprietary action for dp-hash. In rte provider, fail it. In
doca provider, create a hash-pipe in the target ctl-pipe. The next recirc
flow will match on the packet's dp-hash. Support its recovery upon miss.

For example, an offload for a dp-flow like the above is as follows:
CTL-PIPE-CTX#0     CTL-PIPE-CTX#1
ctl-pipe0 ->       get_ctl_pipe_root(ctl-pipe1) -> ctl-pipe1

In ctl-pipe0 there will be M matches, and the action will be fwd to the
get_ctl_pipe_root(ctl-pipe1) pipe.

Issue: OVS-3407068
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -1117,7 +1117,8 @@ OVS_ASSERT_PACKED(struct flow_miss_ctx,
     odp_port_t vport;
     uint32_t recirc_id;
     uint8_t skip_actions;
-    uint8_t pad0[7];
+    uint8_t has_dp_hash;
+    uint8_t pad0[6];
     struct flow_tnl tnl;
 );
 
@@ -2364,6 +2365,8 @@ dump_flow_action(struct ds *s, struct ds *s_extra,
          * should never be actually used.
          */
         OVS_NOT_REACHED();
+    } else if (actions->type == OVS_RTE_FLOW_ACTION_TYPE(HASH)) {
+        ds_put_cstr(s, "hash / ");
     } else {
         ds_put_format(s, "unknown rte flow action (%d)\n", actions->type);
     }
@@ -2422,6 +2425,7 @@ struct act_vars {
     rte_be16_t vlan_tpid;
     uint8_t vlan_pcp;
     uint8_t proto;
+    bool has_dp_hash;
 };
 
 static int
@@ -2460,6 +2464,8 @@ dpdk_offload_rte_create(struct netdev *netdev,
              * should never be actually used.
              */
             OVS_NOT_REACHED();
+        } else if (act_type == OVS_RTE_FLOW_ACTION_TYPE(HASH)) {
+            return -1;
         }
     }
 
@@ -4433,6 +4439,7 @@ add_recirc_action(struct netdev *netdev,
     miss_ctx.vport = act_vars->vport;
     miss_ctx.recirc_id = nl_attr_get_u32(nla);
     miss_ctx.skip_actions = act_vars->pre_ct_cnt;
+    miss_ctx.has_dp_hash = act_vars->has_dp_hash;
     if (act_vars->vport != ODPP_NONE) {
         get_tnl_masked(&miss_ctx.tnl, NULL, act_vars->tnl_key,
                        &act_vars->tnl_mask);
@@ -5252,6 +5259,11 @@ parse_flow_actions(struct netdev *flowdev,
     }
 
     NL_ATTR_FOR_EACH_UNSAFE (nla, left, nl_actions, nl_actions_len) {
+        if (act_vars->has_dp_hash &&
+            nl_attr_type(nla) != OVS_ACTION_ATTR_RECIRC) {
+            return -1;
+        }
+
         if (nl_attr_type(nla) == OVS_ACTION_ATTR_OUTPUT) {
             /* The last output should use port-id action, while previous
              * outputs should embed the port-id action inside a sample action.
@@ -5432,6 +5444,10 @@ parse_flow_actions(struct netdev *flowdev,
                                      offload->reg_fields()[REG_FIELD_CT_MARK].mask);
             add_action_set_reg_field(actions, REG_FIELD_CT_LABEL_ID, 0,
                                      offload->reg_fields()[REG_FIELD_CT_LABEL_ID].mask);
+        } else if (nl_attr_type(nla) == OVS_ACTION_ATTR_HASH) {
+            act_vars->has_dp_hash = true;
+            add_flow_action(actions, OVS_RTE_FLOW_ACTION_TYPE_HASH, NULL);
+            continue;
         } else {
             VLOG_DBG_RL(&rl, "Unsupported action type %d", nl_attr_type(nla));
             return -1;
@@ -6155,6 +6171,15 @@ netdev_offload_dpdk_hw_miss_packet_recover(struct netdev *netdev,
         }
         *skip_actions = flow_miss_ctx.skip_actions;
         packet->md.recirc_id = flow_miss_ctx.recirc_id;
+        if (flow_miss_ctx.has_dp_hash) {
+            /* OVS will not match on dp-hash unless it has a non-zero value.
+             * The 4 LSBs are matched, but zero value is valid. In order to
+             * have a non-zero value in the dp-hash of the packet, set the 5th
+             * bit.
+             */
+            packet->md.dp_hash = info.dp_hash | 0x10;
+        }
+
         if (flow_miss_ctx.vport != ODPP_NONE) {
             if (is_all_zeros(&flow_miss_ctx.tnl, sizeof flow_miss_ctx.tnl)) {
                 vport_netdev = netdev_ports_get(flow_miss_ctx.vport,

lib/dpdk-offload-doca.c
@@ -1176,7 +1176,6 @@ doca_hash_pipe_init(struct netdev *netdev,
     return 0;
 }
 
-OVS_UNUSED
 static struct doca_hash_pipe_ctx *
 doca_hash_pipe_ctx_init(struct doca_flow_pipe *next_pipe,
                         struct netdev *netdev,
@@ -1288,7 +1287,6 @@ err:
     return NULL;
 }
 
-OVS_UNUSED
 static struct doca_flow_pipe *
 get_ctl_pipe_root(struct doca_ctl_pipe_ctx *next_pipe_ctx,
                   struct doca_flow_match *spec,
@@ -1323,6 +1321,7 @@ doca_translate_actions(struct netdev *netdev,
     struct doca_flow_header_format *outer_masks = &dacts_masks->outer;
     struct doca_flow_header_format *outer = &dacts->outer;
     bool vlan_act_push = false;
+    bool has_dp_hash = false;
 
     for (; actions->type != RTE_FLOW_ACTION_TYPE_END; actions++) {
         int act_type = actions->type;
@@ -1438,9 +1437,18 @@ doca_translate_actions(struct netdev *netdev,
             if (!next_pipe_ctx) {
                 return -1;
             }
+            if (has_dp_hash) {
+                next_pipe_ctx->hash_pipe_ctx =
+                    doca_hash_pipe_ctx_init(next_pipe_ctx->pipe, netdev,
+                                            jump->group);
+                if (next_pipe_ctx->hash_pipe_ctx == NULL) {
+                    return -1;
+                }
+            }
 
             fwd->type = DOCA_FLOW_FWD_PIPE;
-            fwd->next_pipe = next_pipe_ctx->pipe;
+            fwd->next_pipe = get_ctl_pipe_root(next_pipe_ctx, spec,
+                                               has_dp_hash);
             flow_res->next_pipe_ctx = next_pipe_ctx;
             flow_res->next_group = jump->group;
         } else if (act_type == RTE_FLOW_ACTION_TYPE_VXLAN_ENCAP) {
@@ -1485,6 +1493,8 @@ doca_translate_actions(struct netdev *netdev,
             vlan_act_push = true;
         } else if (act_type == RTE_FLOW_ACTION_TYPE_OF_SET_VLAN_PCP) {
             continue;
+        } else if (act_type == OVS_RTE_FLOW_ACTION_TYPE(HASH)) {
+            has_dp_hash = true;
         } else {
             return -1;
         }
@@ -2043,8 +2053,8 @@ dpdk_offload_doca_get_pkt_recover_info(struct dp_packet *p,
     if (dpdk_offload_get_reg_field(p, REG_FIELD_FLOW_INFO,
                                    &info->flow_miss_id)) {
         dp_packet_set_flow_mark(p, info->flow_miss_id);
-        dpdk_offload_get_reg_field(p, REG_FIELD_CT_CTX,
-                                   &info->ct_miss_id);
+        dpdk_offload_get_reg_field(p, REG_FIELD_CT_CTX, &info->ct_miss_id);
+        dpdk_offload_get_reg_field(p, REG_FIELD_DP_HASH, &info->dp_hash);
     }
 }
 

lib/dpdk-offload-provider.h
@@ -100,6 +100,7 @@ enum {
     OVS_RTE_FLOW_ACTION_TYPE_FLOW_INFO = INT_MIN,
     OVS_RTE_FLOW_ACTION_TYPE_CT_INFO,
     OVS_RTE_FLOW_ACTION_TYPE_PRE_CT_END,
+    OVS_RTE_FLOW_ACTION_TYPE_HASH,
 };
 
 #define OVS_RTE_FLOW_ACTION_TYPE(TYPE) \
@@ -117,6 +118,7 @@ struct dpdk_offload_recovery_info {
     uint32_t flow_miss_id;
     uint32_t ct_miss_id;
     uint32_t sflow_id;
+    uint32_t dp_hash;
 };
 
 enum dpdk_reg_id {

commit 02cc215f530cbff0c643effd1ba6aef51d98e5ed

netdev-offload-dpdk: Fix dp-hash custom type use
When using custom OVS rte-flow types, a macro is meant to be used but
was omitted.

Fixes: 36a8ac4a99ac ("netdev-offload-dpdk: Support dp-hash action")
Fixes: 329560c59f5d ("dpdk-offload-doca: Support dp-hash match")
Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -3781,7 +3781,7 @@ parse_flow_match(struct netdev *netdev,
         mask = per_thread_xzalloc(sizeof *mask);
         spec->id = match->flow.dp_hash;
         mask->id = match->wc.masks.dp_hash;
-        add_flow_pattern(patterns, OVS_RTE_FLOW_ITEM_TYPE_HASH, spec, mask,
+        add_flow_pattern(patterns, OVS_RTE_FLOW_ITEM_TYPE(HASH), spec, mask,
                          NULL);
         match->wc.masks.dp_hash = 0;
     }
@@ -5472,7 +5472,7 @@ parse_flow_actions(struct netdev *flowdev,
                                      offload->reg_fields()[REG_FIELD_CT_LABEL_ID].mask);
         } else if (nl_attr_type(nla) == OVS_ACTION_ATTR_HASH) {
             act_vars->has_dp_hash = true;
-            add_flow_action(actions, OVS_RTE_FLOW_ACTION_TYPE_HASH, NULL);
+            add_flow_action(actions, OVS_RTE_FLOW_ACTION_TYPE(HASH), NULL);
             continue;
         } else {
             VLOG_DBG_RL(&rl, "Unsupported action type %d", nl_attr_type(nla));