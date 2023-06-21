elibritstein/OVS

commit 8a3f9c5d598d09e632e5f64cb455373126f453e5

offload-metadata: Fix delayed priv uninit
Delayed init and uninit in offload metadata is only used for managing
flow miss contexts. The miss rule of these MD must be immediately
removed while the entry itself must have a delay.

The previous fix to offload-metadata missing priv_uninit only fixed
the offload-metadata logic in a vacuum. It breaks the flow miss context
handling.

Revert back to a mode where the priv is uninitialized immediately,
regardless of a delay being set.

To prevent the double reference preventing sometimes priv uninit from
executing, separate the reference counting between the entry itself
which is delayed and the priv which is not.

It allows triggering the uninit at the immediate release call,
while allowing to take back a reference in the delayed mode.

Fixes: 6e92cd562784 ("offload-metadata: Fix delayed priv uninit not being executed")
Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

lib/offload-metadata.c
@@ -57,7 +57,7 @@ struct data_entry {
     uint32_t id;
     struct ovsrcu_gc_node gc_node;
     struct ovs_refcount refcount;
-    bool priv_init_done;
+    struct ovs_refcount priv_refcount;
     void *data;
     void *priv;
 };
@@ -95,12 +95,6 @@ data_entry_destroy(struct offload_metadata *md, struct data_entry *entry,
 
     ovs_mutex_lock(&md->maps_lock);
 
-
-    if (md->priv_uninit) {
-        md->priv_uninit(entry->priv);
-        entry->priv_init_done = false;
-    }
-
     if (entry->id != 0) {
         if (!associated) {
             cmap_remove(&md->i2d_map, &entry->i2d_node, entry->i2d_hash);
@@ -132,6 +126,17 @@ offload_metadata_remove_entry(struct offload_metadata *md, unsigned int uid,
 {
     struct release_item *item;
     struct ovs_list *list;
+
+    if (md->priv_uninit) {
+        if (ovs_refcount_unref(&entry->priv_refcount) == 1) {
+            /* Immediately uninit the priv, while the data
+             * release is delayed. If another object takes a ref
+             * on the data, the priv will be re-initialized. */
+            ovs_mutex_lock(&md->maps_lock);
+            md->priv_uninit(entry->priv);
+            ovs_mutex_unlock(&md->maps_lock);
+        }
+    }
 
     if (md->delay == 0) {
         data_entry_destroy(md, entry, associated);
@@ -329,17 +334,21 @@ offload_metadata_priv_get(struct offload_metadata *md, void *data,
             if (id) {
                 *id = data_cur->id;
             }
-            if (md->priv_init && !data_cur->priv_init_done) {
-                int ret;
+            if (md->priv_init) {
+                if (ovs_refcount_read(&data_cur->priv_refcount) == 0) {
+                    int ret;
 
-                ovs_mutex_lock(&md->maps_lock);
-                ret = md->priv_init(data_cur->priv, priv_arg, data_cur->id);
-                if (ret) {
+                    ovs_mutex_lock(&md->maps_lock);
+                    ret = md->priv_init(data_cur->priv, priv_arg, data_cur->id);
+                    if (ret) {
+                        ovs_mutex_unlock(&md->maps_lock);
+                        return NULL;
+                    }
                     ovs_mutex_unlock(&md->maps_lock);
-                    return NULL;
+                    ovs_refcount_init(&data_cur->priv_refcount);
+                } else if (take_ref) {
+                    ovs_refcount_ref(&data_cur->priv_refcount);
                 }
-                data_cur->priv_init_done = true;
-                ovs_mutex_unlock(&md->maps_lock);
             }
             return data_cur->priv;
         }
@@ -362,11 +371,13 @@ offload_metadata_priv_get(struct offload_metadata *md, void *data,
     ovs_refcount_init(&data_cur->refcount);
     data_cur->id = alloc_id;
     ovs_mutex_lock(&md->maps_lock);
-    if (md->priv_init && md->priv_init(data_cur->priv, priv_arg, alloc_id)) {
-        ovs_mutex_unlock(&md->maps_lock);
-        goto err_priv_init;
+    if (md->priv_init) {
+        if (md->priv_init(data_cur->priv, priv_arg, alloc_id)) {
+            ovs_mutex_unlock(&md->maps_lock);
+            goto err_priv_init;
+        }
+        ovs_refcount_init(&data_cur->priv_refcount);
     }
-    data_cur->priv_init_done = true;
     data_cur->d2i_hash = dhash;
     cmap_insert(&md->d2i_map, &data_cur->d2i_node, dhash);
     if (alloc_id != 0) {

commit 2f877efa3bcabbff3e51551468b71f7695916378

offload-metadata: Fix associated data priv references
In 'associated' mode, there is no priv init or reference taking
when creating the shallow reference to the original node.

When destroying an entry, only dereference the priv if
the node is not an associated one.

Add a unit test verifying offload-metadata operations when
using ID association and priv init/uninit at the same time.

This is used currently by the flow-id <-> mark map but it
is not tested.

Fixes: 8a3f9c5d598d ("offload-metadata: Fix delayed priv uninit")
Signed-off-by: Gaetan Rivet <gaetanr@nvidia.com>

lib/offload-metadata.c
@@ -127,7 +127,12 @@ offload_metadata_remove_entry(struct offload_metadata *md, unsigned int uid,
     struct release_item *item;
     struct ovs_list *list;
 
-    if (md->priv_uninit) {
+    /* 'Associated' nodes only exist as shallow
+     * references to the original entry in the d2i map,
+     * and they do not hold priv references. In such
+     * case, do nothing. */
+
+    if (md->priv_uninit && entry->d2i_hash != 0) {
         if (ovs_refcount_unref(&entry->priv_refcount) == 1) {
             /* Immediately uninit the priv, while the data
              * release is delayed. If another object takes a ref

tests/test-offload-metadata.c
@@ -411,6 +411,113 @@ test_offload_metadata_id_priv(long long int delay)
 }
 
 static void
+test_offload_metadata_id_set_priv(long long int delay)
+{
+    struct offload_metadata_parameters params = {
+        .id_alloc = id_alloc,
+        .id_free = id_free,
+        .priv_size = sizeof(struct priv),
+        .priv_init = priv_init,
+        .priv_uninit = priv_uninit,
+        .release_delay_ms = delay,
+    };
+    struct offload_metadata *md;
+    long long int release_start;
+    struct priv *privs[N];
+    struct data datas[N];
+    uint32_t override[N];
+    uint32_t ids[N];
+
+    /* Test an offload metadata map that uses
+     * IDs and privs, but also specifically choose
+     * some IDs. */
+    md = offload_metadata_create(test_params.n_threads, "test-md-id-set-priv",
+                                 sizeof(struct data), data_format,
+                                 params);
+
+    for (int i = 0; i < N; i++) {
+        struct arg arg = {
+            .ptr = &datas[i],
+        };
+
+        datas[i].idx = i;
+        override[i] = N + i + 1;
+        ovs_assert(0 == offload_metadata_id_ref(md, &datas[i], &arg, &ids[i]));
+    }
+
+    for (int i = 0; i < N; i++) {
+        struct priv *priv;
+        struct data cur;
+        uint32_t id;
+
+        priv = offload_metadata_priv_get(md, &datas[i], NULL, &id, false);
+        ovs_assert(ids[i] == id);
+
+        offload_metadata_id_set(md, &datas[i], override[i]);
+
+        /* The shallow reference associated should not have a priv for it. */
+        cur.idx = override[i];
+        ovs_assert(NULL == offload_metadata_priv_get(md, &cur, NULL, NULL,
+                                                     false));
+
+        /* Verify that we find the same priv if we use the original id. */
+        ovs_assert(priv == offload_metadata_priv_get(md, &datas[i],
+                                                     NULL, &id, false));
+
+        ovs_assert(ids[i] == id);
+        ovs_assert(priv->hdl != NULL);
+        ovs_assert(id != 0);
+
+        ovs_assert(0 == offload_metadata_data_from_id(md, override[i], &cur));
+        ovs_assert(0 == memcmp(&cur, &datas[i], sizeof cur));
+
+        ovs_assert(priv == offload_metadata_priv_get(md, &datas[i], NULL,
+                                                     NULL, false));
+
+        /* We have already taken the first reference on this priv when
+         * calling 'offload_metadata_id_ref()' above. */
+
+        privs[i] = priv;
+    }
+
+    release_start = time_msec();
+    for (int i = 0; i < N; i++) {
+        offload_metadata_id_unset(md, 0, override[i]);
+        if (i % 2 == 0) {
+            offload_metadata_priv_unref(md, 0, privs[i]);
+        } else {
+            offload_metadata_id_unref(md, 0, ids[i]);
+        }
+    }
+
+    if (delay) {
+        xnanosleep(delay * 1e6 + 1);
+    }
+    offload_metadata_upkeep(md, 0, time_msec());
+
+    for (int i = 0; i < N; i++) {
+        struct data ff;
+        struct data cur;
+
+        memset(&cur, 0xff, sizeof cur);
+        memset(&ff, 0xff, sizeof ff);
+
+        ovs_assert(0 != offload_metadata_data_from_id(md, override[i], &cur));
+        ovs_assert(0 != offload_metadata_data_from_id(md, ids[i], &cur));
+        /* Verify that 'cur' was not written to. */
+        ovs_assert(0 == memcmp(&cur, &ff, sizeof cur));
+    }
+
+    if (delay != 0) {
+        for (int i = 0; i < N; i++) {
+            ovs_assert(id_free_timestamp[i] - release_start >= delay);
+        }
+    }
+
+    offload_metadata_destroy(md);
+}
+
+static void
 test_offload_metadata_priv(long long int delay)
 {
     struct offload_metadata_parameters params = {
@@ -492,6 +599,8 @@ run_tests(struct ovs_cmdl_context *ctx OVS_UNUSED)
     test_offload_metadata_id_set(5);
     test_offload_metadata_id_priv(0);
     test_offload_metadata_id_priv(5);
+    test_offload_metadata_id_set_priv(0);
+    test_offload_metadata_id_set_priv(5);
     test_offload_metadata_priv(0);
     test_offload_metadata_priv(5);
 }