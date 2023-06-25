elibritstein/OVS

commit 1c809c29b0e8c72ff0caa9e572bb89c7854746fc

netdev-offload-dpdk: Map vports to tables without considering physport
When the packet is recirculated to a virtual port (tunnel at this
point), it might arrive from any port. Do not consider the physport for
the mapping.

Issue: OVS-3340314
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -471,10 +471,13 @@ dump_table_id(struct ds *s, void *data,
               void *priv OVS_UNUSED, void *priv_arg OVS_UNUSED)
 {
     struct table_id_data *table_id_data = data;
+    const char *netdev_name;
 
+    netdev_name = table_id_data->netdev
+                  ? netdev_get_name(table_id_data->netdev)
+                  : NULL;
     ds_put_format(s, "%s, vport=%"PRIu32", recirc_id=%"PRIu32,
-                  netdev_get_name(table_id_data->netdev), table_id_data->vport,
-                  table_id_data->recirc_id);
+                  netdev_name, table_id_data->vport, table_id_data->recirc_id);
     return s;
 }
 
@@ -792,6 +795,10 @@ get_table_id(odp_port_t vport,
     if (is_e2e_cache) {
         *table_id = E2E_BASE_TABLE_ID | vport;
         return 0;
+    }
+
+    if (vport != ODPP_NONE) {
+        table_id_data.netdev = NULL;
     }
 
     table_id_init();

commit 489e37df02843ad5ed9bef5ad8eb0812623b1454

netdev-offload-dpdk: Fix get_table_id for vports
A miss flow is created at table_id_ctx_init() on the netdev provided by
the priv_arg. The cited commit nullified it for vport, cuasing a crash.
Separate the context that is mapped, in which the netdev is NULL to the
priv_arg the preserves the correct netdev.

Fixes: 1c809c29b0e8 ("netdev-offload-dpdk: Map vports to tables without considering physport")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -785,6 +785,7 @@ get_table_id(odp_port_t vport,
         .vport = vport,
         .recirc_id = recirc_id,
     };
+    struct table_id_data priv_arg = table_id_data;
 
     if (vport == ODPP_NONE && recirc_id == 0 &&
         !(netdev_is_e2e_cache_enabled() && !is_e2e_cache)) {
@@ -802,10 +803,8 @@ get_table_id(odp_port_t vport,
     }
 
     table_id_init();
-    return offload_metadata_id_ref(table_id_md,
-                       &table_id_data,
-                       &table_id_data,
-                       table_id);
+    return offload_metadata_id_ref(table_id_md, &table_id_data, &priv_arg,
+                                   table_id);
 }
 
 static void