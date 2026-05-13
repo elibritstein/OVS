6126e7c09c50c5f353db2143c071167de09cd0b0
Eli Britstein <elibr@nvidia.com>
2026-05-13 17:13:03 +0300

    h

--- lib/ovs-doca.h
+++ lib/ovs-doca.h
@@ -17,17 +17,16 @@
 #ifndef OVS_DOCA_H
 #define OVS_DOCA_H
 
-struct ovsrec_open_vswitch;
-struct smap;
+#include "netdev.h"
+#include "ovs-thread.h"
+#include "smap.h"
+#include "util.h"
+#include "vswitch-idl.h"
 
 #ifdef DOCA_NETDEV
 
 #include <doca_dev.h>
 #include <doca_flow.h>
-
-#include "dp-packet.h"
-#include "ovs-thread.h"
-#include "util.h"
 
 #define AUX_QUEUE 0
 #define OVS_DOCA_MAX_OFFLOAD_QUEUES 1
@@ -58,47 +57,42 @@ struct ovs_doca_flow_match {
 };
 BUILD_ASSERT_DECL(offsetof(struct ovs_doca_flow_match, d) == 0);
 
-doca_error_t
-ovs_doca_add_entry(struct netdev *netdev,
-                   unsigned int qid,
-                   struct doca_flow_pipe *pipe,
-                   const struct ovs_doca_flow_match *match,
-                   const struct ovs_doca_flow_actions *actions,
-                   const struct doca_flow_monitor *monitor,
-                   const struct doca_flow_fwd *fwd,
-                   uint32_t flags,
-                   struct doca_flow_pipe_entry **pentry);
+doca_error_t ovs_doca_add_entry(struct netdev *,
+                                unsigned int qid,
+                                struct doca_flow_pipe *,
+                                const struct ovs_doca_flow_match *,
+                                const struct ovs_doca_flow_actions *,
+                                const struct doca_flow_monitor *,
+                                const struct doca_flow_fwd *,
+                                uint32_t flags,
+                                struct doca_flow_pipe_entry **pentry);
 
-doca_error_t
-ovs_doca_remove_entry(struct netdev_doca_esw_ctx *esw,
-                      unsigned int qid, uint32_t flags,
-                      struct doca_flow_pipe_entry **entry);
+doca_error_t ovs_doca_remove_entry(struct netdev_doca_esw_ctx *esw,
+                                   unsigned int qid, uint32_t flags,
+                                   struct doca_flow_pipe_entry **entry);
 
-void
-ovs_doca_destroy_pipe(struct doca_flow_pipe **ppipe);
+void ovs_doca_destroy_pipe(struct doca_flow_pipe **ppipe);
 
-doca_error_t
-ovs_doca_pipe_create(struct netdev *netdev,
-                     struct ovs_doca_flow_match *match,
-                     struct ovs_doca_flow_match *match_mask,
-                     struct doca_flow_monitor *monitor,
-                     struct ovs_doca_flow_actions *actions,
-                     struct ovs_doca_flow_actions *actions_mask,
-                     struct doca_flow_action_desc *desc,
-                     struct doca_flow_fwd *fwd,
-                     struct doca_flow_fwd *fwd_miss,
-                     uint32_t nr_entries,
-                     bool is_egress, bool is_root,
-                     uint64_t queues_bitmap,
-                     const char *pipe_str,
-                     struct doca_flow_pipe **pipe);
+doca_error_t ovs_doca_pipe_create(struct netdev *netdev,
+                                  struct ovs_doca_flow_match *ovs_match,
+                                  struct ovs_doca_flow_match *ovs_match_mask,
+                                  struct doca_flow_monitor *,
+                                  struct ovs_doca_flow_actions *ovs_actions,
+                                  struct ovs_doca_flow_actions
+                                      *ovs_actions_mask,
+                                  struct doca_flow_action_desc *,
+                                  struct doca_flow_fwd *fwd,
+                                  struct doca_flow_fwd *fwd_miss,
+                                  uint32_t nr_entries,
+                                  bool is_egress, bool is_root,
+                                  uint64_t queues_bitmap,
+                                  const char *pipe_str,
+                                  struct doca_flow_pipe **pipe);
 
-doca_error_t
-ovs_doca_pipe_cfg_allow_queues(struct doca_flow_pipe_cfg *cfg,
-                               uint64_t queues_bitmap);
+doca_error_t ovs_doca_pipe_cfg_allow_queues(struct doca_flow_pipe_cfg *cfg,
+                                            uint64_t queues_bitmap);
 
-unsigned int
-ovs_doca_max_counters(void);
+unsigned int ovs_doca_max_counters(void);
 
 void ovs_doca_flow_limit_config_changed(unsigned int cfg_flow_limit);
 
